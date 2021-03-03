5d90110c24420a935fa3589c01ebf01860f61581
Luke Melia <luke@lukemelia.com>
2021-03-03 22:40:20 +0800

    WIP isolated card transition (using static context)

--- app/controllers/boxel.js
+++ app/controllers/boxel.js
@@ -116,119 +116,66 @@ class BoxelController extends Controller {
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   @action async isolatedCardTransition({
-    context,
     insertedSprites,
-    receivedSprites,
-    sentSprites,
+    keptSprites,
     removedSprites,
   }) {
-    // let sentSprite = sentSprites.size === 0 ? null : Array.from(sentSprites)[0];
-    // if (sentSprite) {
-    //   context.orphansElement.appendChild(sentSprite.element);
-    //   sentSprite.counterpart.element.style.opacity = 0;
-    // }
-    // let fadeOutAnimations = [];
-    // for (let removedSprite of Array.from(removedSprites)) {
-    //   removedSprite.element.style.opacity = 0;
-    //   let animation = removedSprite.element.animate(
-    //     [{ opacity: 1 }, { opacity: 0 }],
-    //     {
-    //       duration: FADE_DURATION,
-    //     }
-    //   );
-    //   fadeOutAnimations.push(animation);
-    // }
-    // await Promise.all(fadeOutAnimations.map((a) => a.finished));
-    // let translateAnimations = [];
-    // for (let receivedSprite of Array.from(receivedSprites)) {
-    //   let initialBounds = receivedSprite.initialBounds.relativeToPosition(
-    //     receivedSprite.finalBounds.parent
-    //   );
-    //   let finalBounds = receivedSprite.finalBounds.relativeToPosition(
-    //     receivedSprite.finalBounds.parent
-    //   );
-    //   receivedSprite.element.style.opacity = 0;
-    //   let deltaX = initialBounds.left - finalBounds.left;
-    //   let deltaY = initialBounds.top - finalBounds.top;
-    //   let translationKeyFrames = [
-    //     {
-    //       transform: `translate(${deltaX}px, ${deltaY}px)`,
-    //       width: `${initialBounds.width}px`,
-    //       height: `${initialBounds.height}px`,
-    //     },
-    //     {
-    //       transform: 'translate(0, 0)',
-    //       width: `${finalBounds.width}px`,
-    //       height: `${finalBounds.height}px`,
-    //     },
-    //   ];
-    //   context.orphansElement.appendChild(receivedSprite.counterpart.element);
-    //   receivedSprite.counterpart.lockStyles(
-    //     receivedSprite.finalBounds.relativeToPosition(
-    //       receivedSprite.finalBounds.parent
-    //     )
-    //   );
-    //   let animation = receivedSprite.counterpart.element.animate(
-    //     translationKeyFrames,
-    //     {
-    //       duration: TRANSLATE_DURATION,
-    //       easing: 'ease-in-out',
-    //     }
-    //   );
-    //   translateAnimations.push(animation);
-    // }
-    // for (let sentSprite of Array.from(sentSprites)) {
-    //   let initialBounds = sentSprite.initialBounds.relativeToPosition(
-    //     sentSprite.initialBounds.parent
-    //   );
-    //   let finalBounds = sentSprite.finalBounds.relativeToPosition(
-    //     sentSprite.initialBounds.parent
-    //   );
-    //   sentSprite.counterpart.element.style.opacity = 0;
-    //   let deltaX = finalBounds.left - initialBounds.left;
-    //   let deltaY = finalBounds.top - initialBounds.top;
-    //   let translationKeyFrames = [
-    //     {
-    //       transform: 'translate(0, 0)',
-    //       width: `${initialBounds.width}px`,
-    //       height: `${initialBounds.height}px`,
-    //     },
-    //     {
-    //       transform: `translate(${deltaX}px, ${deltaY}px)`,
-    //       width: `${finalBounds.width}px`,
-    //       height: `${finalBounds.height}px`,
-    //     },
-    //   ];
-    //   sentSprite.lockStyles(
-    //     sentSprite.counterpart.finalBounds.relativeToPosition(
-    //       sentSprite.counterpart.finalBounds.parent
-    //     )
-    //   );
-    //   let animation = sentSprite.element.animate(translationKeyFrames, {
-    //     duration: TRANSLATE_DURATION,
-    //     easing: 'ease-in-out',
-    //   });
-    //   translateAnimations.push(animation);
-    // }
-    // await Promise.all(translateAnimations.map((a) => a.finished));
-    // if (sentSprite) {
-    //   sentSprite.counterpart.element.style.opacity = 1;
-    // }
-    // let fadeInAnimations = [];
-    // for (let receivedSprite of Array.from(receivedSprites)) {
-    //   receivedSprite.element.style.opacity = null;
-    // }
-    // for (let insertedSprite of Array.from(insertedSprites)) {
-    //   let animation = insertedSprite.element.animate(
-    //     [{ opacity: 0 }, { opacity: 1 }],
-    //     {
-    //       duration: FADE_DURATION,
-    //     }
-    //   );
-    //   fadeInAnimations.push(animation);
-    // }
-    // await Promise.all(fadeInAnimations.map((a) => a.finished));
-    // context.clearOrphans();
+    for (let insertedSprite of Array.from(insertedSprites)) {
+      if (insertedSprite.id === 'card-more') {
+        insertedSprite.element.style.opacity = '0';
+      }
+    }
+    let fadeOutAnimations = [];
+    for (let removedSprite of Array.from(removedSprites)) {
+      removedSprite.element.style.opacity = 0;
+      let animation = removedSprite.element.animate(
+        [{ opacity: 1 }, { opacity: 0 }],
+        {
+          duration: FADE_DURATION,
+        }
+      );
+      fadeOutAnimations.push(animation);
+    }
+    await Promise.all(fadeOutAnimations.map((a) => a.finished));
+    let translateAnimations = [];
+    for (let keptSprite of Array.from(keptSprites)) {
+      let initialBounds = keptSprite.initialBounds.relativeToContext;
+      let finalBounds = keptSprite.finalBounds.relativeToContext;
+      let deltaX = initialBounds.left - finalBounds.left;
+      let deltaY = initialBounds.top - finalBounds.top;
+      let translationKeyFrames = [
+        {
+          transform: `translate(${deltaX}px, ${deltaY}px)`,
+          width: `${initialBounds.width}px`,
+          height: `${initialBounds.height}px`,
+        },
+        {
+          transform: 'translate(0, 0)',
+          width: `${finalBounds.width}px`,
+          height: `${finalBounds.height}px`,
+        },
+      ];
+      let animation = keptSprite.element.animate(translationKeyFrames, {
+        duration: TRANSLATE_DURATION,
+        easing: 'ease-in-out',
+      });
+      translateAnimations.push(animation);
+    }
+    await Promise.all(translateAnimations.map((a) => a.finished));
+    let fadeInAnimations = [];
+    for (let insertedSprite of Array.from(insertedSprites)) {
+      if (insertedSprite.id === 'card-more') {
+        insertedSprite.element.style.removeProperty('opacity');
+      }
+      let animation = insertedSprite.element.animate(
+        [{ opacity: 0 }, { opacity: 1 }],
+        {
+          duration: FADE_DURATION,
+        }
+      );
+      fadeInAnimations.push(animation);
+    }
+    await Promise.all(fadeInAnimations.map((a) => a.finished));
   }
 }
 
