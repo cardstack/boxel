3145491779b214233ee409c90f9f843eee231859
nickschot <nickschot@gmail.com>
2022-03-03 17:03:14 +0100

    Update motion-study demo to runAnimations utility

--- app/controllers/motion-study.ts
+++ app/controllers/motion-study.ts
@@ -3,9 +3,10 @@ import Changeset from 'animations/models/changeset';
 import magicMove from 'animations/transitions/magic-move';
 import { SpriteType } from 'animations/models/sprite';
 import fade from 'animations/transitions/fade';
+import runAnimations from 'animations/utils/run-animations';
 
 export default class MotionStudy extends Controller {
-  async transition(changeset: Changeset) {
+  async transition(changeset: Changeset): Promise<void> {
     let { context } = changeset;
 
     let removedCardContentSprites = changeset.spritesFor({
@@ -14,15 +15,13 @@ export default class MotionStudy extends Controller {
     });
 
     if (removedCardContentSprites.size) {
-      await fade({
+      fade({
         context,
         insertedSprites: new Set(),
         removedSprites: removedCardContentSprites,
         keptSprites: new Set(),
       } as Changeset);
     }
-
-    let animations = [];
 
     let removedCardSprites = changeset.spritesFor({
       role: 'card',
@@ -38,14 +37,13 @@ export default class MotionStudy extends Controller {
       role: 'card',
       type: SpriteType.Kept,
     });
-    animations.push(
-      magicMove({
-        context,
-        insertedSprites: new Set(),
-        removedSprites: new Set(),
-        keptSprites: cardSprites,
-      } as Changeset)
-    );
+
+    magicMove({
+      context,
+      insertedSprites: new Set(),
+      removedSprites: new Set(),
+      keptSprites: cardSprites,
+    } as Changeset);
 
     let cardContentSprites = changeset.spritesFor({
       role: 'card-content',
@@ -55,16 +53,18 @@ export default class MotionStudy extends Controller {
       s.element.style.opacity = '0';
     });
 
-    await Promise.all(animations);
+    await runAnimations(changeset);
 
     removedCardSprites.forEach((r) => r.hide());
 
-    await fade({
+    fade({
       context,
       insertedSprites: cardContentSprites,
       removedSprites: new Set(),
       keptSprites: new Set(),
     } as Changeset);
+
+    await runAnimations(changeset);
 
     cardContentSprites.forEach((s) => {
       s.element.style.removeProperty('opacity');
