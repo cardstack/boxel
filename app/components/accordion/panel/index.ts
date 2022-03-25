06ce601de515e60987f6a65c67e30dcab4ca7328
nickschot <nickschot@gmail.com>
2022-03-25 17:07:05 +0100

    Clean up accordion demo to what it should be (once we fix measurements)

--- app/components/accordion/panel/index.ts
+++ app/components/accordion/panel/index.ts
@@ -2,12 +2,15 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import Changeset from 'animations/models/changeset';
 import Sprite, { SpriteType } from 'animations/models/sprite';
-import LinearBehavior from 'animations/behaviors/linear';
 import runAnimations from 'animations/utils/run-animations';
+
+//import LinearBehavior from 'animations/behaviors/linear';
+import SpringBehavior from 'animations/behaviors/spring';
 
 export default class AccordionPanel extends Component {
   @action async resizePanels(changeset: Changeset) {
-    let duration = 320;
+    let behavior = new SpringBehavior({ overshootClamping: true });
+    let duration = behavior instanceof SpringBehavior ? undefined : 320;
     let { context } = changeset;
     let containers = changeset.spritesFor({
       type: SpriteType.Kept,
@@ -26,21 +29,13 @@ export default class AccordionPanel extends Component {
     let spritesToAnimate = [];
 
     if (hiddenPanel) {
-      context.appendOrphan(hiddenPanel);
-      hiddenPanel.lockStyles();
-      // hardcoded at the moment, lockStyles seems to be a bit buggy with height auto
-      // the element gets positioned in an awkward way in the orphans element
-      hiddenPanel.element.style.top = '70px';
-      hiddenPanel.element.style.left = '0px';
+      // TODO: might be nice to detect this automatically in the appendOrphan function
+      if (!context.hasOrphan(hiddenPanel)) {
+        context.appendOrphan(hiddenPanel);
 
-      hiddenPanel.setupAnimation('size', {
-        startWidth: hiddenPanel.element.clientWidth,
-        startHeight: hiddenPanel.element.clientHeight,
-        endWidth: hiddenPanel.element.clientWidth,
-        duration,
-        behavior: new LinearBehavior(),
-      });
-      spritesToAnimate.push(hiddenPanel);
+        // TODO: something is weird here when interrupting an interruped animation
+        hiddenPanel.lockStyles();
+      }
     }
 
     if (containers.size) {
@@ -49,7 +44,7 @@ export default class AccordionPanel extends Component {
           startHeight: sprite.initialBounds?.element.height,
           endHeight: sprite.finalBounds?.element.height,
           duration,
-          behavior: new LinearBehavior(),
+          behavior,
         });
         spritesToAnimate.push(sprite);
       }
