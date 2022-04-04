80eec6bddacf34eec23c9bc10fcd2ef906baa1c2
Edward Faulkner <edward@eaf4.com>
2022-04-04 11:06:56 -0400

    this is handled by local-realm service now

--- host/app/routes/application.ts
+++ host/app/routes/application.ts
@@ -1,8 +1,3 @@
 import Route from '@ember/routing/route';
-import { launchWorker } from 'runtime-spike/lib/launch-worker';
 
-export default class Application extends Route {
-  async beforeModel() {
-    await launchWorker();
-  }
-}
+export default class Application extends Route {}
