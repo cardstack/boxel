6aa64277b896f60f4c58cf26bd6d6a589b199553
nickschot <nickschot@gmail.com>
2022-06-10 12:29:08 +0200

    Add nested-contexts w/ context as sprite demo

---
+++ demo-app/app/controllers/nested-contexts.ts
@@ -0,0 +1,24 @@
+import Controller from '@ember/controller';
+import Changeset from 'animations-experiment/models/changeset';
+import { tracked } from '@glimmer/tracking';
+
+export default class NestedContexts extends Controller {
+  @tracked showLevel2 = false;
+  @tracked showLevel3 = false;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function,@typescript-eslint/no-unused-vars
+  async level1Transition(changeset: Changeset) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function,@typescript-eslint/no-unused-vars
+  async level2Transition(changeset: Changeset) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function,@typescript-eslint/no-unused-vars
+  async level3Transition(changeset: Changeset) {}
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
+declare module '@ember/controller' {
+  interface Registry {
+    'nested-contexts': NestedContexts;
+  }
+}
--- demo-app/app/router.js
+++ demo-app/app/router.js
@@ -18,4 +18,5 @@ Router.map(function () {
     this.route('details', { path: '/:id' });
   });
   this.route('accordion');
+  this.route('nested-contexts');
 });
