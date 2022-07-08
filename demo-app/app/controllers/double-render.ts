b6d64d1be8f2c6be56aa744b7c5bab61af0a4972
Aierie <39579264+Aierie@users.noreply.github.com>
2022-07-08 17:40:20 +0800

    demo the double-render problem

---
+++ demo-app/app/controllers/double-render.ts
@@ -0,0 +1,84 @@
+import Controller from '@ember/controller';
+import Sprite, { SpriteType } from 'animations-experiment/models/sprite';
+import Changeset from 'animations-experiment/models/changeset';
+import magicMove from 'animations-experiment/transitions/magic-move';
+import { assert } from '@ember/debug';
+import ContextAwareBounds from 'animations-experiment/models/context-aware-bounds';
+import runAnimations from 'animations-experiment/utils/run-animations';
+import SpringBehavior from 'animations-experiment/behaviors/spring';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import LinearBehavior from 'animations-experiment/behaviors/linear';
+
+export default class DoubleRenderController extends Controller {
+  @tracked count = 0;
+  @tracked isShowing = true;
+
+  @action
+  hide() {
+    this.isShowing = false;
+  }
+
+  @action
+  show() {
+    this.isShowing = true;
+  }
+
+  @action
+  increment() {
+    this.count += 1;
+  }
+
+  async transition(changeset: Changeset): Promise<void> {
+    let { removedSprites, keptSprites, insertedSprites } = changeset;
+    let duration = 3000;
+
+    removedSprites.forEach((sprite) => {
+      if (changeset.context.hasOrphan(sprite)) {
+        changeset.context.removeOrphan(sprite);
+      }
+      console.log('handling removed sprite');
+      changeset.context.appendOrphan(sprite);
+      sprite.lockStyles();
+      sprite.setupAnimation('position', {
+        startY: 0,
+        startX: 0,
+        endY: -200,
+        endX: 0,
+        behavior: new LinearBehavior(),
+        duration,
+      });
+    });
+
+    insertedSprites.forEach((sprite) => {
+      if (changeset.context.hasOrphan(sprite)) {
+        changeset.context.removeOrphan(sprite);
+      }
+      sprite.setupAnimation('position', {
+        startY: -200,
+        behavior: new LinearBehavior(),
+        duration,
+      });
+    });
+
+    keptSprites.forEach((sprite) => {
+      if (changeset.context.hasOrphan(sprite)) {
+        changeset.context.removeOrphan(sprite);
+      }
+      sprite.setupAnimation('position', {
+        startY: sprite.initialBounds?.relativeToContext.y,
+        endY: sprite.finalBounds?.relativeToContext.y,
+        behavior: new LinearBehavior(),
+        duration,
+      });
+    });
+
+    await runAnimations([
+      ...removedSprites,
+      ...keptSprites,
+      ...insertedSprites,
+    ]);
+
+    console.log('done animating');
+  }
+}
