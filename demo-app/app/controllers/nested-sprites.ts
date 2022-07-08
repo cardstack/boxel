9a0ad58a8fb3ad8c21bb5511d5be532fd09b4db2
Nick Schot <nickschot@gmail.com>
2022-07-08 14:24:53 +0200

    Nested sprite demo (#64)
    * sprite tree nested sprite access method
    
    * Add nested sprites demo

---
+++ demo-app/app/controllers/nested-sprites.ts
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import Changeset from 'animations-experiment/models/changeset';
+import magicMove from 'animations-experiment/transitions/magic-move';
+import runAnimations from 'animations-experiment/utils/run-animations';
+
+export default class NestedSprites extends Controller {
+  @tracked moveOuter = false;
+  @tracked moveInner = true;
+
+  async transition(changeset: Changeset) {
+    magicMove(changeset, { duration: 5000 });
+    await runAnimations([...changeset.keptSprites]);
+  }
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
+declare module '@ember/controller' {
+  interface Registry {
+    'nested-sprites': NestedSprites;
+  }
+}
