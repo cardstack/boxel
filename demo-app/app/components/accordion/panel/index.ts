4ca454fd2ff4707841faad6946a0b798145b52fa
Aierie <39579264+Aierie@users.noreply.github.com>
2022-07-20 14:57:50 +0800

    remove unused comments

--- demo-app/app/components/accordion/panel/index.ts
+++ demo-app/app/components/accordion/panel/index.ts
@@ -7,26 +7,6 @@ import runAnimations from 'animations-experiment/utils/run-animations';
 //import LinearBehavior from 'animations-experiment/behaviors/linear';
 import SpringBehavior from 'animations-experiment/behaviors/spring';
 
-// hasOrphan does not work. We cannot compare orphan parents properly
-// We probably need to mark orphan elements with data attributes
-// one from the sprite identifier
-// one as a custom identifier, this is more for the cloning side of things
-// And check for matching based on that
-// We also should probably have a good way to clean up orphans that no longer belong to a context
-// If a changeset passed to a context does not have a sprite matching an orphan, should we remove it?
-//   - probably not. removed sprites will not appear again in the changeset
-// If the same changeset has a non-removed sprite that matches the orphan, should we remove it?
-//   - yes, unless the user is doing cloning things.
-// If another changeset has a non-removed sprite that matches the orphan, should we remove it?
-//   - unsure
-// Should we differentiate opt-in non-removed orphans from removed sprite orphans?
-//   - at first glance, yes.
-// Who should be responsible for this cleanup?
-//   - user first. then when we're clearer about what needs to happen, library
-// Is that a user concern or a library concern?
-// An orphan should be removed if any context has a matching non-removed sprite
-// It's up to the context of that changeset to decide whether or not a replacement orphan is set up
-
 export default class AccordionPanel extends Component {
   @action async resizePanels(changeset: Changeset) {
     let behavior = new SpringBehavior({ overshootClamping: true });
@@ -74,7 +54,6 @@ export default class AccordionPanel extends Component {
     }
 
     if (nonOrphanPanel) {
-      console.log('kept panel', nonOrphanPanel);
       if (context.hasOrphan(nonOrphanPanel)) {
         context.removeOrphan(nonOrphanPanel);
       }
