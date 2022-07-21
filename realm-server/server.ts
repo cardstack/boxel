36a00090966b47f7814200b07d17446863437b38
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2022-07-21 16:25:27 -0400

    updated comment

--- realm-server/server.ts
+++ realm-server/server.ts
@@ -40,13 +40,14 @@ export function createRealmServer(path: string, realmURL: URL) {
         isStreaming = true;
         nodeStream.pipe(res);
       } else if (body instanceof ReadableStream) {
-        // since we are using native fetch Response in our Realm API, any
-        // strings or buffers will be converted into web-streams automatically
-        // in the fetch Response--this is not to be confused with actual file
-        // streams that the Realm is creating. The node HTTP server does not
-        // play nice with web-streams, so we will read these streams into
-        // strings and then include in our node ServerResponse. Actual node file
-        // streams will not be handled here--those will be taken care of above.
+        // A quirk with native fetch Response in node is that it will be clever
+        // and convert strings or buffers in the response.body into web-streams
+        // automatically. This is not to be confused with actual file streams
+        // that the Realm is creating. The node HTTP server does not play nice
+        // with web-streams, so we will read these streams back into strings and
+        // then include in our node ServerResponse. Actual node file streams
+        // (i.e streams that we are intentionally creating in the Realm) will
+        // not be handled here--those will be taken care of above.
         res.write(await webStreamToText(body));
       } else if (body != null) {
         res.write(body);
