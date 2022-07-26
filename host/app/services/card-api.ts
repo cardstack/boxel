ac8e1dee86de529ca9cc9f6c53ab1b7c9123c19c
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2022-07-26 19:20:15 -0400

    debugging logs

--- host/app/services/card-api.ts
+++ host/app/services/card-api.ts
@@ -5,20 +5,14 @@ import { baseRealm } from '@cardstack/runtime-common';
 import config from 'runtime-spike/config/environment';
 
 export type API = typeof import('https://cardstack.com/base/card-api');
-export type StringModule = typeof import('https://cardstack.com/base/string');
-export type IntegerModule = typeof import('https://cardstack.com/base/integer');
-export type DateModule = typeof import('https://cardstack.com/base/date');
-export type DatetimeModule =
-  typeof import('https://cardstack.com/base/datetime');
-export type PickModule = typeof import('https://cardstack.com/base/pick');
 
 export default class CardAPI extends Service {
   #api: API | undefined;
-  #string: StringModule | undefined;
-  #integer: IntegerModule | undefined;
-  #date: DateModule | undefined;
-  #datetime: DatetimeModule | undefined;
-  #pick: PickModule | undefined;
+  #string: typeof import('https://cardstack.com/base/string') | undefined;
+  #integer: typeof import('https://cardstack.com/base/integer') | undefined;
+  #date: typeof import('https://cardstack.com/base/date') | undefined;
+  #datetime: typeof import('https://cardstack.com/base/datetime') | undefined;
+  #pick: typeof import('https://cardstack.com/base/pick') | undefined;
 
   constructor(properties: object) {
     super(properties);
@@ -42,8 +36,18 @@ export default class CardAPI extends Service {
       !this.#datetime ||
       !this.#pick
     ) {
+      let loadStatus = {
+        cardAPI: Boolean(this.#api),
+        string: Boolean(this.#string),
+        integer: Boolean(this.#integer),
+        date: Boolean(this.#date),
+        datetime: Boolean(this.#datetime),
+        pick: Boolean(this.#pick),
+      };
       throw new Error(
-        `bug: card API has not loaded yet--make sure to await this.loaded before using the api`
+        `bug: card API testModules have not loaded yet--make sure to await this.loaded before using the api: ${JSON.stringify(
+          loadStatus
+        )}`
       );
     }
     return {
