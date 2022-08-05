fc1ae1272d349b909ebde9b6020dc7de305d0fab
Aierie <39579264+Aierie@users.noreply.github.com>
2022-08-05 15:35:05 +0800

    add gallery content

--- demo-app/app/components/home-reno-v2/data/gallery.ts
+++ demo-app/app/components/home-reno-v2/data/gallery.ts
@@ -1,4 +1,11 @@
+const Duck1 = '/images/brett-jordan-wF7GqWA3Tag-unsplash.jpg';
+const Duck2 = '/images/jason-richard-VTvnoNBowZs-unsplash.jpg';
+const Duck3 = '/images/s-tsuchiya-_WjhfEzRDak-unsplash.jpg';
+const Duck4 = '/images/timothy-dykes-LhqLdDPcSV8-unsplash.jpg';
+
 export default {
-  model: {},
+  model: {
+    images: [Duck1, Duck2, Duck3, Duck4],
+  },
   suggestions: ['form'],
 };
