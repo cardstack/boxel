de3b5873fc26073548fd576e665c840fb71e8821
Aierie <39579264+Aierie@users.noreply.github.com>
2022-08-12 14:10:27 +0800

    it's kind of working!

--- demo-app/app/components/home-reno-v2/index.ts
+++ demo-app/app/components/home-reno-v2/index.ts
@@ -1,7 +1,19 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { Changeset } from 'animations-experiment/models/changeset';
 import { CARD_STATES, maximizedCardList, Card } from './data/card';
+import {
+  expandedToMax,
+  expandedToMaxImages,
+  groupSprites,
+  maxToExpanded,
+  maxToExpandedImages,
+  simple,
+} from './transitions';
+
+// NEXT: identify
+// NEXT: layers
 
 export default class HomeRenoV2 extends Component {
   entrypoint = new Card({
@@ -15,12 +27,6 @@ export default class HomeRenoV2 extends Component {
 
   updateState() {
     this.cardList = maximizedCardList(this.entrypoint);
-    // console.table(
-    //   this.cardList.map((v) => ({
-    //     type: v.type,
-    //     id: v.id,
-    //   }))
-    // );
   }
 
   @action minimize(card: Card) {
@@ -54,5 +60,196 @@ export default class HomeRenoV2 extends Component {
     }
     card.changeState(CARD_STATES.MAX);
     this.updateState();
+  }
+
+  @action async transition(changeset: Changeset) {
+    let groupedSprites = groupSprites(changeset);
+    let staticCards = Object.fromEntries(
+      Object.entries(groupedSprites).filter(([k, v]) => v.state === 'STATIC')
+    );
+    let shrinkingFromMaxCards = Object.fromEntries(
+      Object.entries(groupedSprites).filter(
+        ([k, v]) => v.state.startsWith('MAX')
+        // || (v.state === 'REMOVED' && v.card?.element.className.includes('MAX'))
+      )
+    );
+    let enteringCards = Object.fromEntries(
+      Object.entries(groupedSprites).filter(([k, v]) => v.state.endsWith('MAX'))
+    );
+
+    for (let cardId in staticCards) {
+      let group = groupedSprites[cardId]!;
+      simple(group.card!);
+    }
+
+    for (let cardId in groupedSprites) {
+      let group = groupedSprites[cardId]!;
+      if (group.placeholder && group.state === 'STATIC') {
+        simple(group.placeholder!);
+      }
+    }
+
+    // hide entering cards that shouldn't be visible yet
+    // keep their counterparts around because we need to fade their contents out before doing the swap
+    if (Object.keys(shrinkingFromMaxCards).length) {
+      for (let cardId in enteringCards) {
+        let group = groupedSprites[cardId]!;
+        group.card!.element.style.opacity = '0';
+        group.card?.counterpart?.lockStyles();
+        changeset.context.appendOrphan(group.card!.counterpart!);
+      }
+    }
+
+    for (let cardId in shrinkingFromMaxCards) {
+      let group = groupedSprites[cardId]!;
+      let isImages = false;
+      group.keptContent.forEach(
+        (v) => (isImages = isImages || v.role === 'image')
+      );
+      if (isImages) {
+        await maxToExpandedImages(
+          changeset.context,
+          group,
+          Object.values(groupedSprites)
+        );
+      } else {
+        await maxToExpanded(changeset.context, group);
+      }
+    }
+
+    // unhide entering cards
+    if (Object.keys(shrinkingFromMaxCards).length) {
+      for (let cardId in enteringCards) {
+        let group = groupedSprites[cardId]!;
+        group.card!.element.style.opacity = '1';
+      }
+    }
+
+    for (let cardId in enteringCards) {
+      let group = groupedSprites[cardId]!;
+
+      // TODO: Actually we can formalize this as "gallery"
+      let isImages = false;
+      group.keptContent.forEach(
+        (v) => (isImages = isImages || v.role === 'image')
+      );
+      if (isImages) {
+        await expandedToMaxImages(
+          changeset.context,
+          group,
+          Object.values(groupedSprites)
+        );
+      } else {
+        await expandedToMax(changeset.context, group);
+      }
+
+      // hasEnteringCards = true;
+      // let group = groupedSprites[cardId]!;
+      // magicMove(
+      //   {
+      //     keptSprites: new Set([group.title]) as Set<Sprite>,
+      //   } as Changeset,
+      //   {
+      //     duration,
+      //     delay: delay,
+      //     relativeTo: {
+      //       initialBounds: (group.card as Sprite).initialBounds!.element,
+      //       finalBounds: (group.card as Sprite).finalBounds!.element,
+      //     },
+      //   }
+      // );
+
+      // if (group.keptContent.size) {
+
+      //   if (isImages) {
+      //   } else {
+      //     let s: Set<Sprite> = new Set();
+      //     group.keptContent.forEach((v) => {
+      //       s.add(v);
+      //     });
+      //     magicMove(
+      //       {
+      //         keptSprites: s,
+      //       } as Changeset,
+      //       {
+      //         duration,
+      //         delay: 0,
+      //       }
+      //     );
+      //   }
+      // }
+
+      // magicMove(
+      //   {
+      //     keptSprites: new Set([group.card]) as Set<Sprite>,
+      //   } as Changeset,
+      //   {
+      //     duration,
+      //     delay: delay,
+      //   }
+      // );
+    }
+
+    // if (hasEnteringCards) delay += duration;
+
+    // for (let cardId in staticCards) {
+    //   let group = groupedSprites[cardId]!;
+    //   magicMove(
+    //     {
+    //       keptSprites: new Set([group.title]) as Set<Sprite>,
+    //     } as Changeset,
+    //     {
+    //       duration,
+    //       delay: 0,
+    //       relativeTo: {
+    //         initialBounds: (group.card as Sprite).initialBounds!.element,
+    //         finalBounds: (group.card as Sprite).finalBounds!.element,
+    //       },
+    //     }
+    //   );
+
+    //   if (group.keptContent.size) {
+    //     let s: Set<Sprite> = new Set();
+    //     group.keptContent.forEach((v) => {
+    //       s.add(v);
+    //     });
+    //     magicMove(
+    //       {
+    //         keptSprites: s,
+    //       } as Changeset,
+    //       {
+    //         duration,
+    //         delay: delay,
+    //         // relativeTo: {
+    //         //   initialBounds: (group.card as Sprite).initialBounds!.element,
+    //         //   finalBounds: (group.card as Sprite).finalBounds!.element,
+    //         // },
+    //       }
+    //     );
+    //   }
+
+    //   magicMove(
+    //     {
+    //       keptSprites: new Set([group.card]) as Set<Sprite>,
+    //     } as Changeset,
+    //     {
+    //       duration,
+    //       delay: 0,
+    //     }
+    //   );
+
+    //   if (group.placeholder)
+    //     magicMove(
+    //       {
+    //         keptSprites: new Set([group.placeholder]) as Set<Sprite>,
+    //       } as Changeset,
+    //       {
+    //         duration,
+    //         delay: 0,
+    //       }
+    //     );
+    // }
+
+    // await runAnimations(allSprites);
   }
 }
---
+++ demo-app/app/components/home-reno-v2/transitions.ts
@@ -0,0 +1,547 @@
+import runAnimations from 'animations-experiment/utils/run-animations';
+import Sprite from 'animations-experiment/models/sprite';
+import { IContext } from 'animations-experiment/models/sprite-tree';
+import magicMove from 'animations-experiment/transitions/magic-move';
+import { Changeset } from 'animations-experiment/models/changeset';
+import { CARD_STATES } from './data/card';
+import LinearBehavior from 'animations-experiment/behaviors/linear';
+
+const DEFAULT_DURATION = 500;
+
+interface SpriteGroup {
+  // HMM: these states are not reliably identified once interruptions happen
+  state:
+    | 'STATIC' // INCLUDES MIN -> EXPANDED, EXPANDED -> MIN
+    | 'MAX -> EXPANDED'
+    | 'EXPANDED -> MAX'
+    | 'MAX -> MIN'
+    | 'REMOVED'
+    | 'INSERTED';
+  placeholder: Sprite | null;
+  mainCardContent: Sprite | null;
+  keptContent: Set<Sprite>;
+  insertedContent: Set<Sprite>;
+  removedContent: Set<Sprite>;
+  title: Sprite | null;
+  card: Sprite | null;
+}
+
+export async function maxToExpanded(
+  context: IContext,
+  spriteGroup: SpriteGroup
+) {
+  let duration = DEFAULT_DURATION;
+  let behavior = new LinearBehavior();
+  let fadeOutSprites: Sprite[] = [];
+  // Lock placeholder if needed (if this is a shrink)
+  let placeholder = spriteGroup.placeholder;
+  if (placeholder) {
+    placeholder.lockStyles();
+    placeholder.element.style.zIndex = '0';
+    context.appendOrphan(placeholder);
+    placeholder.setupAnimation('position', {
+      duration,
+      behavior,
+      startY: 0,
+      endY:
+        -placeholder.initialBounds!.relativeToContext.y +
+        spriteGroup.card!.finalBounds!.relativeToContext.y,
+    });
+    fadeOutSprites.push(placeholder);
+  }
+
+  // Hide the card proper
+  spriteGroup.card!.element.style.opacity = '0';
+
+  // Lock counterpart of card
+  let counterpartCard = spriteGroup.card!.counterpart!;
+  counterpartCard.lockStyles();
+  context.appendOrphan(counterpartCard!);
+
+  // Content fades out
+  let mainCardContent = spriteGroup.mainCardContent!;
+  {
+    let counterpart = mainCardContent.counterpart;
+    if (counterpart) {
+      // Hide all content of card sprite proper besides title
+      mainCardContent.element.style.opacity = '0';
+      counterpart.lockStyles();
+      counterpart.element.style.zIndex =
+        getComputedStyle(counterpartCard.element).zIndex + 1;
+      context.appendOrphan(counterpart);
+      counterpart.setupAnimation('opacity', {
+        from: 1,
+        to: 0,
+        duration: duration,
+      });
+      fadeOutSprites.push(mainCardContent.counterpart!);
+    }
+  }
+
+  // Run these first
+  await runAnimations(fadeOutSprites);
+
+  // Hide counterpart
+  if (mainCardContent.counterpart) {
+    context.removeOrphan(mainCardContent.counterpart);
+  }
+  spriteGroup.card!.element.style.opacity = '1';
+  context.removeOrphan(counterpartCard);
+
+  // Card sprite proper resizes and moves together with title
+  magicMove(
+    {
+      keptSprites: new Set([spriteGroup.card!]),
+    } as Changeset,
+    {
+      duration,
+    }
+  );
+  spriteGroup.card!.element.style.zIndex = '2';
+  if (placeholder) {
+    placeholder.element.style.zIndex = '0';
+  }
+  await runAnimations([spriteGroup.card!]);
+  if (placeholder) context.removeOrphan(placeholder!);
+
+  let fadeInSprites: Sprite[] = [];
+
+  // Card sprite proper content fades in
+  mainCardContent.element.style.opacity = '1';
+  mainCardContent.setupAnimation('opacity', {
+    from: 0,
+    to: 1,
+    duration: duration,
+  });
+  fadeInSprites.push(mainCardContent);
+
+  await runAnimations(fadeInSprites);
+}
+
+export async function expandedToMax(
+  context: IContext,
+  spriteGroup: SpriteGroup
+) {
+  let duration = DEFAULT_DURATION;
+  let fadeOutSprites: Sprite[] = [];
+
+  // Hide the card proper
+  spriteGroup.card!.element.style.opacity = '0';
+
+  // Lock counterpart of card
+  let counterpartCard = spriteGroup.card!.counterpart!;
+  if (!context.hasOrphan(counterpartCard)) {
+    counterpartCard.lockStyles();
+    context.appendOrphan(counterpartCard!);
+  }
+
+  // Content fades out
+  let mainCardContent = spriteGroup.mainCardContent!;
+  {
+    let counterpart = mainCardContent.counterpart;
+    if (counterpart) {
+      // Hide all content of card sprite proper besides title
+      mainCardContent.element.style.opacity = '0';
+      counterpart.element.style.zIndex =
+        getComputedStyle(counterpartCard.element).zIndex + 1;
+      counterpart.lockStyles();
+      context.appendOrphan(counterpart);
+      counterpart.setupAnimation('opacity', { to: 0, duration: duration });
+      fadeOutSprites.push(mainCardContent.counterpart!);
+    }
+  }
+
+  // Run these first
+  await runAnimations(fadeOutSprites);
+
+  // Hide counterpart
+  if (mainCardContent.counterpart) {
+    context.removeOrphan(mainCardContent.counterpart);
+  }
+  spriteGroup.card!.element.style.opacity = '1';
+  context.removeOrphan(counterpartCard);
+
+  // Card sprite proper resizes and moves together with title
+  magicMove(
+    {
+      keptSprites: new Set([spriteGroup.card!]),
+    } as Changeset,
+    {
+      duration,
+    }
+  );
+  spriteGroup.card!.element.style.zIndex = '2';
+  await runAnimations([spriteGroup.card!]);
+
+  let fadeInSprites: Sprite[] = [];
+  // Card sprite proper content fades in
+  mainCardContent.element.style.opacity = '1';
+  mainCardContent.setupAnimation('opacity', {
+    from: 0,
+    to: 1,
+    duration: duration,
+  });
+  fadeInSprites.push(mainCardContent);
+
+  await runAnimations(fadeInSprites);
+}
+
+export async function expandedToMaxImages(
+  context: IContext,
+  spriteGroup: SpriteGroup,
+  otherCards: SpriteGroup[]
+) {
+  // Put images in old positions
+  // Move the card
+  // Put images in new positions
+
+  let duration = DEFAULT_DURATION;
+  let images: Sprite[] = [];
+
+  let fadeInSprites = otherCards
+    .filter((group) => {
+      return (
+        spriteGroup.card!.element !== group.card!.element &&
+        spriteGroup.card!.element.contains(group.card!.element)
+      );
+    })
+    .map((v) => v.card!);
+  fadeInSprites.forEach((card) => (card.element.style.opacity = '0'));
+
+  spriteGroup.keptContent.forEach((s) => {
+    s.setupAnimation('position', {
+      duration,
+      behavior: new LinearBehavior(),
+      startX: -s.boundsDelta!.x + spriteGroup.card!.boundsDelta!.x,
+      startY: -s.boundsDelta!.y + spriteGroup.card!.boundsDelta!.y,
+      endX: -s.boundsDelta!.x + spriteGroup.card!.boundsDelta!.x + 5,
+      endY: -s.boundsDelta!.y + spriteGroup.card!.boundsDelta!.y + 5,
+    });
+    images.push(s);
+  });
+
+  // Card sprite proper resizes and moves together with title
+  magicMove(
+    {
+      keptSprites: new Set([spriteGroup.card!]),
+    } as Changeset,
+    {
+      duration,
+    }
+  );
+  spriteGroup.card!.element.style.zIndex = '2';
+
+  await runAnimations([...images, spriteGroup.card!]);
+
+  images.forEach((image) => {
+    image.setupAnimation('position', {
+      duration,
+      behavior: new LinearBehavior(),
+      startX: -image.boundsDelta!.x + spriteGroup.card!.boundsDelta!.x + 5,
+      startY: -image.boundsDelta!.y + spriteGroup.card!.boundsDelta!.y + 5,
+    });
+  });
+
+  await runAnimations(images);
+
+  fadeInSprites.forEach((card) => (card.element.style.opacity = '1'));
+
+  fadeInSprites.forEach((sprite) => {
+    sprite.setupAnimation('opacity', {
+      duration,
+      from: 0,
+      to: 1,
+    });
+  });
+
+  await runAnimations(fadeInSprites);
+}
+
+export async function maxToExpandedImages(
+  context: IContext,
+  spriteGroup: SpriteGroup,
+  otherCards: SpriteGroup[]
+) {
+  // Put images in old positions
+  // Move the card
+  // Put images in new positions
+
+  let duration = DEFAULT_DURATION;
+  let behavior = new LinearBehavior();
+  let images: Sprite[] = [];
+
+  // Lock placeholder
+  let placeholder = spriteGroup.placeholder;
+  if (placeholder) {
+    placeholder.lockStyles();
+    placeholder.element.style.zIndex = '0';
+    context.appendOrphan(placeholder);
+    // FIXME?: This is a way of handling changes in the position of the placeholder
+    // Because of other cards resizing
+    placeholder.setupAnimation('position', {
+      duration,
+      behavior,
+      startY: 0,
+      endY:
+        -placeholder.initialBounds!.relativeToContext.y +
+        spriteGroup.card!.finalBounds!.relativeToContext.y,
+    });
+  }
+
+  spriteGroup.card!.element.style.opacity = '0';
+  let counterpart = spriteGroup.card!.counterpart!;
+  context.appendOrphan(counterpart);
+  counterpart.lockStyles();
+
+  let fadeOutSprites = otherCards
+    .filter((group) => {
+      return (
+        counterpart!.element !== group.card!.element &&
+        counterpart!.element.contains(group.card!.element)
+      );
+    })
+    .map((v) => v.card!);
+
+  fadeOutSprites.forEach((sprite) => {
+    sprite.setupAnimation('opacity', {
+      duration,
+      to: 0,
+    });
+  });
+
+  await runAnimations(
+    fadeOutSprites
+      .concat([counterpart])
+      .concat(placeholder ? [placeholder] : [])
+  );
+
+  spriteGroup.card!.element.style.opacity = '1';
+  context.removeOrphan(counterpart);
+
+  spriteGroup.keptContent.forEach((s) => {
+    s.setupAnimation('position', {
+      duration,
+      behavior: new LinearBehavior(),
+      startX: -s.boundsDelta!.x + spriteGroup.card!.boundsDelta!.x,
+      startY: -s.boundsDelta!.y + spriteGroup.card!.boundsDelta!.y,
+      // endX: spriteGroup.card!.boundsDelta!.x,
+      // endY: spriteGroup.card!.boundsDelta!.y,
+    });
+    images.push(s);
+  });
+
+  let positionTransform = `translate(${-spriteGroup.card!.boundsDelta!
+    .x}px, ${-spriteGroup.card!.boundsDelta!.y}px)`;
+  let sizeTransform = `scaleY(${
+    spriteGroup.card!.initialHeight! / spriteGroup.card!.finalHeight!
+  }) scaleX(${
+    spriteGroup.card!.initialWidth! / spriteGroup.card!.finalWidth!
+  })`;
+
+  spriteGroup.card!.element.style.transform = `${positionTransform} ${sizeTransform}`;
+  spriteGroup.card!.element.style.transformOrigin = `top left`;
+  spriteGroup.card!.element.style.setProperty(
+    '--animation-scale-inversion-y',
+    `${1 / (spriteGroup.card!.initialHeight! / spriteGroup.card!.finalHeight!)}`
+  );
+
+  // spriteGroup.card!.setupAnimation('size', {
+  //   duration,
+  //   behavior: new LinearBehavior(),
+  //   startWidth: -spriteGroup.card!.finalBounds!.element.width,
+  //   startHeight: -spriteGroup.card!.finalBounds!.element.height,
+  //   endWidth: -spriteGroup.card!.finalBounds!.element.width,
+  //   endHeight: -spriteGroup.card!.finalBounds!.element.height,
+  // });
+
+  await runAnimations([...images, spriteGroup.card!]);
+
+  spriteGroup.card!.element.style.transform = '';
+  spriteGroup.card!.element.style.transformOrigin = '';
+  spriteGroup.card!.element.style.setProperty(
+    '--animation-scale-inversion-y',
+    ''
+  );
+
+  if (placeholder) {
+    placeholder.element.style.zIndex = '0';
+  }
+
+  // Card sprite proper resizes and moves together with title
+  spriteGroup.card?.setupAnimation('position', {
+    duration,
+    behavior,
+  });
+  spriteGroup.card?.setupAnimation('size', {
+    duration,
+    behavior,
+    startWidth: spriteGroup.card.counterpart!.initialWidth,
+  });
+
+  // spriteGroup.card!.element.style.zIndex = '2';
+  await runAnimations([spriteGroup.card!]);
+
+  // images.forEach((image) => {
+  //   image.setupAnimation('position', {
+  //     startX: -image.boundsDelta!.x + spriteGroup.card!.boundsDelta!.x + 5,
+  //     startY: -image.boundsDelta!.y + spriteGroup.card!.boundsDelta!.y + 5,
+  //   });
+  // });
+
+  // await runAnimations(images);
+}
+
+export async function simple(sprite: Sprite) {
+  let behavior = new LinearBehavior();
+  let duration = DEFAULT_DURATION;
+  // position has to come before size so that we don't mess up movement w scale
+  sprite.setupAnimation('position', {
+    duration,
+    behavior,
+  });
+  sprite.setupAnimation('size', {
+    duration,
+    behavior,
+  });
+  await runAnimations([sprite!]);
+}
+
+function expandedToMin() {}
+function minToExpanded() {}
+
+function justMove() {}
+function placeholderIn() {}
+function placeholderOut() {}
+// function imageLayoutBeforeMove(){}
+// function imageMoveBeforeLayout(){}
+// function textToForm(){}
+// function formToText(){}
+
+function getCardId(sprite: Sprite) {
+  let role = sprite.role;
+  let id = sprite.id;
+
+  if (role === 'image') {
+    return id?.replace(/-image-\d$/, '');
+  } else {
+    return id?.replace('-' + role, '');
+  }
+}
+
+function getStateChange(
+  cardSprite: Sprite
+): 'STATIC' | 'MAX -> EXPANDED' | 'EXPANDED -> MAX' | 'MAX -> MIN' {
+  let counterpart = cardSprite.counterpart;
+
+  if (!counterpart) {
+    return 'STATIC';
+  }
+
+  let counterpartState = [
+    CARD_STATES.EXPANDED,
+    CARD_STATES.MAX,
+    CARD_STATES.MIN,
+  ].find((v) => counterpart?.element.className.includes(v));
+  let spriteState = [
+    CARD_STATES.EXPANDED,
+    CARD_STATES.MAX,
+    CARD_STATES.MIN,
+  ].find((v) => cardSprite.element.className.includes(v));
+  if (
+    spriteState === counterpartState ||
+    (spriteState !== 'MAX' && counterpartState !== 'MAX')
+  ) {
+    return 'STATIC';
+  } else {
+    return `${counterpartState} -> ${spriteState}` as
+      | 'MAX -> EXPANDED'
+      | 'EXPANDED -> MAX'
+      | 'MAX -> MIN';
+  }
+}
+
+export function groupSprites(changeset: Changeset) {
+  let groupsOfSprites: Record<string, SpriteGroup> = {};
+
+  for (let sprite of changeset.keptSprites) {
+    let id = getCardId(sprite);
+    groupsOfSprites[id as string] ??= {
+      state: 'STATIC',
+      card: null,
+      mainCardContent: null,
+      title: null,
+      placeholder: null,
+      keptContent: new Set(),
+      insertedContent: new Set(),
+      removedContent: new Set(),
+    };
+    let group = groupsOfSprites[id as string]!;
+    if (sprite.role === 'card') {
+      group.card = sprite;
+      group.state = getStateChange(sprite);
+    } else if (sprite.role === 'card-content') {
+      group.mainCardContent = sprite;
+    } else if (sprite.role === 'title') {
+      group.title = sprite;
+    } else if (sprite.role === 'placeholder') {
+      group.placeholder = sprite;
+    } else {
+      group.keptContent.add(sprite);
+    }
+  }
+  for (let sprite of changeset.insertedSprites) {
+    let id = getCardId(sprite);
+    groupsOfSprites[id as string] ??= {
+      state: 'STATIC',
+      card: null,
+      mainCardContent: null,
+      title: null,
+      placeholder: null,
+      keptContent: new Set(),
+      insertedContent: new Set(),
+      removedContent: new Set(),
+    };
+    let group = groupsOfSprites[id as string]!;
+    if (sprite.role === 'card') {
+      group.card = sprite;
+      group.state = 'INSERTED';
+    } else if (sprite.role === 'card-content') {
+      group.mainCardContent = sprite;
+    } else if (sprite.role === 'title') {
+      group.title = sprite;
+    } else if (sprite.role === 'placeholder') {
+      group.placeholder = sprite;
+    } else {
+      group.insertedContent.add(sprite);
+    }
+  }
+
+  for (let sprite of changeset.removedSprites) {
+    let id = getCardId(sprite);
+    groupsOfSprites[id as string] ??= {
+      state: 'STATIC',
+      card: null,
+      mainCardContent: null,
+      title: null,
+      placeholder: null,
+      keptContent: new Set(),
+      insertedContent: new Set(),
+      removedContent: new Set(),
+    };
+    let group = groupsOfSprites[id as string]!;
+    if (sprite.role === 'card') {
+      group.card = sprite;
+      group.state = 'REMOVED';
+    } else if (sprite.role === 'card-content') {
+      group.mainCardContent = sprite;
+    } else if (sprite.role === 'title') {
+      group.title = sprite;
+    } else if (sprite.role === 'placeholder') {
+      group.placeholder = sprite;
+    } else {
+      group.removedContent.add(sprite);
+    }
+  }
+
+  return groupsOfSprites;
+}
