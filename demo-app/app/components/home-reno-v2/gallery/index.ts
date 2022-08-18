f3363c8314853f58d63aa958d9c25d30d1139319
Aierie <39579264+Aierie@users.noreply.github.com>
2022-08-18 12:21:30 +0800

    add "inside" animation contexts

---
+++ demo-app/app/components/home-reno-v2/gallery/index.ts
@@ -0,0 +1,22 @@
+import Component from '@glimmer/component';
+import { SpriteType } from 'animations-experiment/models/sprite';
+import { Changeset } from 'animations-experiment/models/changeset';
+import { simple } from '../transitions';
+
+export default class Gallery extends Component {
+  async transition(changeset: Changeset) {
+    let card = changeset.spriteFor({
+      role: 'card',
+    });
+    if (card) await simple(card);
+
+    let placeholder = changeset.spriteFor({
+      role: 'placeholder',
+    });
+    if (placeholder?.type === SpriteType.Removed) {
+      placeholder.lockStyles();
+      changeset.context.appendOrphan(placeholder);
+      await new Promise((res) => setTimeout(res, 1000));
+    }
+  }
+}
