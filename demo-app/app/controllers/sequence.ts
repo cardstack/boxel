05308381b5e9d8091daf33d1033fab83ce5f3bf9
nickschot <nickschot@gmail.com>
2022-09-16 17:06:07 +0200

    Add Sequence demo

---
+++ demo-app/app/controllers/sequence.ts
@@ -0,0 +1,67 @@
+import Controller from '@ember/controller';
+import { Changeset } from 'animations-experiment/models/changeset';
+import LinearBehavior from 'animations-experiment/behaviors/linear';
+import SpringBehavior from 'animations-experiment/behaviors/spring';
+import { AnimationDefinition } from 'animations-experiment/models/transition-runner';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class Sequence extends Controller {
+  @tracked leftPosition = '0px';
+
+  @action
+  toggleMove() {
+    if (this.leftPosition !== '0px') {
+      this.leftPosition = '0px';
+    } else {
+      this.leftPosition = '500px';
+    }
+  }
+
+  transition(changeset: Changeset): AnimationDefinition {
+    let { keptSprites } = changeset;
+
+    return {
+      timeline: {
+        sequence: [
+          {
+            sprites: keptSprites,
+            properties: {
+              opacity: { to: 0 },
+            },
+            timing: {
+              behavior: new LinearBehavior(),
+              duration: 300,
+            },
+          },
+          {
+            sprites: keptSprites,
+            properties: {
+              opacity: { from: 0, to: 1 },
+            },
+            timing: {
+              behavior: new LinearBehavior(),
+              duration: 300,
+            },
+          },
+          {
+            sprites: keptSprites,
+            properties: {
+              position: {},
+            },
+            timing: {
+              behavior: new SpringBehavior(),
+            },
+          },
+        ],
+      },
+    } as unknown as AnimationDefinition;
+  }
+}
+
+// DO NOT DELETE: this is how TypeScript knows how to look up your controllers.
+declare module '@ember/controller' {
+  interface Registry {
+    sequence: Sequence;
+  }
+}
