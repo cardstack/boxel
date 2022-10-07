c70bea62d7182160b61815a70ca641e74aa4b73a
Aierie <39579264+Aierie@users.noreply.github.com>
2022-10-07 19:53:01 +0800

    TransitionRunner runs additional AnimationDefinitions from Rules

--- addon/addon/models/transition-runner.ts
+++ addon/addon/models/transition-runner.ts
@@ -1,10 +1,6 @@
 import { task } from 'ember-concurrency';
 import { Changeset } from '../models/changeset';
-import Sprite, {
-  MotionOptions,
-  MotionProperty,
-  SpriteType,
-} from '../models/sprite';
+import Sprite, { MotionOptions, MotionProperty } from '../models/sprite';
 import { assert } from '@ember/debug';
 import { IContext } from './sprite-tree';
 import { SpriteAnimation } from 'animations-experiment/models/sprite-animation';
@@ -47,16 +43,21 @@ export default class TransitionRunner {
     this.animationContext = animationContext;
   }
 
-  setupAnimations(definition: AnimationDefinition): SpriteAnimation[] {
-    let timeline = definition.timeline;
-    assert('No timeline present in AnimationDefinition', Boolean(timeline));
-    assert(
-      'Timeline can have either a sequence or a parallel definition, not both',
-      !(timeline.sequence && timeline.parallel)
-    );
-
-    let orchestrationMatrix = OrchestrationMatrix.fromTimeline(timeline);
+  setupAnimations(definitions: AnimationDefinition[]): SpriteAnimation[] {
     let result: SpriteAnimation[] = [];
+
+    let orchestrationMatrix = new OrchestrationMatrix();
+    for (let definition of definitions) {
+      let timeline = definition.timeline;
+      assert('No timeline present in AnimationDefinition', Boolean(timeline));
+      assert(
+        'Timeline can have either a sequence or a parallel definition, not both',
+        !(timeline.sequence && timeline.parallel)
+      );
+
+      orchestrationMatrix.add(0, OrchestrationMatrix.fromTimeline(timeline));
+    }
+
     for (let [sprite, keyframes] of orchestrationMatrix
       .getKeyframes((prev, incoming) => {
         return Object.assign({}, prev, ...incoming);
@@ -76,6 +77,7 @@ export default class TransitionRunner {
 
       result.push(animation);
     }
+
     return result;
   }
 
@@ -90,58 +92,44 @@ export default class TransitionRunner {
     //playUnrelatedAnimations();
 
     if (animationContext.shouldAnimate()) {
-      this.logChangeset(changeset, animationContext); // For debugging
-      let animationDefinition = animationContext.args.use?.(changeset) as
-        | AnimationDefinition
-        | undefined;
+      try {
+        let animationDefinitions = [];
+        if (animationContext.args.use instanceof Function) {
+          // Note: some use() calls are async and return a Promise.
+          // yielding this is currently incorrect (I think), because we probably don't want to
+          // allow delaying animations by deferring the returning of an AnimationDefinition
+          // TODO: Convert all current demos to use AnimationDefinition
+          let animationDefinition: AnimationDefinition | undefined =
+            (yield animationContext.args.use?.(changeset)) as
+              | AnimationDefinition
+              | undefined;
 
-      if (animationDefinition) {
-        // TODO: compile animation
-        let animations = this.setupAnimations(animationDefinition);
-        let promises = animations.map((animation) => animation.finished);
-
-        animations.forEach((a) => {
-          a.play();
-        });
-
-        try {
-          yield Promise.resolve(Promise.all(promises));
-        } catch (error) {
-          console.error(error);
-          throw error;
+          if (animationDefinition) {
+            animationDefinitions.push(animationDefinition);
+          }
         }
+
+        animationDefinitions = [
+          ...changeset.animationDefinitions,
+          ...animationDefinitions,
+        ];
+
+        if (animationDefinitions.length) {
+          let animations = this.setupAnimations(animationDefinitions);
+          let promises = animations.map((animation) => animation.finished);
+
+          animations.forEach((a) => {
+            a.play();
+          });
+
+          yield Promise.all(promises);
+        }
+      } catch (error) {
+        console.error(error);
+        throw error;
       }
+
       animationContext.clearOrphans();
     }
-  }
-
-  private logChangeset(changeset: Changeset, animationContext: IContext): void {
-    let contextId = animationContext.args.id;
-    function row(type: SpriteType, sprite: Sprite) {
-      return {
-        intent: changeset.intent,
-        context: contextId,
-        type,
-        spriteRole: sprite.role,
-        spriteId: sprite.id,
-        initialBounds: sprite.initialBounds
-          ? JSON.stringify(sprite.initialBounds)
-          : null,
-        finalBounds: sprite.finalBounds
-          ? JSON.stringify(sprite.finalBounds)
-          : null,
-      };
-    }
-    let tableRows = [];
-    for (let type of [
-      SpriteType.Inserted,
-      SpriteType.Removed,
-      SpriteType.Kept,
-    ]) {
-      for (let sprite of changeset.spritesFor({ type })) {
-        tableRows.push(row(type, sprite));
-      }
-    }
-    console.table(tableRows);
   }
 }
