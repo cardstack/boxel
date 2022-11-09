15d46cb09b84a9039864349daf7d3249da7b9657
Aierie <michael.khor@cardstack.com>
2022-11-10 01:19:27 +0800

    enforce id at modifier level

--- addon/addon/modifiers/sprite.ts
+++ addon/addon/modifiers/sprite.ts
@@ -1,19 +1,11 @@
 import Modifier from 'ember-modifier';
-import {
-  getDocumentPosition,
-  copyComputedStyle,
-  CopiedCSS,
-  DocumentPositionArgs,
-} from '../utils/measurement';
-import { assert } from '@ember/debug';
 import { inject as service } from '@ember/service';
-import { once } from '@ember/runloop';
 import AnimationsService from '../services/animations';
 
 interface SpriteModifierArgs {
   Positional: unknown[];
   Named: {
-    id?: string;
+    id: string;
     role?: string;
   };
 }
@@ -24,42 +16,15 @@ export interface SpriteModifierSignature {
 }
 
 export default class SpriteModifier extends Modifier<SpriteModifierSignature> {
-  id: string | null = null;
+  id!: string;
   role: string | null = null;
-  lastBounds: DOMRect | undefined;
-  currentBounds: DOMRect | undefined;
-  lastComputedStyle: CopiedCSS | undefined;
-  currentComputedStyle: CopiedCSS | undefined;
-
-  alreadyTracked = false;
 
   @service declare animations: AnimationsService;
 
   didReceiveArguments(): void {
-    this.id = this.args.named.id ?? null;
+    this.id = this.args.named.id;
     this.role = this.args.named.role ?? null;
     this.animations.registerSpriteModifier(this);
-    this.captureSnapshot();
-  }
-
-  captureSnapshot(opts: Partial<DocumentPositionArgs> = {}): void {
-    if (!this.alreadyTracked) {
-      let { element } = this;
-      assert(
-        'sprite modifier can only be installed on HTML elements',
-        element instanceof HTMLElement
-      );
-      this.lastBounds = this.currentBounds;
-      this.lastComputedStyle = this.currentComputedStyle;
-      this.currentBounds = getDocumentPosition(element, opts);
-      this.currentComputedStyle = copyComputedStyle(element);
-      this.alreadyTracked = true;
-    }
-    once(this, this.clearTrackedPosition);
-  }
-
-  clearTrackedPosition(): void {
-    this.alreadyTracked = false;
   }
 
   willRemove(): void {
