1385df9fbc689e1bfe8700dda22a252796515bcc
Aierie <michael.khor@cardstack.com>
2022-11-11 20:43:00 +0800

    DOMRefNode tests

---
+++ test-app/tests/unit/models/dom-ref-test.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+// import { DOMRefNode, addToDOMRefTrees } from '@cardstack/boxel-motion/models/dom-ref';
+import { module, test } from 'qunit';
+
+module.skip('Unit | DOMRefNode', function () {
+  test('it can add to trees, preserving DOM hierarchy', function () {});
+  test('it can delete a node', function () {});
+});
