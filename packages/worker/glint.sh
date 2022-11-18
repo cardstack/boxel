9e324fb3cd78b193142d2a890256bb5357d91429
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2022-11-18 13:59:00 -0500

    update glint wrapper to not exit with error status on deprecations

--- packages/worker/glint.sh
+++ packages/worker/glint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 # we use this to ignore all the DOM errors. add any new DOM errors to this regex
-glint_output=$(pnpm glint 2>&1 | perl -p0e "s/[^\n]*Cannot find name '(HTMLElement|ShadowRoot|HTMLInputElement|CSSStyleSheet|CSSStyleRule|Node|DocumentOrShadowRoot)'[^\n]*[\n]*[^\n]*[\n]*[^\n]*[\n]*//smg")
-echo "$glint_output"
+maybe_deprecations=$(pnpm glint 2>&1 | perl -p0e "s/[^\n]*Cannot find name '(HTMLElement|ShadowRoot|HTMLInputElement|CSSStyleSheet|CSSStyleRule|Node|DocumentOrShadowRoot)'[^\n]*[\n]*[^\n]*[\n]*[^\n]*[\n]*//smg")
+glint_output=$(echo "$maybe_deprecations" | perl -p0e "s/DeprecationWarning:[^\n]*[\n]*//smg")
+echo "$maybe_deprecations"
 if [ ${#glint_output} -gt 0 ]; then
   exit 1
 else
