bc2670de22a11f1e2207684d91aca6466a2456b6
Burcu Noyan <burcunoyan@gmail.com>
2022-12-16 11:30:25 -0500

    Resolve type error

--- packages/boxel-ui/addon/helpers/element.ts
+++ packages/boxel-ui/addon/helpers/element.ts
@@ -10,15 +10,9 @@ interface Signature<T extends keyof HTMLElementTagNameMap> {
 export default function element<T extends keyof HTMLElementTagNameMap>(
   tagName: T | undefined
 ): ComponentLike<Signature<T>> {
-  let tag = tagName ? tagName : ('div' as T);
-  if (typeof tag !== 'string') {
-    throw new Error(
-      'The `element` helper only accepts strings as its first argument'
-    );
-  }
   return ensureSafeComponent(
-    class DynamicElement extends EmberComponent {
-      tagName = tag;
+    class DynamicElement extends EmberComponent<Signature<T>> {
+      tagName = (tagName ?? ('div' as T)) as string;
     },
     undefined
   ) as ComponentLike<Signature<T>>;
