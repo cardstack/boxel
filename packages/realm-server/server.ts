15c6126e68d085b66f99d0edc4212ebe585013d0
Buck Doyle <buck.doyle@cardstack.com>
2023-01-20 11:25:15 -0600

    Add version indicator

--- packages/realm-server/server.ts
+++ packages/realm-server/server.ts
@@ -13,7 +13,7 @@ export interface RealmConfig {
 export function createRealmServer(realms: Realm[]) {
   detectRealmCollision(realms);
 
-  console.log(`${new Date()} starting server`);
+  console.log(`${new Date()} starting server 1124`);
 
   let server = http.createServer(async (req, res) => {
     console.log(new Date());
