e4205739b0e2028567c0d0a4aadb5803680c03ad
Buck Doyle <buck.doyle@cardstack.com>
2023-01-20 16:37:08 -0600

    Update logged time

--- packages/realm-server/server.ts
+++ packages/realm-server/server.ts
@@ -13,7 +13,7 @@ export interface RealmConfig {
 export function createRealmServer(realms: Realm[]) {
   detectRealmCollision(realms);
 
-  console.log(`${new Date()} starting server 1437`);
+  console.log(`${new Date()} starting server 1636`);
 
   let server = http.createServer(async (req, res) => {
     console.log(new Date());
