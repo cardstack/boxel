2f8ba4a342b92b57f8d87b6df3c67579ef8169ce
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2023-02-07 11:23:23 -0500

    getting server tests passing

--- packages/runtime-common/search-index.ts
+++ packages/runtime-common/search-index.ts
@@ -197,6 +197,11 @@ export class SearchIndex {
     return this.#index;
   }
 
+  // TODO remove this
+  get index() {
+    return this.#index;
+  }
+
   async run() {
     await this.setupRunner(async () => {
       if (!this.#fromScratch) {
