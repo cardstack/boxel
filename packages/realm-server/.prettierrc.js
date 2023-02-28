6e7fdb3f575ce583cfbdf7b52997f45d03e7e786
Buck Doyle <buck.doyle@cardstack.com>
2023-02-27 19:46:55 -0600

    Add Glint Prettier autofixes for realm-server

---
+++ packages/realm-server/.prettierrc.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  overrides: [
+    {
+      files: "*.gts",
+      options: {
+        parser: "ember-template-tag",
+      },
+    },
+  ],
+  plugins: ["prettier-plugin-ember-template-tag"],
+};
