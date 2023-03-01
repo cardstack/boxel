c477641db78c541f3cfa663c1aaff88f48578cf1
Buck Doyle <buck.doyle@cardstack.com>
2023-02-28 18:10:03 -0600

    Remove inapplicable note

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:prettier/recommended',
   ],
   rules: {
     // this doesn't work well with the monorepo. Typescript already complains if you try to import something that's not found
