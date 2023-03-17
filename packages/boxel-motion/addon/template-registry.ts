e7561a8c7aae1d1a7b17c7579370eee6de380f6d
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2023-03-17 15:49:28 -0400

    disabled standalone template checking for animation lib

--- packages/boxel-motion/addon/template-registry.ts
+++
@@ -1,7 +0,0 @@
-import registerContentModifier from './modifiers/register-context';
-import registerContextOrphansEl from './modifiers/register-context-orphans-el';
-
-export default interface Registry {
-  'register-context-orphans-el': typeof registerContextOrphansEl;
-  'register-context': typeof registerContentModifier;
-}
