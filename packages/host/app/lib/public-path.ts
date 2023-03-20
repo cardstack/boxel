fd913b744af473834a76133252bfb7974d4672c1
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2023-03-20 15:14:58 -0400

    remove magic constants

--- packages/host/app/lib/public-path.ts
+++ packages/host/app/lib/public-path.ts
@@ -1,6 +1,8 @@
 import config from '@cardstack/host/config/environment';
+import { baseRealm, assetsDir } from '@cardstack/runtime-common';
 
 const { isBaseRealmHosting } = config;
+let assetPathname = new URL(`${baseRealm.url}${assetsDir}`).pathname;
 
 // @ts-expect-error this is consumed by webpack to set the public asset path at runtime
-__webpack_public_path__ = isBaseRealmHosting ? '/base/__boxel/' : '/';
+__webpack_public_path__ = isBaseRealmHosting ? assetPathname : '/';
