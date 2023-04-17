9571071b1e14378ad0b54c3187c109623d865a5d
Luke Melia <luke@lukemelia.com>
2023-04-17 15:49:22 -0400

    When local realm is not connected, avoid throwing an error by trying to fetch a card

---
+++ packages/host/app/components/local-realm-not-connected.gts
@@ -0,0 +1,7 @@
+import Component from '@glimmer/component';
+
+export default class LocalRealmNotConnectedComponent extends Component {
+  <template>
+    LocalRealmNotConnectedComponent: Local realm not connected.
+  </template>
+}
--- packages/host/app/routes/card.ts
+++ packages/host/app/routes/card.ts
@@ -5,14 +5,17 @@ import ENV from '@cardstack/host/config/environment';
 import { parse } from 'qs';
 import type CardService from '../services/card-service';
 import type RouterService from '@ember/routing/router-service';
+import type LocalRealmService from '../services/local-realm';
+import LocalRealmNotConnectedComponent from '@cardstack/host/components/local-realm-not-connected';
 
-const { ownRealmURL } = ENV;
+const { ownRealmURL, isLocalRealm } = ENV;
 const rootPath = new URL(ownRealmURL).pathname.replace(/^\//, '');
 
 export default class RenderCard extends Route<
   ComponentLike<{ Args: {}; Blocks: {} }>
 > {
   @service declare cardService: CardService;
+  @service declare localRealm: LocalRealmService;
   @service declare router: RouterService;
 
   beforeModel(transition: any) {
@@ -38,11 +41,15 @@ export default class RenderCard extends Route<
     let url = path
       ? new URL(`/${path}`, ownRealmURL)
       : new URL('./', ownRealmURL);
+    await this.localRealm.startedUp;
     try {
       let instance = await this.cardService.loadModel(url);
       return instance.constructor.getComponent(instance, 'isolated');
     } catch (e) {
       (e as any).failureLoadingIndexCard = url.href === ownRealmURL;
+      if (isLocalRealm && !this.localRealm.isAvailable) {
+        return LocalRealmNotConnectedComponent;
+      }
       throw e;
     }
   }
