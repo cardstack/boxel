db985d5bb73497211ef229e6266a86c079b8d35a
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2023-05-02 13:35:54 -0400

    polyfilling node stream

--- packages/host/ember-cli-build.js
+++ packages/host/ember-cli-build.js
@@ -51,6 +51,7 @@ module.exports = function (defaults) {
               fs: false,
               path: require.resolve('path-browserify'),
               crypto: require.resolve('crypto-browserify'),
+              stream: require.resolve('stream-browserify'),
             },
           },
           node: {
