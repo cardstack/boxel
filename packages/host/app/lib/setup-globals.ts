2b60219ee7358e968b0ecac9f27d385b37355ffb
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2023-05-08 14:58:25 -0400

    update olm version

--- packages/host/app/lib/setup-globals.ts
+++ packages/host/app/lib/setup-globals.ts
@@ -5,4 +5,4 @@ import { Buffer } from 'buffer';
 (globalThis as any)._logDefinitions =
   (globalThis as any)._logDefinitions ?? makeLogDefinitions(ENV.logLevels);
 
-globalThis.Buffer = Buffer;
+(globalThis as any).Buffer = Buffer;
