5530db260d42701b12849b7a74810b6648284b57
Buck Doyle <buck.doyle@cardstack.com>
2023-05-15 15:12:57 -0400

    Update boxel-ui to 4.12.1

--- packages/boxel-ui/.eslintrc.js
+++ packages/boxel-ui/.eslintrc.js
@@ -4,10 +4,13 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true,
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      ],
     },
   },
   plugins: ['ember', '@typescript-eslint'],
@@ -36,6 +39,7 @@ module.exports = {
       files: [
         './.eslintrc.js',
         './.prettierrc.js',
+        './.stylelintrc.js',
         './.template-lintrc.js',
         './ember-cli-build.js',
         './index.js',
@@ -51,10 +55,9 @@ module.exports = {
         browser: false,
         node: true,
       },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
+      extends: ['plugin:n/recommended'],
       rules: {
-        'node/no-unpublished-require': 'off',
+        'n/no-unpublished-require': 'off',
         '@typescript-eslint/no-var-requires': 'off',
       },
     },
---
+++ packages/boxel-ui/README.md
@@ -0,0 +1,32 @@
+# @cardstack/boxel-ui
+
+[Short description of the addon.]
+
+
+## Compatibility
+
+* Ember.js v4.4 or above
+* Ember CLI v4.4 or above
+* Node.js v14 or above
+
+
+## Installation
+
+```
+ember install @cardstack/boxel-ui
+```
+
+
+## Usage
+
+[Longer description of how to use the addon in apps.]
+
+
+## Contributing
+
+See the [Contributing](CONTRIBUTING.md) guide for details.
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
