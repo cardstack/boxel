29bee71504652ccc38252a24413168a2bd502a74
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2023-05-17 14:03:29 -0400

    remove redundant tests

--- packages/matrix/tests/encrypted-messages.spec.ts
+++ packages/matrix/tests/encrypted-messages.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 import {
   synapseStart,
   synapseStop,
@@ -13,8 +13,6 @@ import {
   assertMessages,
   sendMessage,
   joinRoom,
-  scrollToTopOfMessages,
-  leaveRoom,
 } from '../helpers';
 
 test.describe('Encrypted Room messages', () => {
@@ -35,18 +33,9 @@ test.describe('Encrypted Room messages', () => {
     await createRoom(page, { name: 'Room 2', encrypted: true });
     await openRoom(page, 'Room 1');
 
-    await expect(page.locator('[data-test-timeline-start]')).toHaveCount(1);
-    await expect(page.locator('[data-test-no-messages]')).toHaveCount(1);
-    await expect(page.locator('[data-test-message-field]')).toHaveValue('');
     await assertMessages(page, []);
+    await sendMessage(page, 'Message 1');
 
-    await expect(page.locator('[data-test-send-message-btn]')).toBeDisabled();
-    await page.locator('[data-test-message-field]').fill('Message 1');
-    await expect(page.locator('[data-test-send-message-btn]')).toBeEnabled();
-    await page.locator('[data-test-send-message-btn]').click();
-
-    await expect(page.locator('[data-test-message-field]')).toHaveValue('');
-    await expect(page.locator('[data-test-no-messages]')).toHaveCount(0);
     await assertMessages(page, [{ from: 'user1', message: 'Message 1' }]);
 
     await page.reload();
@@ -55,13 +44,6 @@ test.describe('Encrypted Room messages', () => {
 
     await logout(page);
     await login(page, 'user1', 'pass');
-    await openRoom(page, 'Room 1');
-    await assertMessages(page, [{ from: 'user1', message: 'Message 1' }]);
-
-    // make sure that room state doesn't leak
-    await openRoom(page, 'Room 2');
-    await assertMessages(page, []);
-
     await openRoom(page, 'Room 1');
     await assertMessages(page, [{ from: 'user1', message: 'Message 1' }]);
   });
@@ -101,118 +83,6 @@ test.describe('Encrypted Room messages', () => {
     await assertMessages(page, [
       { from: 'user1', message: 'first message' },
       { from: 'user2', message: 'second message' },
-    ]);
-  });
-
-  test(`it can paginate back to beginning of timeline for timelines that truncated`, async ({
-    page,
-  }) => {
-    // generally the matrix server paginates after 10 messages
-    const totalMessageCount = 20;
-
-    await login(page, 'user1', 'pass');
-    await createRoom(page, {
-      name: 'Room 1',
-      invites: ['user2'],
-      encrypted: true,
-    });
-    await openRoom(page, 'Room 1');
-
-    for (let i = 1; i <= totalMessageCount; i++) {
-      await sendMessage(page, `message ${i}`);
-      await page.waitForTimeout(100);
-    }
-    await logout(page);
-
-    await login(page, 'user2', 'pass');
-    await joinRoom(page, 'Room 1');
-    await openRoom(page, 'Room 1');
-
-    let displayedMessageCount = await page
-      .locator('[data-test-message-idx]')
-      .count();
-    expect(displayedMessageCount).toBeGreaterThan(0);
-    expect(displayedMessageCount).toBeLessThan(totalMessageCount);
-    await expect(
-      page.locator('[data-test-timeline-start]'),
-      'the beginning of the timeline is not displayed'
-    ).toHaveCount(0);
-    await expect(
-      page.getByText(`message ${totalMessageCount}`),
-      'the most recent message is displayed'
-    ).toHaveCount(1);
-
-    await scrollToTopOfMessages(page);
-    await expect(page.locator('[data-test-message-idx]')).toHaveCount(
-      totalMessageCount
-    );
-    expect(displayedMessageCount).toBeLessThan(totalMessageCount);
-    await expect(
-      page.locator('[data-test-timeline-start]'),
-      'the beginning of the timeline is displayed'
-    ).toHaveCount(1);
-  });
-
-  test(`it can send a markdown message`, async ({ page }) => {
-    await login(page, 'user1', 'pass');
-    await createRoom(page, {
-      name: 'Room 1',
-      encrypted: true,
-    });
-    await openRoom(page, 'Room 1');
-    await sendMessage(page, 'message with _style_');
-    await assertMessages(page, [
-      {
-        from: 'user1',
-        message: 'message with style',
-      },
-    ]);
-    await expect(
-      page.locator(`[data-test-message-idx="0"] .boxel-message__content em`)
-    ).toContainText('style');
-  });
-
-  test(`it can create a room specific pending message`, async ({ page }) => {
-    await login(page, 'user1', 'pass');
-    await createRoom(page, { name: 'Room 1', encrypted: true });
-    await createRoom(page, { name: 'Room 2', encrypted: true });
-    await openRoom(page, 'Room 1');
-
-    await page.locator('[data-test-message-field]').fill('room 1 message');
-    await openRoom(page, 'Room 2');
-    await expect(page.locator('[data-test-message-field]')).toHaveValue('');
-    await page.locator('[data-test-message-field]').fill('room 2 message');
-    await openRoom(page, 'Room 1');
-    await expect(page.locator('[data-test-message-field]')).toHaveValue(
-      'room 1 message'
-    );
-    await openRoom(page, 'Room 2');
-    await expect(page.locator('[data-test-message-field]')).toHaveValue(
-      'room 2 message'
-    );
-  });
-
-  test('message sender has left room', async ({ page }) => {
-    await login(page, 'user1', 'pass');
-    await createRoom(page, {
-      name: 'Room 1',
-      invites: ['user2'],
-      encrypted: true,
-    });
-    await logout(page);
-
-    await login(page, 'user2', 'pass');
-    await joinRoom(page, 'Room 1');
-    await openRoom(page, 'Room 1');
-    await sendMessage(page, 'first message');
-    await leaveRoom(page, 'Room 1');
-
-    await logout(page);
-    await login(page, 'user1', 'pass');
-    await openRoom(page, 'Room 1');
-
-    await assertMessages(page, [
-      { from: 'user2 (left room)', message: 'first message' },
     ]);
   });
 });
