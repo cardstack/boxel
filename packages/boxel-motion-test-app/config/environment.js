b98c966e21763d393ed5a904f5042a69de33c531
Buck Doyle <buck.doyle@cardstack.com>
2023-05-18 11:59:11 -0400

    Fix prefer-let lint errors

--- packages/boxel-motion-test-app/config/environment.js
+++ packages/boxel-motion-test-app/config/environment.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (environment) {
-  const ENV = {
+  let ENV = {
     modulePrefix: 'boxel-motion-test-app',
     environment,
     rootURL: '/',
--- packages/boxel-motion-test-app/ember-cli-build.js
+++ packages/boxel-motion-test-app/ember-cli-build.js
@@ -4,7 +4,7 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
+  let app = new EmberApp(defaults, {
     // Add options here
   });
 
