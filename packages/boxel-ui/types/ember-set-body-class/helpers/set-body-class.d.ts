dbfab7aca47e40d7e6021e59cb0ab180e14144d0
Buck Doyle <buck.doyle@cardstack.com>
2023-05-26 14:42:31 -0400

    Fix type for set-body-class helper

--- packages/boxel-ui/types/ember-set-body-class/helpers/set-body-class.d.ts
+++
@@ -1,8 +0,0 @@
-import Helper from '@ember/component/helper';
-
-interface SetBodyClassHelperSignature {
-  Args: { Positional: [string] };
-  Return: never;
-}
-
-export default class SetBodyClassHelper extends Helper<SetBodyClassHelperSignature> {}
