2ae559650f4182946a745a8264aba9d8c639ffdb
tintinthong <justinthong93@gmail.com>
2023-06-01 15:01:43 +0800

    load shim

--- packages/realm-server/lib/externals.ts
+++ packages/realm-server/lib/externals.ts
@@ -5,6 +5,7 @@ import * as boxelUI from '@cardstack/boxel-ui';
 import * as flat from 'flat';
 import * as lodash from 'lodash';
 import * as dateFns from 'date-fns';
+import * as cardpay from '@cardstack/cardpay-sdk';
 
 export function shimExternals(loader: Loader = Loader.getLoader()) {
   loader.shimModule('@cardstack/runtime-common', runtime);
@@ -87,6 +88,7 @@ export function shimExternals(loader: Loader = Loader.getLoader()) {
   });
   loader.shimModule('lodash', lodash);
   loader.shimModule('date-fns', dateFns);
+  loader.shimModule('@cardstack/cardpay-sdk', cardpay);
 }
 
 shimExternals();
