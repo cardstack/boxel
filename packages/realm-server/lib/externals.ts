ed063b747a3a3aca98dd2aeaf836da66fc0c415b
tintinthong <justinthong93@gmail.com>
2023-06-05 15:38:08 +0800

    Shimming realm server doesn't require literal package

--- packages/realm-server/lib/externals.ts
+++ packages/realm-server/lib/externals.ts
@@ -5,7 +5,6 @@ import * as boxelUI from '@cardstack/boxel-ui';
 import * as flat from 'flat';
 import * as lodash from 'lodash';
 import * as dateFns from 'date-fns';
-import * as cardpay from '@cardstack/cardpay-sdk';
 
 export function shimExternals(loader: Loader = Loader.getLoader()) {
   loader.shimModule('@cardstack/runtime-common', runtime);
@@ -88,7 +87,7 @@ export function shimExternals(loader: Loader = Loader.getLoader()) {
   });
   loader.shimModule('lodash', lodash);
   loader.shimModule('date-fns', dateFns);
-  loader.shimModule('@cardstack/cardpay-sdk', cardpay);
+  loader.shimModule('@cardstack/cardpay-sdk', {});
 }
 
 shimExternals();
