ca915a9f74118a66095e3def2d639405b928712b
Matic Jurglič <matic@jurglic.si>
2023-06-12 13:40:19 +0200

    Change priorities in route pattern matching for card route

--- packages/host/app/router.ts
+++ packages/host/app/router.ts
@@ -13,6 +13,8 @@ Router.map(function () {
   this.route('freestyle', { path: '/_freestyle' });
   this.route('indexer', { path: '/indexer/:id' });
   this.route('acceptance-test-setup');
+  this.route('card', { path: '/*path' });
+
   if (!path || hostsOwnAssets) {
     this.route('chat', function () {
       this.route('register');
@@ -28,5 +30,4 @@ Router.map(function () {
     this.route('card', { path });
     this.route('code', { path: `${path}/code` });
   }
-  this.route('card', { path: '/*path' });
 });
