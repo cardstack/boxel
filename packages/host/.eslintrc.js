6857a8f294c8f9f201b5e7b43204b70e912ac991
Buck Doyle <buck.doyle@cardstack.com>
2023-06-28 11:28:42 -0500

    Add Percy file to lint exclusions

--- packages/host/.eslintrc.js
+++ packages/host/.eslintrc.js
@@ -42,6 +42,7 @@ module.exports = {
     {
       files: [
         './.eslintrc.js',
+        './.percy.js',
         './.prettierrc.js',
         './.stylelintrc.js',
         './.template-lintrc.js',
