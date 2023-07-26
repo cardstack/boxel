ca16c1f2e23e101a954410c139a3acb291ba1e68
Justin <justinthong93@gmail.com>
2023-07-26 12:20:35 +0800

    add core code for indefninte and pluarlize suggestion

--- packages/runtime-common/text-suggestion.ts
+++ packages/runtime-common/text-suggestion.ts
@@ -1,12 +1,18 @@
 import { isCardTypeFilter, isEveryFilter, type Filter } from './query';
+import * as a from 'indefinite';
 
 interface ChooseCardSuggestion {
   suggestion: string; // suggests a UI text
   depth: number;
 }
+
+interface TextOpts {
+  multipleSelection: boolean;
+}
 export function suggestCardChooserTitle(
   filter: Filter,
-  depth: number = 0 //lower the depth, higher the priority
+  depth: number = 0, //lower the depth, higher the priority
+  textOpts?: TextOpts
 ): ChooseCardSuggestion[] {
   let MAX_RECURSION_DEPTH = 2;
   if (filter === undefined || depth + 1 > MAX_RECURSION_DEPTH) {
@@ -16,17 +22,20 @@ export function suggestCardChooserTitle(
   //--base case--
   if ('on' in filter && filter.on !== undefined) {
     let cardRefName = (filter.on as { module: string; name: string }).name;
-    return [{ suggestion: `Choose a ${cardRefName} card`, depth }];
+    return [{ suggestion: titleText(cardRefName, 'card', textOpts), depth }];
   }
   if (isCardTypeFilter(filter)) {
     let cardRefName = (filter.type as { module: string; name: string }).name;
     if (cardRefName == 'Card') {
       suggestions.push({
-        suggestion: `Choose a ${cardRefName} instance`,
+        suggestion: titleText(cardRefName, 'instance', textOpts),
         depth,
       });
     } else {
-      suggestions.push({ suggestion: `Choose a ${cardRefName} card`, depth });
+      suggestions.push({
+        suggestion: titleText(cardRefName, 'card', textOpts),
+        depth,
+      });
     }
   }
   //--inductive case--
@@ -37,6 +46,21 @@ export function suggestCardChooserTitle(
     suggestions = [...suggestions, ...nestedSuggestions];
   }
   return suggestions;
+}
+
+type CardNoun = 'instance' | 'type' | 'card';
+
+function titleText(
+  cardRefName: string,
+  cardNoun: CardNoun,
+  textOpts?: TextOpts
+) {
+  let object = `${cardRefName} ${cardNoun}`;
+  if (textOpts?.multipleSelection) {
+    return `Choose one or more ${object}`;
+  } else {
+    return `Choose ${a(object)}`;
+  }
 }
 
 export function getSuggestionWithLowestDepth(
