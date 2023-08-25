bdd395c756aed9c9c456f0ca72f8daa3c07ca6ba
Ian Calvert <ianjcalvert@gmail.com>
2023-08-25 10:25:34 +0100

    Drop concurrency back to 1
    Just until we ensure the flakiness issues are solved, otherwise this exacerbates the issue.

--- packages/matrix/playwright.config.ts
+++ packages/matrix/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 4,
+  workers: 1,
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
