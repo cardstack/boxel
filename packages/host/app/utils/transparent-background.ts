01d909bb161be9303e82e029f25c396102ca267b
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2023-09-21 19:27:49 +0700

    test fixes

--- packages/host/app/utils/transparent-background.ts
+++ packages/host/app/utils/transparent-background.ts
@@ -3,7 +3,12 @@ import 'jimp';
 export async function hasTransparentBackground(
   imageURL: string,
 ): Promise<boolean> {
-  // @ts-ignore Jimp is exported as a global variable
-  let image = await Jimp.read(imageURL);
-  return image.hasAlpha();
+  try {
+    // @ts-ignore Jimp is exported as a global variable
+    let image = await Jimp.read(imageURL);
+    return image.hasAlpha();
+  } catch (e) {
+    console.error('failed to check image background', e);
+    return false;
+  }
 }
