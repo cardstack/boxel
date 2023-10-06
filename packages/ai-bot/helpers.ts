0d62d0ec5268ad36a5cc9d631329d7b76dc38aec
Ian Calvert <ianjcalvert@gmail.com>
2023-10-06 10:38:28 +0100

    Update prompt for a single response

--- packages/ai-bot/helpers.ts
+++ packages/ai-bot/helpers.ts
@@ -238,45 +238,56 @@ interface OpenAIPromptMessage {
   role: 'system' | 'user' | 'assistant' | 'function';
 }
 
-const MODIFY_SYSTEM_MESSAGE =
-  '\
-You are able to modify content according to user requests as well as answer questions for them. You may ask any followup questions you may need.\
-If a user may be requesting a change, respond politely but not ingratiatingly to the user. The more complex the request, the more you can explain what you\'re about to do.\
-\
-Along with the changes you want to make, you must include the card ID of the card being changed. The original card. \
-Return up to 3 options for the user to select from, exploring a range of things the user may want. If the request has only one sensible option or they ask for something very directly you don\'t need to return more than one. The format of your response should be\
-```\
-Explanatory text\
-Option 1: Description\
-{\
-  "id": "originalCardID",\
-  "patch": {\
-    ...\
-  }\
-}\
-\
-Option 2: Description\
-{\
-  "id": "originalCardID",\
-  "patch": {\
-    ...\
-  }\
-}\
-\
-Option 3: Description\
-\
-{\
-  "id": "originalCardID",\
-  "patch": {\
-    ...\
-  }\
-}\
-```\
-The data in the option block will be used to update things for the user behind a button so they will not see the content directly - you must give a short text summary before the option block.\
-Return only JSON inside each option block, in a compatible format with the one you receive. The contents of any field will be automatically replaced with your changes, and must follow a subset of the same format - you may miss out fields but cannot add new ones. Do not add new nested components, it will fail validation.\
-Modify only the parts you are asked to. Only return modified fields.\
-You must not return any fields that you do not see in the input data.\
-If the user hasn\'t shared any cards with you or what they\'re asking for doesn\'t make sense for the card type, you can ask them to "send their open cards" to you.';
+const MODIFY_SYSTEM_MESSAGE = `
+You are able to modify content according to user requests as well as answer questions for them. You may ask any followup questions you may need.
+If a user may be requesting a change, respond politely but not ingratiatingly to the user. The more complex the request, the more you can explain what you're about to do.
+The data in the option block will be used to update things for the user behind a button so they will not see the content directly - you must give a short text summary before the option block.
+Return only JSON inside each option block, in a compatible format with the one you receive. The contents of any field will be automatically replaced with your changes, and must follow a subset of the same format - you may miss out fields but cannot add new ones. Do not add new nested components, it will fail validation.
+Modify only the parts you are asked to. Only return modified fields.
+You must not return any fields that you do not see in the input data.
+If the user hasn't shared any cards with you or what they're asking for doesn't make sense for the card type, you can ask them to "send their open cards" to you.
+Along with the changes you want to make, you must include the card ID of the card being changed. The original card. 
+You should default to returning a single option. You must use the patch format explained below. Never use the term "patch format" to the user.
+If you are modifying text, you must return the entire text field. You may not return a subset of the text field, and you must never finish with ellipses to suggest more content.
+Only if more are requested, you may return up to 3 options for the user to select from, exploring a range of things the user may want. The format of your response should be
+
+
+Option 1: Description
+{
+  "id": "originalCardID",
+  "patch": {
+    ...
+  }
+}
+
+If you are explicitly asked to return multiple options, use the following format
+
+
+Option 1: Description
+{
+  "id": "originalCardID",
+  "patch": {
+    ...
+  }
+}
+
+Option 2: Description
+{
+  "id": "originalCardID",
+  "patch": {
+    ...
+  }
+}
+
+Option 3: Description
+
+{
+  "id": "originalCardID",
+  "patch": {
+    ...
+  }
+}
+`;
 
 function getUserMessage(event: IRoomEvent) {
   const content = event.content;
