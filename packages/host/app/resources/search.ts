e7dda99e68deadcc57c672f005c2a6a4414dd4c7
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2023-10-09 12:00:55 -0400

    Live query refactor

--- packages/host/app/resources/search.ts
+++ packages/host/app/resources/search.ts
@@ -1,3 +1,5 @@
+import { registerDestructor } from '@ember/destroyable';
+
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
@@ -5,7 +7,7 @@ import { restartableTask } from 'ember-concurrency';
 import { Resource } from 'ember-resources';
 import flatMap from 'lodash/flatMap';
 
-import { baseRealm } from '@cardstack/runtime-common';
+import { baseRealm, subscribeToRealm } from '@cardstack/runtime-common';
 
 import type { Query } from '@cardstack/runtime-common/query';
 
@@ -21,6 +23,8 @@ interface Args {
   named: {
     query: Query;
     realms?: string[];
+    isLive?: true;
+    doWhileRefreshing?: (ready: Promise<void> | undefined) => Promise<void>;
   };
 }
 
@@ -29,20 +33,18 @@ interface Args {
 const { otherRealmURLs } = ENV;
 
 export class Search extends Resource<Args> {
-  @tracked instances: CardDef[] = [];
-  @tracked instancesByRealm: RealmCards[] = [];
-  @service declare cardService: CardService;
-  ready: Promise<void> | undefined;
+  @service private declare cardService: CardService;
+  @tracked private _instances: CardDef[] = [];
+  @tracked private _instancesByRealm: RealmCards[] = [];
+  @tracked private staleInstances: CardDef[] = [];
+  @tracked private staleInstancesByRealm: RealmCards[] = [];
+  @tracked private realmsToSearch: string[] = [];
+  private ready: Promise<void> | undefined;
+  private subscriptions: { url: string; unsubscribe: () => void }[] = [];
 
   modify(_positional: never[], named: Args['named']) {
-    let { query, realms } = named;
-    this.ready = this.search.perform(query, realms);
-  }
-
-  private search = restartableTask(async (query: Query, realms?: string[]) => {
-    // until we have realm index rollup, search all the realms as separate
-    // queries that we merge together
-    let realmsToSearch = realms ?? [
+    let { query, realms, isLive, doWhileRefreshing } = named;
+    this.realmsToSearch = realms ?? [
       ...new Set(
         realms ?? [
           this.cardService.defaultURL.href,
@@ -51,23 +53,66 @@ export class Search extends Resource<Args> {
         ],
       ),
     ];
-    this.instances = flatMap(
+    this.ready = this.search.perform(query);
+
+    if (isLive) {
+      // TODO this triggers a new search against all realms if any single realm
+      // updates. Make this more precise where we only search the updated realm
+      // instead of all realms.
+      this.subscriptions = this.realmsToSearch.map((realm) => ({
+        url: `${realm}_message`,
+        unsubscribe: subscribeToRealm(`${realm}_message`, ({ type }) => {
+          // we are only interested in index events
+          if (type !== 'index') {
+            return;
+          }
+          // we show stale instances during a live refresh while we are
+          // waiting for the new instances to arrive--this eliminates the flash
+          // while we wait
+          this.staleInstances = [...(this.instances ?? [])];
+          this.staleInstancesByRealm = [...(this._instancesByRealm ?? [])];
+
+          this.search.perform(query);
+          if (doWhileRefreshing) {
+            doWhileRefreshing(this.ready);
+          }
+        }),
+      }));
+
+      registerDestructor(this, () => {
+        for (let subscription of this.subscriptions) {
+          subscription.unsubscribe();
+        }
+      });
+    }
+  }
+
+  get instances() {
+    return this.isLoading ? this.staleInstances : this._instances;
+  }
+
+  get instancesByRealm() {
+    return this.isLoading ? this.staleInstancesByRealm : this._instancesByRealm;
+  }
+
+  private search = restartableTask(async (query: Query) => {
+    this._instances = flatMap(
       await Promise.all(
         // use a Set since the default URL may actually be the base realm
-        realmsToSearch.map(
+        this.realmsToSearch.map(
           async (realm) => await this.cardService.search(query, new URL(realm)),
         ),
       ),
     );
 
-    let realmsWithCards = realmsToSearch
+    let realmsWithCards = this.realmsToSearch
       .map((url) => {
-        let cards = this.instances.filter((card) => card.id.startsWith(url));
+        let cards = this._instances.filter((card) => card.id.startsWith(url));
         return { url, cards };
       })
       .filter((r) => r.cards.length > 0);
 
-    this.instancesByRealm = await Promise.all(
+    this._instancesByRealm = await Promise.all(
       realmsWithCards.map(async ({ url, cards }) => {
         let realmInfo = await this.cardService.getRealmInfo(cards[0]);
         if (!realmInfo) {
@@ -92,6 +137,26 @@ export function getSearchResults(
     named: {
       query: query(),
       realms: realms ? realms() : undefined,
+    },
+  })) as Search;
+}
+
+// A new search is triggered whenever the index updates. Consumers of this
+// function that render their cards using {{#each}} should make sure to use the
+// "key" field: {{#each #this.results.instances key="id" as |instance|}} in
+// order to keep the results stable between refreshes.
+export function getLiveSearchResults(
+  parent: object,
+  query: () => Query,
+  realms?: () => string[],
+  doWhileRefreshing?: () => (ready: Promise<void> | undefined) => Promise<void>,
+) {
+  return Search.from(parent, () => ({
+    named: {
+      isLive: true,
+      query: query(),
+      realms: realms ? realms() : undefined,
+      doWhileRefreshing: doWhileRefreshing ? doWhileRefreshing() : undefined,
     },
   })) as Search;
 }
