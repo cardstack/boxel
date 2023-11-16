20bf2d5735d23307fe45af8eec4a1b00ff27b0c1
Luke Melia <luke@lukemelia.com>
2023-11-16 17:27:36 -0500

    Extract StackBackgroundsResource from interact-submode component

---
+++ packages/host/app/resources/stack-backgrounds.ts
@@ -0,0 +1,57 @@
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+import { Resource } from 'ember-resources';
+
+import type { Stack } from '../components/operator-mode/interact-submode';
+import type CardService from '../services/card-service';
+
+interface Args {
+  positional: [stacks: Stack[]];
+}
+
+export class StackBackgroundsResource extends Resource<Args> {
+  @tracked value: (string | undefined | null)[] = [];
+  @service declare cardService: CardService;
+
+  get backgroundImageURLs() {
+    return this.value?.map((u) => (u ? u : undefined)) ?? [];
+  }
+
+  get hasDifferingBackgroundURLs() {
+    let { backgroundImageURLs } = this;
+    return (
+      backgroundImageURLs &&
+      backgroundImageURLs.length > 1 &&
+      backgroundImageURLs.some(
+        (u) => u === null || backgroundImageURLs[0] !== u,
+      )
+    );
+  }
+
+  get differingBackgroundImageURLs() {
+    if (!this.hasDifferingBackgroundURLs) {
+      return [];
+    }
+    return this.backgroundImageURLs;
+  }
+
+  async modify(positional: Args['positional'], _named: never) {
+    let [stacks] = positional;
+    let result = await Promise.all(
+      stacks.map(async (stack) => {
+        if (stack.length === 0) {
+          this.value = [];
+        }
+        let bottomMostCard = stack[0];
+        return (await this.cardService.getRealmInfo(bottomMostCard.card))
+          ?.backgroundURL;
+      }),
+    );
+    this.value = result;
+  }
+}
+
+export function stackBackgroundsResource(parent: { stacks: Stack[] }) {
+  return StackBackgroundsResource.from(parent, () => [parent.stacks]);
+}
