ce2a8bef5ede8edc8b5ab9fb5a269062a4a02ad2
Buck Doyle <buck.doyle@cardstack.com>
2023-12-06 17:20:06 -0600

    Fix nesting of CSS override

--- packages/host/.percy.js
+++ packages/host/.percy.js
@@ -2,10 +2,10 @@ module.exports = {
   version: 2,
   snapshot: {
     widths: [1280],
+    percyCSS: `
+      .monaco-container[data-test-editor] {
+        display: none;
+      }
+    `,
   },
-  percyCSS: `
-    .monaco-container[data-test-editor] {
-      display: none;
-    }
-  `,
 };
