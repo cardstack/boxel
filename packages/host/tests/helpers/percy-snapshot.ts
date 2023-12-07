106fe5b19a41e0a552fe6ffba4f29a610fb82dfa
Buck Doyle <buck.doyle@cardstack.com>
2023-12-07 16:09:36 -0600

    Add settled helper before taking snapshot
    It’s my belief that a snapshot should not be taken before
    everything has settled.

--- packages/host/tests/helpers/percy-snapshot.ts
+++ packages/host/tests/helpers/percy-snapshot.ts
@@ -1,8 +1,12 @@
+import { settled } from '@ember/test-helpers';
+
 import originalPercySnapshot from '@percy/ember';
 
 export default async function percySnapshot(
   ...args: Parameters<typeof originalPercySnapshot>
 ) {
+  await settled();
+
   // Adapted from: https://github.com/GoogleForCreators/web-stories-wp/pull/6324/files#diff-970412cd35c9346699038fab952d3c0c9a0e5060a60ed528813def9a00ca157b
   const weights = ['400', '500', '700'];
   const font = '12px Poppins';
