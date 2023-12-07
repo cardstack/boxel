eb932ecf5fdf8e25cc1d6eee3ff057e8492486ec
Buck Doyle <buck.doyle@cardstack.com>
2023-12-07 14:42:50 -0600

    Add last-modifieds to Percy-specific CSS

--- packages/host/.percy.js
+++ packages/host/.percy.js
@@ -6,6 +6,10 @@ module.exports = {
       .monaco-container[data-test-editor] {
         display: none;
       }
+
+      [data-test-definition-info-test], [data-test-last-modified] {
+        visibility: hidden;
+      }
     `,
   },
 };
