a6e1196753003ffdf5465a1b773a47967e6751f6
Buck Doyle <buck.doyle@cardstack.com>
2024-01-05 11:03:27 -0600

    Remove check that obscured errors

--- packages/matrix/scripts/register-test-user.ts
+++ packages/matrix/scripts/register-test-user.ts
@@ -8,7 +8,7 @@ let password = process.env.PASSWORD;
   return new Promise<string>((resolve, reject) => {
     childProcess.exec(`docker exec boxel-synapse register_new_matrix_user http://localhost:8008 -c /data/homeserver.yaml -u ${username} -p ${password} --no-admin`,
       (err, stdout) => {
-        if (err && !stdout.includes('User ID already taken')) {
+        if (err) {
           reject(err);
         }
         console.log(stdout.trim());
