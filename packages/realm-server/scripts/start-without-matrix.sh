f543d77af5ddb0d7c16bd6219e88181a0669700e
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-01-12 16:10:53 -0500

    host tests matrix server startup

---
+++ packages/realm-server/scripts/start-without-matrix.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+NODE_NO_WARNINGS=1 start-server-and-test \
+  'run-p start:development start:base:root' \
+  'http-get://localhost:4201/base/fields/boolean-field?acceptHeader=application%2Fvnd.card%2Bjson|http-get://localhost:4203/fields/boolean-field?acceptHeader=application%2Fvnd.card%2Bjson|http-get://localhost:4201/drafts/index?acceptHeader=application%2Fvnd.card%2Bjson' \
+  'run-p start:test-realms start:test-container' \
+  'http-get://localhost:4202/node-test/person-1?acceptHeader=application%2Fvnd.card%2Bjson|http-get://127.0.0.1:4205' \
+  'wait'
