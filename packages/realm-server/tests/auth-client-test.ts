bd95218f7dd4138b19500b7b68bb2a2f0d99bfd2
Matic Jurglic <matic@jurglic.si>
2024-02-05 13:55:27 +0100

    Some more refactor, add tests

--- packages/realm-server/tests/auth-client-test.ts
+++ packages/realm-server/tests/auth-client-test.ts
@@ -5,7 +5,7 @@ import { RealmAuthClient } from '@cardstack/runtime-common/realm-auth-client';
 import jwt from 'jsonwebtoken';
 import { MatrixClient } from '@cardstack/runtime-common/matrix-client';
 
-function createJWT(expiresIn: string) {
+function createJWT(expiresIn: string | number) {
   return jwt.sign({}, 'secret', { expiresIn });
 }
 
@@ -18,7 +18,7 @@ module('realm-auth-client', function (assert) {
       'password',
       new URL('http://testmatrix.com/'),
       new URL('http://testrealm.com/'),
-    );
+    ) as any;
     let mockMatrixClient = {
       async login() {
         return Promise.resolve();
@@ -33,6 +33,8 @@ module('realm-auth-client', function (assert) {
         return Promise.resolve();
       },
     };
+
+    // [] notation is a hack to make TS happy so we can set private properties with mocks
     client['matrixClient'] = mockMatrixClient as unknown as MatrixClient;
     client['initiateSessionRequest'] = async function (): Promise<Response> {
       return {
@@ -69,8 +71,16 @@ module('realm-auth-client', function (assert) {
       await client.getJWT(),
       'jwt is the same which means it is cached until it is about to expire',
     );
+  });
 
-    jwtFromClient = createJWT('1s');
+  test('it refreshes the jwt if it is about to expire in the client', async function (assert) {
+    let jwtFromClient = createJWT('10s');
+    client['jwt'] = jwtFromClient; // Expires very soon, so the client will first refresh it
+    assert.notEqual(jwtFromClient, await client.getJWT(), 'jwt got refreshed');
+  });
+
+  test('it refreshes the jwt if it expired in the client', async function (assert) {
+    let jwtFromClient = createJWT(Date.now() / 1000 - 1); // Expired 1 second ago
     client['jwt'] = jwtFromClient; // Expires very soon, so the client will first refresh it
     assert.notEqual(jwtFromClient, await client.getJWT(), 'jwt got refreshed');
   });
