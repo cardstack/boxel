176e430ab7352db2625de44408b683b15cb0ba71
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-02-20 12:20:22 -0500

    more debugging

--- packages/matrix/tests/messages.spec.ts
+++ packages/matrix/tests/messages.spec.ts
@@ -133,7 +133,11 @@ test.describe('Room messages', () => {
     await createRoom(page);
 
     await page.locator('[data-test-choose-card-btn]').click();
-    await expect(page.locator(`[data-test-card-catalog]`)).toHaveCount(1); // this triggers a wait for this element
+    await expect(
+      page.locator(
+        `[data-test-card-catalog] [data-test-realm="Test Workspace A"]`,
+      ),
+    ).toHaveCount(1); // this triggers a wait for this element
     await expect(page.locator(`[data-test-select="${testCard}"]`)).toHaveCount(
       1,
     ); // this triggers a wait for this element
