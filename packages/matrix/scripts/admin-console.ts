b4d05a05a8feb88bb9d58df8432d151cf0855ec6
tintinthong <justinthong93@gmail.com>
2024-02-26 20:18:15 +0800

    cleanup script admin console

--- packages/matrix/scripts/admin-console.ts
+++ packages/matrix/scripts/admin-console.ts
@@ -1,13 +1,5 @@
 import * as childProcess from 'child_process';
 
-import { loginUser } from '../docker/synapse';
-
-export const adminUsername = 'admin';
-export const adminPassword = 'password';
-
-let username = process.env.MATRIX_USERNAME || adminUsername;
-let password = process.env.MATRIX_PASSWORD || adminPassword;
-
 (async () => {
   return new Promise<string>((resolve, reject) => {
     childProcess.exec(
@@ -15,21 +7,6 @@ let password = process.env.MATRIX_PASSWORD || adminPassword;
       async (err, stdout) => {
         console.log(stdout);
         if (err) {
-          if (stdout.includes('User ID already taken')) {
-            let cred = await loginUser(username, password);
-            if (!cred.userId) {
-              reject(
-                `User ${username} already exists, but the password does not match`,
-              );
-              return;
-            } else {
-              console.log(
-                `User ${username} already exists and the password matches`,
-              );
-              resolve(`User already exists as ${cred.userId}`);
-              return;
-            }
-          }
           reject(err);
         }
         console.log(stdout.trim());
