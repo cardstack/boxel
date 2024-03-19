5b703d22fb84c0f6f9727a0f01ce22b58c7edb7f
Luke Melia <luke@lukemelia.com>
2024-03-19 19:01:20 -0400

    Generalize `fill` behavior of behaviors rather than special casing StaticBehavior treatment

--- packages/boxel-motion/addon/src/behaviors/static.ts
+++ packages/boxel-motion/addon/src/behaviors/static.ts
@@ -1,16 +1,22 @@
-import type Behavior from './base.ts';
-import {
+import Behavior, {
   type Frame,
   type StaticToFramesArgument,
   timeToFrame,
 } from './base.ts';
 
+export interface StaticBehaviorOptions {
+  fill?: boolean;
+}
 export default class StaticBehavior implements Behavior {
+  fill: boolean;
+  constructor(options?: StaticBehaviorOptions) {
+    this.fill = options?.fill ?? false;
+  }
+
   *getFrames(options: StaticToFramesArgument) {
     let frameCount = timeToFrame(options.duration) + 1;
 
     for (let i = 0; i < frameCount; i++) {
-      // TODO: this can explicitly be non-numeric, fix TS
       yield { value: options.value, velocity: 0 } as Frame;
     }
   }
