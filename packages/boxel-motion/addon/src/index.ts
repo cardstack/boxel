609aaabf7ec8dd7efd8bcd57a07f10bedce7fcc6
Luke Melia <luke@lukemelia.com>
2024-03-20 16:21:24 -0400

    boxel-motion improvements
    - Add Split View example to boxel-motion test app as a study for the AI Assistant Panel animation
    - Incorporate boxel-motion builds into CI
    - Generalize fill behavior of behaviors rather than special casing StaticBehavior treatment
    - Default StaticBehavior to a duration of 1, since you can now opt into forward fill
        - when using StaticBehavior with SpringBehavior, you don't know how long the animation will be, and you may want a static animation to persist for the duration of the animation
    - Use context-relative bounds for removed sprites

--- packages/boxel-motion/addon/src/index.ts
+++ packages/boxel-motion/addon/src/index.ts
@@ -4,13 +4,14 @@ import StaticBehavior from './behaviors/static.ts';
 import TweenBehavior from './behaviors/tween.ts';
 import WaitBehavior from './behaviors/wait.ts';
 import AnimationContext from './components/animation-context.gts';
-import { type Changeset } from './models/animator.ts';
+import { type Changeset, type IContext } from './models/animator.ts';
 import {
   type AnimationDefinition,
   type AnimationTimeline,
   OrchestrationMatrix,
 } from './models/orchestration.ts';
 import Sprite, { type ISpriteModifier, SpriteType } from './models/sprite.ts';
+import sprite from './modifiers/sprite.ts';
 import AnimationsService from './services/animations.ts';
 
 export {
@@ -20,10 +21,12 @@ export {
   AnimationTimeline,
   Changeset,
   FPS,
+  IContext,
   ISpriteModifier,
   OrchestrationMatrix,
   SpringBehavior,
-  Sprite,
+  Sprite, // model
+  sprite, // modifier
   SpriteType,
   StaticBehavior,
   TweenBehavior,
--- packages/boxel-motion/test-app/app/app.js
+++ packages/boxel-motion/test-app/app/app.js
@@ -2,6 +2,7 @@ import Application from '@ember/application';
 import config from 'boxel-motion-test-app/config/environment';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
+import '@cardstack/boxel-motion/styles/addon.css';
 import './deprecation-workflow';
 
 export default class App extends Application {
