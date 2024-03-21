b664176bae34232b7b1ea6177645b28f7afcc5f3
Buck Doyle <buck.doyle@cardstack.com>
2024-03-21 10:39:50 -0700

    Add preliminary UI deployment

---
+++ packages/boxel-ui/test-app/config/deploy.js
@@ -0,0 +1,43 @@
+/* eslint-env node */
+
+module.exports = function (deployTarget) {
+  let ENV = {
+    pipeline: {
+      activateOnDeploy: true,
+    },
+    plugins: ['build', 'smart-compress', 'revision-data', 's3', 'cloudfront'],
+    build: {},
+    s3: {
+      allowOverwrite: true,
+      bucket: process.env.AWS_S3_BUCKET,
+      region: process.env.AWS_REGION,
+      filePattern: '**/*',
+    },
+    cloudfront: {
+      objectPaths: ['/*'],
+      distribution: process.env.AWS_CLOUDFRONT_DISTRIBUTION,
+    },
+  };
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+  }
+
+  if (deployTarget === 'build-only') {
+    ENV.build.environment = 'production';
+    ENV.plugins = ['build'];
+  }
+
+  if (
+    deployTarget === 's3-preview-staging' ||
+    deployTarget === 's3-preview-production'
+  ) {
+    ENV.s3.prefix = process.env.PR_BRANCH_NAME;
+  }
+
+  return ENV;
+};
