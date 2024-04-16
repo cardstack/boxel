b52ada71ff957d60c182a0d27d4e0fdd8784e8f5
Buck Doyle <buck.doyle@cardstack.com>
2024-04-16 15:55:05 -0500

    Change to use blobs in CI

--- packages/matrix/playwright.config.ts
+++ packages/matrix/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1,
-  reporter: 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: 'http://localhost:4202/test',
