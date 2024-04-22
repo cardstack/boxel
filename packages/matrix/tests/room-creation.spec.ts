924e83f662f4b467a3be149ce4f19c809e62745c
Buck Doyle <buck.doyle@cardstack.com>
2024-04-22 09:16:21 -0500

    Add slow marker for multi-user test

--- packages/matrix/tests/room-creation.spec.ts
+++ packages/matrix/tests/room-creation.spec.ts
@@ -68,6 +68,8 @@ test.describe('Room creation', () => {
   test('it does not create a new room when another new room is available', async ({
     page,
   }) => {
+    test.slow(); // FIXME remove?
+
     await login(page, 'user1', 'pass');
 
     let room = await getRoomId(page); // Automatically created room
