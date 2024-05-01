288c06423c649dc0f233e4747731be4ed2f2bf7f
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-05-01 15:00:06 -0400

    more debugging of CI

--- packages/realm-server/scripts/drop-all-dbs.sh
+++ packages/realm-server/scripts/drop-all-dbs.sh
@@ -4,5 +4,3 @@ pnpm run drop-db boxel_dev
 pnpm run drop-db boxel_test
 pnpm run drop-db boxel_dev_base
 pnpm run drop-db boxel_test_base_root
-pnpm run drop-db boxel_test_drafts_root
-pnpm run drop-db boxel_test_drafts_root
