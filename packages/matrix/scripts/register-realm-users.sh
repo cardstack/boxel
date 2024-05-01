2bef9b25519069f727139d26ccc640002b8e46cd
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-05-01 11:18:27 -0400

    debugging

--- packages/matrix/scripts/register-realm-users.sh
+++ packages/matrix/scripts/register-realm-users.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
+set -x
+
+echo "starting register realm users"
 
 until $(curl --output /dev/null --silent --head --fail http://localhost:8008); do
   printf '.'
   sleep 5
 done
+echo "matrix server is up"
 MATRIX_USERNAME=base_realm MATRIX_PASSWORD=password ts-node --transpileOnly ./scripts/register-test-user.ts
 MATRIX_USERNAME=drafts_realm MATRIX_PASSWORD=password ts-node --transpileOnly ./scripts/register-test-user.ts
 MATRIX_USERNAME=published_realm MATRIX_PASSWORD=password ts-node --transpileOnly ./scripts/register-test-user.ts
