f3c350b728cc29b70021c59c14c2d64ecf9b5290
tintinthong <justinthong93@gmail.com>
2024-05-16 22:15:47 +0800

    cleanup

--- packages/ai-bot/setup-logger.ts
+++ packages/ai-bot/setup-logger.ts
@@ -1,10 +1,7 @@
 import { makeLogDefinitions } from '@cardstack/runtime-common';
 import { logger as matrixJsLogger } from 'matrix-js-sdk/lib/logger';
 
-if (
-  process.env.DISABLE_MATRIX_JS_LOGGING &&
-  process.env.DISABLE_MATRIX_JS_LOGGING === 'TRUE'
-) {
+if (process.env.DISABLE_MATRIX_JS_LOGGING === 'TRUE') {
   matrixJsLogger.disableAll();
 }
 (globalThis as any)._logDefinitions = makeLogDefinitions(
