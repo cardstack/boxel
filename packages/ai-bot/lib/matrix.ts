aed81176a3ca3fcd17aeb3985879d8e9be5ee8cc
tintinthong <justinthong93@gmail.com>
2024-05-21 12:58:05 +0800

    patchCard didn get applied after refactor (#1279)
    * patchCard didn get applied after refactor
    
    * another merge issue

--- packages/ai-bot/lib/matrix.ts
+++ packages/ai-bot/lib/matrix.ts
@@ -76,7 +76,7 @@ export async function sendOption(
     format: 'org.matrix.custom.html',
     data: {
       command: {
-        type: 'patch',
+        type: 'patchCard',
         id: id,
         patch: {
           attributes: patch['attributes'],
