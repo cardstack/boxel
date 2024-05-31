da3b73157f4f72219c7b291972833ca54a6d9c45
Buck Doyle <buck.doyle@cardstack.com>
2024-05-31 09:19:54 -0500

    host: Add wait for async assertion callback

--- packages/host/tests/acceptance/code-submode/inspector-test.ts
+++ packages/host/tests/acceptance/code-submode/inspector-test.ts
@@ -929,6 +929,8 @@ module('Acceptance | code submode | inspector tests', function (hooks) {
   });
 
   test<TestContextWithSSE>('can delete a card instance from code submode with no recent files to fall back on', async function (assert) {
+    let done = assert.async();
+
     let expectedEvents = [
       {
         type: 'index',
@@ -987,6 +989,7 @@ module('Acceptance | code submode | inspector tests', function (hooks) {
       expectedEvents,
       callback: async () => {
         await click('[data-test-confirm-delete-button]');
+        done();
       },
     });
     await waitFor('[data-test-empty-code-mode]');
