d0e4b19c4cdc25e8882db2aa08eea8c92303980e
Matic Jurglic <matic@jurglic.si>
2024-05-31 13:02:39 +0200

    Prevent SQL injection

--- packages/runtime-common/realm-permission-queries.ts
+++ packages/runtime-common/realm-permission-queries.ts
@@ -1,29 +1,43 @@
 import { DBAdapter } from './db';
 import { RealmPermissions } from './realm';
 
+async function insertPermission(
+  dbAdapter: DBAdapter,
+  realmURL: URL,
+  username: string,
+  read: boolean,
+  write: boolean,
+) {
+  const query = `INSERT INTO realm_user_permissions (realm_url, username, read, write) VALUES ($1, $2, $3, $4);`;
+  const values = [realmURL.href, username, read, write];
+  return dbAdapter.execute(query, { bind: values });
+}
+
 export async function insertPermissions(
   dbAdapter: DBAdapter,
   realmURL: URL,
   permissions: RealmPermissions,
 ) {
-  return dbAdapter.execute(
-    `INSERT INTO realm_user_permissions (realm_url, username, read, write) VALUES ${Object.entries(
-      permissions,
-    )
-      .map(
-        ([user, userPermissions]) =>
-          `('${realmURL}', '${user}', ${userPermissions.includes(
-            'read',
-          )}, ${userPermissions.includes('write')})`,
-      )
-      .join(', ')};`,
+  const insertPromises = Object.entries(permissions).map(
+    ([user, userPermissions]) => {
+      return insertPermission(
+        dbAdapter,
+        realmURL,
+        user,
+        userPermissions.includes('read'),
+        userPermissions.includes('write'),
+      );
+    },
   );
+
+  await Promise.all(insertPromises);
 }
 
 export async function permissionsExist(dbAdapter: DBAdapter, realmURL: URL) {
   return (
     await dbAdapter.execute(
-      `SELECT EXISTS(SELECT 1 FROM realm_user_permissions WHERE realm_url = '${realmURL.href}') AS has_rows`,
+      `SELECT EXISTS(SELECT 1 FROM realm_user_permissions WHERE realm_url = $1) AS has_rows`,
+      { bind: [realmURL.href] },
     )
   )[0].has_rows;
 }
@@ -33,7 +47,8 @@ export async function fetchUserPermissions(
   realmURL: URL,
 ) {
   let permissions = await dbAdapter.execute(
-    `SELECT username, read, write FROM realm_user_permissions WHERE realm_url = '${realmURL.href}';`,
+    `SELECT username, read, write FROM realm_user_permissions WHERE realm_url = $1;`,
+    { bind: [realmURL.href] },
   );
 
   return permissions.reduce((permissionsAcc, { username, read, write }) => {
