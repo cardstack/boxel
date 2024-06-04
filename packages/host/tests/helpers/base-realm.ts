5c14fa5c3980432aa1ba4215a61a15edb1f157a1
Edward Faulkner <edward@eaf4.com>
2024-06-04 16:05:25 -0400

    use independent loaders each test

--- packages/host/tests/helpers/base-realm.ts
+++ packages/host/tests/helpers/base-realm.ts
@@ -4,15 +4,15 @@ import { baseRealm } from '@cardstack/runtime-common';
 
 import type LoaderService from '@cardstack/host/services/loader-service';
 
+import type * as Base64ImageFieldModule from 'https://cardstack.com/base/base64-image';
+import type * as BigIntegerModule from 'https://cardstack.com/base/big-integer';
 import type * as CardAPIModule from 'https://cardstack.com/base/card-api';
-import type * as NumberFieldModule from 'https://cardstack.com/base/number';
-import type * as StringFieldModule from 'https://cardstack.com/base/string';
+import type * as CodeRefModule from 'https://cardstack.com/base/code-ref';
 import type * as DateFieldModule from 'https://cardstack.com/base/date';
 import type * as DatetimeFieldModule from 'https://cardstack.com/base/datetime';
-import type * as Base64ImageFieldModule from 'https://cardstack.com/base/base64-image';
-import type * as CodeRefModule from 'https://cardstack.com/base/code-ref';
-import type * as BigIntegerModule from 'https://cardstack.com/base/big-integer';
 import type * as EthereumAddressModule from 'https://cardstack.com/base/ethereum-address';
+import type * as NumberFieldModule from 'https://cardstack.com/base/number';
+import type * as StringFieldModule from 'https://cardstack.com/base/string';
 
 let _string: (typeof StringFieldModule)['default'];
 let _number: (typeof NumberFieldModule)['default'];
@@ -39,12 +39,7 @@ let isSaved: (typeof CardAPIModule)['isSaved'];
 let relationshipMeta: (typeof CardAPIModule)['relationshipMeta'];
 let getQueryableValue: (typeof CardAPIModule)['getQueryableValue'];
 
-let didInit = false;
-
 async function initialize() {
-  if (didInit) {
-    return;
-  }
   let owner = (getContext() as TestContext).owner;
   let loader = (owner.lookup('service:loader-service') as LoaderService).loader;
 
@@ -104,8 +99,6 @@ async function initialize() {
     relationshipMeta,
     getQueryableValue,
   } = cardAPI);
-
-  didInit = true;
 }
 
 export async function setupBaseRealm(hooks: NestedHooks) {
