f253d3fd28c8db41f596b0629c3e383651982774
tintinthong <justinthong93@gmail.com>
2024-07-02 17:04:30 +0800

    fix more lint

--- packages/host/app/lib/matrix-handlers/timeline.ts
+++ packages/host/app/lib/matrix-handlers/timeline.ts
@@ -18,7 +18,7 @@ import {
 
 export function onReceipt(context: Context) {
   return async (e: MatrixEvent) => {
-    let userId = context.client.credentials.userId!;
+    let userId = context.client?.credentials.userId!;
     let eventIds = Object.keys(e.getContent());
     for (let eventId of eventIds) {
       let receipt = e.getContent()[eventId]['m.read'][userId];
@@ -58,7 +58,7 @@ async function drainTimeline(context: Context) {
   let events = [...context.timelineQueue];
   context.timelineQueue = [];
   for (let { event, oldEventId } of events) {
-    await context.client.decryptEventIfNeeded(event);
+    await context.client?.decryptEventIfNeeded(event);
     await processDecryptedEvent(
       context,
       {
@@ -84,14 +84,14 @@ async function processDecryptedEvent(
       `bug: roomId is undefined for event ${JSON.stringify(event, null, 2)}`,
     );
   }
-  let room = context.client.getRoom(roomId);
+  let room = context.client?.getRoom(roomId);
   if (!room) {
     throw new Error(
       `bug: should never get here--matrix sdk returned a null room for ${roomId}`,
     );
   }
 
-  let userId = context.client.getUserId();
+  let userId = context.client?.getUserId();
   if (!userId) {
     throw new Error(
       `bug: userId is required for event ${JSON.stringify(event, null, 2)}`,
@@ -131,7 +131,7 @@ async function processDecryptedEvent(
           );
           let fragmentData: CardFragmentContent['data'];
           if (!fragmentEvent) {
-            fragmentEvent = (await context.client.fetchRoomEvent(
+            fragmentEvent = (await context.client?.fetchRoomEvent(
               roomId,
               currentFragmentId ?? '',
             )) as DiscreteMatrixEvent;
@@ -177,6 +177,6 @@ async function processDecryptedEvent(
 
   if (room.oldState.paginationToken != null) {
     // we need to scroll back to capture any room events fired before this one
-    await context.client.scrollback(room);
+    await context.client?.scrollback(room);
   }
 }
