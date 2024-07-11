70f48088f7505f2ca948fa3bda6f30e993b85089
tintinthong <justinthong93@gmail.com>
2024-07-12 00:46:29 +0800

    add models

---
+++ packages/host/app/lib/matrix-model/message.ts
@@ -0,0 +1,94 @@
+import { EventStatus } from 'matrix-js-sdk';
+
+import { getCard } from '@cardstack/runtime-common';
+
+import { CardDef } from 'https://cardstack.com/base/card-api';
+
+import { PatchField } from 'https://cardstack.com/base/command';
+
+import { RoomMember } from './member';
+
+const ErrorMessage: Record<string, string> = {
+  ['M_TOO_LARGE']: 'Message is too large',
+};
+
+type AttachedCardResource = {
+  card: CardDef | undefined;
+  loaded?: Promise<void>;
+  cardError?: { id: string; error: Error };
+};
+
+type RoomMessageInterface = RoomMessageRequired & RoomMessageOptional;
+
+interface RoomMessageRequired {
+  author: RoomMember;
+  created: Date;
+  updated: Date;
+  message: string;
+  formattedMessage: string;
+  eventId: string;
+  status: EventStatus | null;
+}
+
+interface RoomMessageOptional {
+  transactionId?: string | null;
+  attachedCardIds?: string[] | null;
+  isStreamingFinished?: boolean;
+  index?: number;
+  errorMessage?: string;
+  clientGeneratedId?: string | null;
+  command?: PatchField | null;
+}
+
+export class RoomMessageModel implements RoomMessageInterface {
+  attachedCardIds?: string[] | null;
+  index?: number;
+  transactionId?: string | null;
+  isStreamingFinished?: boolean;
+  errorMessage?: string;
+  clientGeneratedId?: string;
+  command?: PatchField | null;
+
+  author: RoomMember;
+  formattedMessage: string;
+  status: EventStatus | null;
+  created: Date;
+  updated: Date;
+  eventId: string;
+  message: string;
+
+  constructor(init: RoomMessageInterface) {
+    Object.assign(this, init);
+    this.author = init.author;
+    this.formattedMessage = init.formattedMessage;
+    this.message = init.message;
+    this.eventId = init.eventId;
+    this.created = init.created;
+    this.updated = init.updated;
+    this.status = init.status;
+  }
+  get isRetryable() {
+    return (
+      this.errorMessage && this.errorMessage !== ErrorMessage['M_TOO_LARGE']
+    );
+  }
+  get attachedResources(): AttachedCardResource[] | undefined {
+    if (!this.attachedCardIds?.length) {
+      return undefined;
+    }
+    let cards = this.attachedCardIds.map((id) => {
+      let card = getCard(new URL(id));
+      if (!card) {
+        return {
+          card: undefined,
+          cardError: {
+            id,
+            error: new Error(`cannot find card for id "${id}"`),
+          },
+        };
+      }
+      return card;
+    });
+    return cards;
+  }
+}
---
+++ packages/host/app/lib/matrix-model/room.ts
@@ -0,0 +1,44 @@
+import { tracked } from '@glimmer/tracking';
+
+import type { MatrixEvent as DiscreteMatrixEvent } from 'https://cardstack.com/base/matrix-event';
+import type {
+  RoomCreateEvent,
+  RoomNameEvent,
+} from 'https://cardstack.com/base/matrix-event';
+
+export class RoomModel {
+  @tracked events: DiscreteMatrixEvent[] = [];
+
+  get roomId() {
+    return this.events.length > 0 ? this.events[0].room_id : undefined;
+  }
+
+  get created() {
+    let event = this.events.find((e) => e.type === 'm.room.create') as
+      | RoomCreateEvent
+      | undefined;
+    if (event) {
+      return new Date(event.origin_server_ts);
+    }
+    return new Date();
+  }
+
+  get name() {
+    // Read from this.events instead of this.newEvents to avoid a race condition bug where
+    // newEvents never returns the m.room.name while the event is present in events
+    let events = this.events
+      .filter((e) => e.type === 'm.room.name')
+      .sort(
+        (a, b) => a.origin_server_ts - b.origin_server_ts,
+      ) as RoomNameEvent[];
+    if (events.length > 0) {
+      return events.pop()!.content.name;
+    }
+    return;
+  }
+
+  get lastActiveTimestamp() {
+    let maybeLastActive = this.events[this.events.length - 1]?.origin_server_ts;
+    return maybeLastActive ?? this.created.getTime();
+  }
+}
