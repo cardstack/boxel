ef49a2ca0e8b7a13f737d2c9b26aabb364e735c1
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-08-01 08:28:45 -0400

    fixed save issue

--- packages/host/tests/unit/index-writer-test.ts
+++ packages/host/tests/unit/index-writer-test.ts
@@ -1,24 +1,27 @@
 import { module, test } from 'qunit';
-import { prepareTestDB } from './helpers';
+
 import { IndexWriter, IndexQueryEngine } from '@cardstack/runtime-common';
 import { runSharedTest } from '@cardstack/runtime-common/helpers';
-import PgAdapter from '../pg-adapter';
+// eslint-disable-next-line ember/no-test-import-export
 import indexWriterTests from '@cardstack/runtime-common/tests/index-writer-test';
 
-module('index-writer', function (hooks) {
-  let adapter: PgAdapter;
+import type SQLiteAdapter from '@cardstack/host/lib/sqlite-adapter';
+
+import { getDbAdapter } from '../helpers';
+
+module('Unit | index-writer', function (hooks) {
+  let adapter: SQLiteAdapter;
   let indexWriter: IndexWriter;
   let indexQueryEngine: IndexQueryEngine;
 
-  hooks.beforeEach(async function () {
-    prepareTestDB();
-    adapter = new PgAdapter();
-    indexWriter = new IndexWriter(adapter);
-    indexQueryEngine = new IndexQueryEngine(adapter);
+  hooks.before(async function () {
+    adapter = await getDbAdapter();
   });
 
-  hooks.afterEach(async function () {
-    await adapter.close();
+  hooks.beforeEach(async function () {
+    await adapter.reset();
+    indexWriter = new IndexWriter(adapter);
+    indexQueryEngine = new IndexQueryEngine(adapter);
   });
 
   test('can perform invalidations for a instance entry', async function (assert) {
