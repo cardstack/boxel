84103cb16920ed4c30f51b5012f5b9d2ee9806a2
Edward Faulkner <edward@eaf4.com>
2024-08-29 14:00:06 -0400

    fix other outside usages of sdk

--- packages/host/app/lib/matrix-handlers/membership.ts
+++ packages/host/app/lib/matrix-handlers/membership.ts
@@ -1,5 +1,4 @@
 import debounce from 'lodash/debounce';
-import { type MatrixEvent, type RoomMember } from 'matrix-js-sdk';
 
 import type { MatrixEvent as DiscreteMatrixEvent } from 'https://cardstack.com/base/matrix-event';
 
@@ -8,6 +7,7 @@ import { eventDebounceMs } from '../matrix-utils';
 import { addRoomEvent } from './index';
 
 import type MatrixService from '../../services/matrix-service';
+import type { Direction, MatrixEvent, RoomMember } from 'matrix-js-sdk';
 
 export function onMembership(MatrixService: MatrixService) {
   return (event: MatrixEvent, member: RoomMember) => {
@@ -65,29 +65,26 @@ async function drainMembership(MatrixService: MatrixService) {
       room.getMyMembership() === 'invite'
     ) {
       if (event.content.membership === 'invite') {
-        let forwardTimeline = MatrixService.matrixSDK?.EventTimeline.FORWARDS;
-        if (forwardTimeline) {
-          let stateEvents = room.getLiveTimeline().getState(forwardTimeline)
-            ?.events;
-          if (!stateEvents) {
-            throw new Error(`bug: cannot get state events for room ${roomId}`);
+        let stateEvents = room.getLiveTimeline().getState('f' as Direction)
+          ?.events;
+        if (!stateEvents) {
+          throw new Error(`bug: cannot get state events for room ${roomId}`);
+        }
+        for (let eventType of STATE_EVENTS_OF_INTEREST) {
+          let events = stateEvents.get(eventType);
+          if (!events) {
+            continue;
           }
-          for (let eventType of STATE_EVENTS_OF_INTEREST) {
-            let events = stateEvents.get(eventType);
-            if (!events) {
-              continue;
-            }
-            await Promise.all(
-              [...events.values()]
-                .map((e) => ({
-                  ...e.event,
-                  // annoyingly these events have been stripped of their id's
-                  event_id: `${roomId}_${eventType}_${e.localTimestamp}`,
-                  status: e.status,
-                }))
-                .map((event) => addRoomEvent(MatrixService, event)),
-            );
-          }
+          await Promise.all(
+            [...events.values()]
+              .map((e) => ({
+                ...e.event,
+                // annoyingly these events have been stripped of their id's
+                event_id: `${roomId}_${eventType}_${e.localTimestamp}`,
+                status: e.status,
+              }))
+              .map((event) => addRoomEvent(MatrixService, event)),
+          );
         }
       }
     }
