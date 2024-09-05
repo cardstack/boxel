5ed3e39ec8b2e283278ccd73a0defb26afbb6491
Buck Doyle <buck.doyle@cardstack.com>
2024-09-05 16:44:14 -0500

    Remove redundant rules

--- packages/host/.template-lintrc.js
+++ packages/host/.template-lintrc.js
@@ -3,14 +3,4 @@
 module.exports = {
   extends: ['recommended', '@cardstack/template-lint:recommended'],
   plugins: ['../template-lint/plugin'],
-  rules: {
-    'require-button-type': false,
-    'no-negated-condition': false,
-
-    // https://github.com/ember-template-lint/ember-template-lint/issues/2785
-    'no-implicit-this': false,
-
-    // We need this to be able to use <style scoped> tags in our components for scoped CSS
-    'no-forbidden-elements': ['meta', 'html', 'script'],
-  },
 };
