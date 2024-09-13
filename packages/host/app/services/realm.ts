331a2a319917b21966f2f9792dc1ee0622183608
Buck Doyle <buck.doyle@cardstack.com>
2024-09-13 09:14:50 -0500

    Restore function privacy

--- packages/host/app/services/realm.ts
+++ packages/host/app/services/realm.ts
@@ -374,8 +374,7 @@ export default class RealmService extends Service {
     return resource;
   }
 
-  // FIXME this was private
-  getOrCreateRealmResource(realmURL: string): RealmResource {
+  private getOrCreateRealmResource(realmURL: string): RealmResource {
     // this should be the only place we do the untracked read. It needs to be
     // untracked so our `this.realms.set` below will not be an assertion.
     let resource = this.knownRealm(realmURL, false);
