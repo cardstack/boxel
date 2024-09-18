34bf107300c5524e3f720ae51831f3703be3a546
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-09-17 23:07:50 -0400

    typo in permissions

--- packages/realm-server/migrations/1726620529359_backfill-realm-owners.js
+++ packages/realm-server/migrations/1726620529359_backfill-realm-owners.js
@@ -7,7 +7,7 @@ exports.up = (pgm) => {
            ('https://realms-staging.stack.cards/seed/', '@seed_realm:stack.cards', true, true, true),
            ('https://realms-staging.stack.cards/experiments/', '@experiments_realm:stack.cards', true, true, true),
            ('https://realms-staging.stack.cards/experiments/', '*', true, false, false),
-           ('https://realms-staging.stack.cards/experiments/', 'user', true, true, false),
+           ('https://realms-staging.stack.cards/experiments/', 'users', true, true, false),
            ('https://cardstack.com/base/', '@base_realm:stack.cards', true, true, true),
            ('https://cardstack.com/base/', '*', true, false, false)`,
       );
@@ -19,7 +19,7 @@ exports.up = (pgm) => {
            ('https://app.boxel.ai/seed/', '@seed_realm:boxel.ai', true, true, true),
            ('https://app.boxel.ai/experiments/', '@experiments_realm:boxel.ai', true, true, true),
            ('https://app.boxel.ai/experiments/', '*', true, false, false),
-           ('https://app.boxel.ai/experiments/', 'user', true, true, false),
+           ('https://app.boxel.ai/experiments/', 'users', true, true, false),
            ('https://cardstack.com/base/', '@base_realm:boxel.ai', true, true, true),
            ('https://cardstack.com/base/', '*', true, false, false)`,
       );
@@ -33,18 +33,18 @@ exports.up = (pgm) => {
            ('http://localhost:4201/seed/', '@seed_realm:localhost', true, true, true),
            ('http://localhost:4201/experiments/', '@experiments_realm:localhost', true, true, true),
            ('http://localhost:4201/experiments/', '*', true, false, false),
-           ('http://localhost:4201/experiments/', 'user', true, true, false),
+           ('http://localhost:4201/experiments/', 'users', true, true, false),
            ('http://localhost:4202/test/', '@test_realm:localhost', true, true, true),
            ('http://localhost:4202/test/', '*', true, false, false),
-           ('http://localhost:4202/test/', 'user', true, true, false),
+           ('http://localhost:4202/test/', 'users', true, true, false),
            ('http://localhost:4202/node-test/', '@node-test_realm:localhost', true, true, true),
            ('http://localhost:4202/node-test/', '*', true, false, false),
-           ('http://localhost:4202/node-test/', 'user', true, true, false),
+           ('http://localhost:4202/node-test/', 'users', true, true, false),
            ('http://localhost:4203/', '@base_realm:localhost', true, true, true),
            ('http://localhost:4203/', '*', true, false, false),
            ('http://localhost:4204/', '@experiments_realm:localhost', true, true, true),
            ('http://localhost:4204/', '*', true, false, false),
-           ('http://localhost:4204/', 'user', true, true, false)`,
+           ('http://localhost:4204/', 'users', true, true, false)`,
       );
   }
 };
