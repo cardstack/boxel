63cc5bcad825e2919e8b6b9eaff18268261737ca
Buck Doyle <buck.doyle@cardstack.com>
2024-09-20 07:33:03 -0500

    Add local variable logging
    A test!

--- packages/realm-server/main.ts
+++ packages/realm-server/main.ts
@@ -27,6 +27,7 @@ if (process.env.REALM_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.REALM_SENTRY_DSN,
     environment: process.env.REALM_SENTRY_ENVIRONMENT || 'development',
+    includeLocalVariables: true,
   });
 
   setErrorReporter(Sentry.captureException);
