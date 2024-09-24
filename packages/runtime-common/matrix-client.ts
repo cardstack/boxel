6ce2ed3b0ae9809a3392348dcbd78787dad8021d
Buck Doyle <buck.doyle@cardstack.com>
2024-09-24 11:01:33 -0500

    Remove handling for rate-limiting

--- packages/runtime-common/matrix-client.ts
+++ packages/runtime-common/matrix-client.ts
@@ -99,15 +99,6 @@ export class MatrixClient {
     let json = await response.json();
 
     if (!response.ok) {
-      if (json['errcode'] === 'M_LIMIT_EXCEEDED') {
-        let retryAfter = json['retry_after_ms'];
-        this.#log.warn(
-          `Matrix rate limit exceeded, retrying after ${retryAfter}ms`,
-        );
-        await new Promise((res) => setTimeout(res, retryAfter));
-        return this.login();
-      }
-
       throw new Error(
         `Unable to login to matrix ${this.matrixURL.href} as user ${
           this.username
