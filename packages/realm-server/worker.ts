244d9653d192270220e93a5711f4f9f6e451f679
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-09-27 09:48:47 -0400

    add actual worker IDs

--- packages/realm-server/worker.ts
+++ packages/realm-server/worker.ts
@@ -24,6 +24,15 @@ if (!REALM_SECRET_SEED) {
   process.exit(-1);
 }
 
+// This is an ENV var we get from ECS that looks like:
+// http://169.254.170.2/v3/a1de500d004f49bea02ace30cefb0f01-3236013547 where the
+// last segment is the "container runtime ID", where the value on the left of
+// the '-' is the task ID.
+const ECS_CONTAINER_METADATA_URI = process.env.ECS_CONTAINER_METADATA_URI;
+let workerId = ECS_CONTAINER_METADATA_URI
+  ? ECS_CONTAINER_METADATA_URI.split('/').pop()!
+  : 'realm_worker';
+
 let {
   port,
   matrixURL,
@@ -85,7 +94,7 @@ let dist: URL = new URL(distURL);
 
 (async () => {
   let dbAdapter = new PgAdapter();
-  let queue = new PgQueueRunner(dbAdapter, 'realm-worker'); // TODO: update worker ID with ecs task ID
+  let queue = new PgQueueRunner(dbAdapter, workerId);
   let manager = new RunnerOptionsManager();
   let { getRunner } = await makeFastBootIndexRunner(
     dist,
