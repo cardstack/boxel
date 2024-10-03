ff39d75694f4222f2753f08d4fd27db4342b38c8
Buck Doyle <buck.doyle@cardstack.com>
2024-10-03 12:28:26 -0500

    Add preliminary test

--- packages/vscode-boxel-tools/esbuild.mjs
+++ packages/vscode-boxel-tools/esbuild.mjs
@@ -5,14 +5,14 @@ const watch = process.argv.includes('--watch');
 
 async function main() {
   const ctx = await context({
-    entryPoints: ['src/extension.ts'],
+    entryPoints: ['src/extension.ts', 'src/test/extension.test.ts'],
     bundle: true,
     format: 'cjs',
     minify: production,
     sourcemap: !production,
     sourcesContent: false,
     platform: 'node',
-    outfile: 'dist/extension.js',
+    outdir: 'dist',
     external: ['vscode'],
     logLevel: 'silent',
     plugins: [
