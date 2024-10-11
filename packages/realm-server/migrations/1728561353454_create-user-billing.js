37c16fe752d0b7cc5dcb317a38e09860696afa97
Matic Jurglic <matic@jurglic.si>
2024-10-11 12:44:56 +0200

    Add plan id to user so that we can assign the free plan (which does not have a subscription)

--- packages/realm-server/migrations/1728561353454_create-user-billing.js
+++ packages/realm-server/migrations/1728561353454_create-user-billing.js
@@ -1,18 +1,4 @@
 exports.up = (pgm) => {
-  pgm.createTable('users', {
-    id: {
-      type: 'serial',
-      primaryKey: true,
-    },
-    matrix_user_id: { type: 'varchar', notNull: true, unique: true },
-    stripe_customer_id: { type: 'varchar' },
-    created_at: {
-      type: 'timestamp',
-      notNull: true,
-      default: pgm.func('current_timestamp'),
-    },
-  });
-
   pgm.createTable('plans', {
     id: {
       type: 'serial',
@@ -22,6 +8,21 @@ exports.up = (pgm) => {
     monthly_price: { type: 'numeric(19,4)', notNull: true },
     credits_included: { type: 'integer', notNull: true },
     overage_fee_per_credit_in_usd: { type: 'numeric(19,4)' },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+
+  pgm.createTable('users', {
+    id: {
+      type: 'serial',
+      primaryKey: true,
+    },
+    matrix_user_id: { type: 'varchar', notNull: true, unique: true },
+    stripe_customer_id: { type: 'varchar' },
+    plan_id: { type: 'integer', references: 'plans(id)', notNull: true },
     created_at: {
       type: 'timestamp',
       notNull: true,
