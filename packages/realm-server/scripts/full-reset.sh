2faf61518f75914be63facabc17aaf68d8ecde24
Buck Doyle <buck.doyle@cardstack.com>
2024-11-08 13:20:20 -0600

    Add error handling to reset script

--- packages/realm-server/scripts/full-reset.sh
+++ packages/realm-server/scripts/full-reset.sh
@@ -2,29 +2,47 @@
 CURRENT_DIR="$(pwd)"
 SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-cd ${SCRIPTS_DIR}/../../postgres
-pnpm run drop-db boxel
-pnpm run drop-db boxel_test
-pnpm run drop-db boxel_base
+errors=()
 
-# clearing the DB means that we also lose all the info we have on the realm
-# owners of the dynamic realms, which means that we should eliminate these as
-# well.
-rm -rf "${SCRIPTS_DIR}/../realms"
+run_command() {
+    "$@"
+    if [ $? -ne 0 ]; then
+        errors+=("Failed: $*")
+    fi
+}
 
-# also now you will have users that have realm associations to realms that don't
-# exist anymore, so we should clear the matrix state so it can be in sync with
-# the DB state
-cd "${SCRIPTS_DIR}/../../matrix"
-pnpm stop:synapse
-rm -rf ./synapse-data
-pnpm start:synapse
-pnpm register-all
+cd ${SCRIPTS_DIR}/../../postgres || errors+=("Failed: changing to postgres directory")
+run_command pnpm run drop-db boxel
+run_command pnpm run drop-db boxel_test
+run_command pnpm run drop-db boxel_base
 
-cd "${CURRENT_DIR}"
+if ! rm -rf "${SCRIPTS_DIR}/../realms"; then
+    errors+=("Failed: removing realms directory")
+fi
+
+cd "${SCRIPTS_DIR}/../../matrix" || errors+=("Failed: changing to matrix directory")
+
+run_command pnpm stop:synapse
+
+if ! rm -rf ./synapse-data; then
+    errors+=("Failed: removing synapse-data")
+fi
+
+run_command pnpm start:synapse
+run_command pnpm register-all
+
+cd "${CURRENT_DIR}" || errors+=("Failed: returning to original directory")
 
 echo "
 WARNING: Any matrix server authorization tokens cached in the browser's localstorage are now invalid. Make sure to clear browser localstorage. Also make sure to execute the following in the browser after logging in as 'user' to add the experiments realm:
 
 window['@cardstack/host'].lookup('service:matrix-service')._client.setAccountData('com.cardstack.boxel.realms', {realms: ['http://localhost:4201/experiments/']})
 "
+
+if [ ${#errors[@]} -ne 0 ]; then
+    echo "\nThe following errors occurred during execution:"
+    printf '%s\n' "${errors[@]}"
+    exit 1
+else
+    echo "\nAll operations completed successfully."
+fi
