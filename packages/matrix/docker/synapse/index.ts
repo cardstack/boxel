34d5bcce67405da13d4303b2cbe40c9f26469d95
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-11-07 21:38:52 -0500

    updated to latest released version of synapse

--- packages/matrix/docker/synapse/index.ts
+++ packages/matrix/docker/synapse/index.ts
@@ -126,7 +126,7 @@ export async function synapseStart(
   );
   await dockerCreateNetwork({ networkName: 'boxel' });
   const synapseId = await dockerRun({
-    image: 'matrixdotorg/synapse:v1.109.0',
+    image: 'matrixdotorg/synapse:v1.118.0',
     containerName: 'boxel-synapse',
     dockerParams: [
       '--rm',
