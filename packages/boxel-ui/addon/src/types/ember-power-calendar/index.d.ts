ff6c613feeb18aea52f5db1d485fc696e1f9866a
tintinthong <justinthong93@gmail.com>
2024-11-12 01:44:11 +0800

    add type

---
+++ packages/boxel-ui/addon/src/types/ember-power-calendar/index.d.ts
@@ -0,0 +1,10 @@
+import PowerCalendar from 'ember-power-calendar/components/power-calendar';
+import DayComponent from 'ember-power-calendar/components/days';
+import NavComponent from 'ember-power-calendar/components/nav';
+
+declare module 'ember-power-calendar/components/power-calendar-range' {
+  interface PowerCalendarRangeDefaultBlock {
+    Days: typeof DayComponent;
+    Nav: typeof NavComponent;
+  }
+}
