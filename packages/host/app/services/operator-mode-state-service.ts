cd6f6833e5a2ce21842f70e8fbf71d3cde509189
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2024-11-15 14:57:35 -0500

    helpful comment

--- packages/host/app/services/operator-mode-state-service.ts
+++ packages/host/app/services/operator-mode-state-service.ts
@@ -402,6 +402,9 @@ export default class OperatorModeStateService extends Service {
 
   private persist() {
     this.operatorModeController.operatorModeState = this.serialize();
+    // This sets the title of the document for it's appearance in the browser
+    // history (which needs to happen after the history pushState)--the
+    // afterRender is the perfect place for this
     document.title = this.title;
   }
 
