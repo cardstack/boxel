f83d311b68bdda81384c7a6c63a9a6433e1ae606
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2024-11-18 17:40:49 +0700

    remove unnecessary comment

--- packages/billing/stripe-webhook-handlers/stripe.ts
+++ packages/billing/stripe-webhook-handlers/stripe.ts
@@ -1,7 +1,6 @@
 import Stripe from 'stripe';
 
 let stripe: Stripe;
-// Need to export this function so the stripe instance can be mocked in test
 export function getStripe() {
   if (!process.env.STRIPE_WEBHOOK_SECRET) {
     throw new Error('STRIPE_WEBHOOK_SECRET is not set');
