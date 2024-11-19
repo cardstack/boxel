bc5e24fecbf4916bf2fd338665d13fbc97b6fc55
Matic Jurglic <matic@jurglic.si>
2024-11-19 13:01:25 +0100

    Revert default pg config, provide config with the start command

--- packages/postgres/pg-config.ts
+++ packages/postgres/pg-config.ts
@@ -3,9 +3,9 @@ import { type ClientConfig } from 'pg';
 export function postgresConfig(defaultConfig: ClientConfig = {}) {
   return Object.assign({}, defaultConfig, {
     host: process.env.PGHOST || 'localhost',
-    port: process.env.PGPORT || '5435',
+    port: process.env.PGPORT || '5432',
     user: process.env.PGUSER || 'postgres',
     password: process.env.PGPASSWORD || undefined,
-    database: process.env.PGDATABASE || 'boxel',
+    database: process.env.PGDATABASE || 'postgres',
   });
 }
