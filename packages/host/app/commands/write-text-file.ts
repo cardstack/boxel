d5bc2f2e733c3fadb49ad77aa28bc1c31c85a964
Ian Calvert <ianjcalvert@gmail.com>
2024-12-03 14:23:02 +0000

    Add the file writing command

---
+++ packages/host/app/commands/write-text-file.ts
@@ -0,0 +1,44 @@
+import { service } from '@ember/service';
+
+import type * as BaseCommandModule from 'https://cardstack.com/base/command';
+
+import HostBaseCommand from '../lib/host-base-command';
+import NetworkService from '../services/network';
+
+export default class WriteTextFileCommand extends HostBaseCommand<
+  BaseCommandModule.WriteTextFileInput,
+  undefined,
+  undefined
+> {
+  @service private declare network: NetworkService;
+
+  description = `Write a text file to a realm, such as a module or a card.`;
+
+  async getInputType() {
+    let commandModule = await this.loadCommandModule();
+    const { WriteTextFileInput } = commandModule;
+    return WriteTextFileInput;
+  }
+
+  protected async run(
+    input: BaseCommandModule.WriteTextFileInput,
+  ): Promise<undefined> {
+    let url = new URL(input.path, input.realm);
+    if (!input.overwrite) {
+      let existing = await this.network.authedFetch(url);
+      if (existing.status != 404) {
+        throw new Error(`File already exists: ${input.path}`);
+      }
+    }
+    let response = await this.network.authedFetch(url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/vnd.card+source',
+      },
+      body: input.content,
+    });
+    if (!response.ok) {
+      throw new Error(`Failed to write file ${url}: ${response.statusText}`);
+    }
+  }
+}
