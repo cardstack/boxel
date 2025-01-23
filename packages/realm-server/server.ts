09375bdd6e1b61609949c7e5ab02a78b4182207e
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2025-01-23 21:35:59 +0700

    lint fix

--- packages/realm-server/server.ts
+++ packages/realm-server/server.ts
@@ -382,7 +382,7 @@ export class RealmServer {
       {
         ...(this.seedRealmURL && copyFromSeedRealm
           ? {
-              copiedFromRealm: this.seedRealmURL
+              copiedFromRealm: this.seedRealmURL,
             }
           : {}),
       },
