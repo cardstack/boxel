892350bf015c57dee26f67fd1967340c900854ee
Matic Jurglic <matic@jurglic.si>
2025-01-28 11:13:48 +0100

    When making a prompt with history, do not include events that are still streaming (Thinking...)

--- packages/ai-bot/helpers.ts
+++ packages/ai-bot/helpers.ts
@@ -452,6 +452,12 @@ export function getModifyPrompt(
     if (isCommandResultEvent(event)) {
       continue;
     }
+    if (
+      'isStreamingFinished' in event.content &&
+      event.content.isStreamingFinished === false
+    ) {
+      continue;
+    }
     let body = event.content.body;
     if (body) {
       if (event.sender === aiBotUserId) {
@@ -619,23 +625,4 @@ export function eventRequiresResponse(event: MatrixEvent) {
 
   // If it's a different type, or a command result without output, we should not respond
   return false;
-}
-
-// Helps reduce the prompt size (prompt token cost) by removing the thinking message indicators
-export function cleanupPromptParts(promptParts: PromptParts): PromptParts {
-  return {
-    ...promptParts,
-    history: promptParts.history.filter(
-      (item) =>
-        !(
-          item.sender === '@aibot:localhost' &&
-          'body' in item.content &&
-          item.content.body === thinkingMessage
-        ),
-    ),
-    messages: promptParts.messages.filter(
-      (message) =>
-        !(message.role === 'assistant' && message.content === thinkingMessage),
-    ),
-  };
 }
