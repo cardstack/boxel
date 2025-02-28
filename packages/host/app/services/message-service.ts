ac1f6daf6e991fe352078bab20a542454b6ff59b
Buck Doyle <buck.doyle@cardstack.com>
2025-02-28 12:41:13 -0800

    Remove old subscription code

--- packages/host/app/services/message-service.ts
+++ packages/host/app/services/message-service.ts
@@ -1,16 +1,8 @@
 import Service, { service } from '@ember/service';
 
-import { isTesting } from '@embroider/macros';
-
 import { tracked } from '@glimmer/tracking';
 
-import window from 'ember-window-mock';
-
-import qs from 'qs';
-
 import type { RealmEventEventContent } from 'https://cardstack.com/base/matrix-event';
-
-import { SessionLocalStorageKey } from '../utils/local-storage-keys';
 
 import type NetworkService from './network';
 
@@ -30,63 +22,18 @@ export default class MessageService extends Service {
     realmURL: string,
     cb: (ev: RealmEventEventContent) => void,
   ): () => void {
-    console.log('subscribe', realmURL, cb);
-    if (isTesting()) {
-      // only use Matrix-y callbacks in testing
-      if (!this.listenerCallbacks.has(realmURL)) {
-        this.listenerCallbacks.set(realmURL, []);
-      }
-      this.listenerCallbacks.get(realmURL)?.push(cb);
-      return () => {};
-    }
-
-    let maybeEventSource = this.subscriptions.get(realmURL);
-
-    if (!maybeEventSource) {
-      let token = getPersistedTokenForRealm(realmURL);
-      if (!token) {
-        throw new Error(`Could not find JWT for realm ${realmURL}`);
-      }
-      let urlWithAuth = `${realmURL}_message?${qs.stringify({
-        authHeader: 'Bearer ' + token,
-      })}`;
-      maybeEventSource = this.network.createEventSource(urlWithAuth);
-      maybeEventSource.onerror = () => eventSource.close();
-      this.subscriptions.set(realmURL, maybeEventSource);
-    }
-
-    let eventSource = maybeEventSource;
-    // TODO might want to consider making separate subscription methods so that
-    // you can subscribe to a specific type of events instead of all of the
-    // events...
-
     if (!this.listenerCallbacks.has(realmURL)) {
       this.listenerCallbacks.set(realmURL, []);
     }
     this.listenerCallbacks.get(realmURL)?.push(cb);
-
-    eventSource.addEventListener('update', cb);
-    eventSource.addEventListener('index', cb);
-    return () => {
-      eventSource.removeEventListener('update', cb);
-      eventSource.removeEventListener('index', cb);
-    };
+    return () => {};
   }
 
   relayMatrixSSE(realmURL: string, event: RealmEventEventContent) {
     console.log('relaying matrix sse event', realmURL, event);
+    console.log('listener callbacks', this.listenerCallbacks);
     this.listenerCallbacks.get(realmURL)?.forEach((cb) => {
       cb(event);
     });
   }
-}
-
-function getPersistedTokenForRealm(realmURL: string) {
-  if (isTesting()) {
-    return 'TEST_TOKEN';
-  }
-
-  let sessionStr = window.localStorage.getItem(SessionLocalStorageKey) ?? '{}';
-  let session = JSON.parse(sessionStr);
-  return session[realmURL] as string | undefined;
 }
