46302f3def4c35fbc6759e0ee95e21477ef1db38
Buck Doyle <buck.doyle@cardstack.com>
2025-03-17 13:06:46 -0700

    Remove feature flag emptiness enforcement
    I’m confused why they should always be erased in development.

--- packages/host/config/environment.js
+++ packages/host/config/environment.js
@@ -53,7 +53,6 @@ module.exports = function (environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV.featureFlags = {};
   }
 
   if (environment === 'test') {
