f0cc15c02f6350d79afd1acefe85d5b8bab27673
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2025-03-19 13:59:02 -0400

    update comment

--- packages/host/app/services/store.ts
+++ packages/host/app/services/store.ts
@@ -222,8 +222,7 @@ export default class StoreService extends Service {
 
   // This method is used for specific scenarios where you just want an instance
   // that is _not_ part of the identity map and detached from the store. This
-  // may include things like tests, freestyle guide, perhaps you just need a
-  // couple attributes from an instance and that's it, etc.
+  // may include things like tests, freestyle guide, etc.
   async getInstanceDetachedFromStore(
     url: string,
   ): Promise<CardDef | undefined> {
