0d134c47b207f8b36b46ff2eb7ea3d1619592862
Buck Doyle <buck.doyle@cardstack.com>
2025-03-24 09:29:14 -0700

    Add more repetitions

--- packages/matrix/playwright.config.ts
+++ packages/matrix/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   // forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  repeatEach: 10,
+  repeatEach: 20,
   workers: 1,
   reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
