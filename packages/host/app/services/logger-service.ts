92c10ba3286e4a8a6d43811504051046d3e67ce9
Buck Doyle <buck.doyle@cardstack.com>
2025-04-10 12:28:55 -0600

    Fix how logger is obtained
    Using logger from runtime-common doesn’t let you set the
    level properly.

--- packages/host/app/services/logger-service.ts
+++ packages/host/app/services/logger-service.ts
@@ -2,11 +2,9 @@ import Service from '@ember/service';
 
 import LogLevel from 'loglevel';
 
-import { logger } from '@cardstack/runtime-common';
-
 export default class LoggerService extends Service {
   getLogger(name: string) {
-    return logger(name);
+    return LogLevel.getLogger(name);
   }
 
   getLoggers() {
