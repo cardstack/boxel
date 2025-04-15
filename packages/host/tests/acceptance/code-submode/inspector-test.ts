ccdd2337d9e12cffcd8088c1d695db4a5f62ddf5
Luke Melia <luke@lukemelia.com>
2025-04-15 10:30:43 -0400

    Eliminate commented empty templates from CardDef template

--- packages/host/tests/acceptance/code-submode/inspector-test.ts
+++ packages/host/tests/acceptance/code-submode/inspector-test.ts
@@ -1704,28 +1704,6 @@ import { ExportedCard } from './in-this-file';
 import { Component } from 'https://cardstack.com/base/card-api';
 export class TestCard extends ExportedCard {
   static displayName = "Test Card";
-
-  /*
-  static isolated = class Isolated extends Component<typeof this> {
-    <template></template>
-  }
-
-  static embedded = class Embedded extends Component<typeof this> {
-    <template></template>
-  }
-
-  static atom = class Atom extends Component<typeof this> {
-    <template></template>
-  }
-
-  static edit = class Edit extends Component<typeof this> {
-    <template></template>
-  }
-
-  static fitted = class Fitted extends Component<typeof this> {
-    <template></template>
-  }
-  */
 }`.trim();
     await visitOperatorMode({
       stacks: [[]],
@@ -1837,24 +1815,6 @@ import { ExportedField } from './in-this-file';
 import { Component } from 'https://cardstack.com/base/card-api';
 export class TestField extends ExportedField {
   static displayName = "Test Field";
-
-  /*
-  static embedded = class Embedded extends Component<typeof this> {
-    <template></template>
-  }
-
-  static atom = class Atom extends Component<typeof this> {
-    <template></template>
-  }
-
-  static edit = class Edit extends Component<typeof this> {
-    <template></template>
-  }
-
-  static fitted = class Fitted extends Component<typeof this> {
-    <template></template>
-  }
-  */
 }`.trim(),
         'the source is correct',
       );
@@ -1915,28 +1875,6 @@ import { ExportedCard as ExportedCardParent } from './in-this-file';
 import { Component } from 'https://cardstack.com/base/card-api';
 export class ExportedCard extends ExportedCardParent {
   static displayName = "Exported Card";
-
-  /*
-  static isolated = class Isolated extends Component<typeof this> {
-    <template></template>
-  }
-
-  static embedded = class Embedded extends Component<typeof this> {
-    <template></template>
-  }
-
-  static atom = class Atom extends Component<typeof this> {
-    <template></template>
-  }
-
-  static edit = class Edit extends Component<typeof this> {
-    <template></template>
-  }
-
-  static fitted = class Fitted extends Component<typeof this> {
-    <template></template>
-  }
-  */
 }`.trim();
     await visitOperatorMode({
       submode: 'code',
