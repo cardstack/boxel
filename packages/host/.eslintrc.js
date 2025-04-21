b473f681affe03e2837db551efac7e07c2560781
Luke Melia <luke@lukemelia.com>
2025-04-21 11:37:45 -0400

    Avoid duplication of missing invokables configuration

--- packages/host/.eslintrc.js
+++ packages/host/.eslintrc.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const MISSING_INVOKABLES_CONFIG = require('../../eslint/missing-invokables-config');
+
 module.exports = {
   root: true,
   env: {
@@ -94,26 +96,11 @@ module.exports = {
         'ember/no-tracked-properties-from-args': 'off',
         'ember/no-runloop': 'off',
         'node/no-deprecated-api': 'off',
-        'deprecation/deprecation': 'off',
         'window-mock/mock-window-only': 'error',
         'window-mock/wrapped-setup-helpers-only': 'error',
         '@cardstack/boxel/template-missing-invokable': [
           'error',
-          {
-            invokables: {
-              fn: ['fn', '@ember/helper'],
-              on: ['on', '@ember/modifier'],
-              and: ['and', '@cardstack/boxel-ui/helpers'],
-              bool: ['bool', '@cardstack/boxel-ui/helpers'],
-              eq: ['eq', '@cardstack/boxel-ui/helpers'],
-              gt: ['gt', '@cardstack/boxel-ui/helpers'],
-              lt: ['lt', '@cardstack/boxel-ui/helpers'],
-              not: ['not', '@cardstack/boxel-ui/helpers'],
-              or: ['or', '@cardstack/boxel-ui/helpers'],
-              add: ['add', '@cardstack/boxel-ui/helpers'],
-              subtract: ['subtract', '@cardstack/boxel-ui/helpers'],
-            },
-          },
+          { invokables: MISSING_INVOKABLES_CONFIG.invokables },
         ],
       },
     },
