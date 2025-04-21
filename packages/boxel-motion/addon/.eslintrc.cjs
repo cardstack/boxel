ded2669e5e3d6f1fb97e377bf67cf8e1d68558c6
Luke Melia <luke@lukemelia.com>
2025-04-21 11:44:24 -0400

    Fix linting in boxel-motion

--- packages/boxel-motion/addon/.eslintrc.cjs
+++ packages/boxel-motion/addon/.eslintrc.cjs
@@ -43,6 +43,52 @@ module.exports = {
     ],
   },
   overrides: [
+    {
+      files: ['**/*.gts'],
+      parser: 'ember-eslint-parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        requireConfigFile: false,
+        babelOptions: {
+          plugins: [
+            [
+              '@babel/plugin-proposal-decorators',
+              { decoratorsBeforeExport: true },
+            ],
+          ],
+        },
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+      plugins: ['ember'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:ember/recommended',
+        'plugin:ember/recommended-gts',
+        'plugin:prettier/recommended',
+        'plugin:qunit-dom/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+        ],
+        'prefer-const': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-this-alias': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        'no-undef': 'off',
+        'ember/template-no-let-reference': 'off',
+        'ember/no-tracked-properties-from-args': 'off',
+        'ember/no-runloop': 'off',
+        'node/no-deprecated-api': 'off',
+      },
+    },
     // node files
     {
       files: [
