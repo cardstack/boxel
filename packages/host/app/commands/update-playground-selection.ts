358e82d06d3c3b4c7f47283aa62205c83f21c5dd
Richard Tan <hjtan3333@gmail.com>
2025-04-29 14:09:19 +0800

    Add command to persist selection of code ref and playground

---
+++ packages/host/app/commands/update-playground-selection.ts
@@ -0,0 +1,33 @@
+import { service } from '@ember/service';
+
+import type * as BaseCommandModule from 'https://cardstack.com/base/command';
+
+import HostBaseCommand from '../lib/host-base-command';
+
+import { Format } from 'https://cardstack.com/base/card-api';
+
+import type PlaygroundPanelService from '../services/playground-panel-service';
+
+export default class UpdatePlaygroundSelectionCommand extends HostBaseCommand<
+  typeof BaseCommandModule.UpdatePlaygroundSelectionInput
+> {
+  @service declare private playgroundPanelService: PlaygroundPanelService;
+  description = 'Persist the playground selections.';
+
+  async getInputType() {
+    let commandModule = await this.loadCommandModule();
+    const { UpdatePlaygroundSelectionInput } = commandModule;
+    return UpdatePlaygroundSelectionInput;
+  }
+
+  protected async run(
+    input: BaseCommandModule.UpdatePlaygroundSelectionInput,
+  ): Promise<undefined> {
+    this.playgroundPanelService.persistSelections(
+      input.moduleId,
+      input.cardId,
+      input.format as Format,
+      input.fieldIndex,
+    );
+  }
+}
