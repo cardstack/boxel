51297fb044c682d32c18d7d25322d771e6ba2a98
Buck Doyle <buck.doyle@cardstack.com>
2025-05-06 13:23:10 -0500

    Extract Synapse startup

--- packages/realm-server/scripts/start-all.sh
+++ packages/realm-server/scripts/start-all.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
+# FIXME this had start:matrix… how to adapt?
+
 NODE_NO_WARNINGS=1 start-server-and-test \
-  'run-p start:pg start:matrix start:smtp start:worker-development start:development' \
+  'run-p start:pg start:smtp start:worker-development start:development' \
   'http-get://localhost:4201/base/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson|http-get://localhost:4201/experiments/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson|http://localhost:8008|http://localhost:5001' \
   'run-p start:worker-test start:test-realms' \
   'http-get://localhost:4202/node-test/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson' \
