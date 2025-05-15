c308879e8769adfacb51b1962f121e8a9b409073
Edward Faulkner <edward@eaf4.com>
2025-05-15 14:45:31 -0400

    use in-progress index

--- packages/host/app/routes/render.ts
+++ packages/host/app/routes/render.ts
@@ -6,6 +6,7 @@ import { isCardInstance } from '@cardstack/runtime-common';
 
 import type { CardDef } from 'https://cardstack.com/base/card-api.gts';
 
+import LoaderService from '../services/loader-service';
 import StoreService from '../services/store';
 
 export type Model = CardDef;
@@ -13,13 +14,19 @@ export type Model = CardDef;
 export default class RenderRoute extends Route<Model> {
   @service declare store: StoreService;
   @service declare router: RouterService;
+  @service declare loaderService: LoaderService;
 
   async model({ id }: { id: string }) {
+    // Make it easy for Puppeteer to do regular Ember transitions
     (globalThis as any).boxelTransitionTo = (
       ...args: Parameters<RouterService['transitionTo']>
     ) => {
       this.router.transitionTo(...args);
     };
+
+    // Opt in to reading the in-progress index, as opposed to the last completed index.
+    this.loaderService.setIsIndexing(true);
+
     let instance = await this.store.get(id);
     if (!isCardInstance(instance)) {
       throw new Error('todo: failed to load');
