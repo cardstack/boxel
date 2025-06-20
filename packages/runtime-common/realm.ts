c1a855c7b374aba98068d35b02390d6ea71f2802
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2025-06-20 09:50:46 -0400

    add debug

--- packages/runtime-common/realm.ts
+++ packages/runtime-common/realm.ts
@@ -1021,7 +1021,7 @@ export class Realm {
 
       if (!(await realmPermissionChecker.can(user, requiredPermission))) {
         console.error(
-          `encountered insufficient permissions: token user=${token.user}, assumedUser=${assumedUser}, realm permissions: ${JSON.stringify(realmPermissions, null, 2)}`,
+          `encountered insufficient permissions: token user=${token.user}, assumedUser=${assumedUser}, effective user=${user} realm permissions: ${JSON.stringify(realmPermissions, null, 2)}`,
         );
         throw new AuthorizationError(
           'Insufficient permissions to perform this action',
