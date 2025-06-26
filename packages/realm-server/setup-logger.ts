a01e8a25b879895a6721589c449ec4cb9270f382
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2025-06-26 10:17:09 -0400

    Add separate env var for worker log levels

--- packages/realm-server/setup-logger.ts
+++ packages/realm-server/setup-logger.ts
@@ -1,4 +1,4 @@
 import { makeLogDefinitions } from '@cardstack/runtime-common';
 (globalThis as any)._logDefinitions = makeLogDefinitions(
-  process.env.LOG_LEVELS || '*=info',
+  process.env.WORKER_LOG_LEVELS || process.env.LOG_LEVELS || '*=info',
 );
