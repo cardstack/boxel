98f3de69b8dffc44687189a3a2f85da70a719d02
Matic Jurglic <matic@jurglic.si>
2025-07-10 13:14:14 +0200

    Add helpful comments

--- packages/host/app/services/local-persistence-service.ts
+++ packages/host/app/services/local-persistence-service.ts
@@ -5,6 +5,9 @@ import { v4 as uuidv4 } from 'uuid';
 import { CurrentRoomIdPersistenceKey } from '../utils/local-storage-keys';
 
 export default class LocalPersistenceService extends Service {
+  // Using sessionStorage for agent id because we want:
+  // - to keep the same agent id if user reloads the page (or tab)
+  // - to get a new agent id if user opens an additional tab
   getAgentId() {
     let agentId = window.sessionStorage.getItem('agentId');
     if (!agentId) {
@@ -14,6 +17,12 @@ export default class LocalPersistenceService extends Service {
     return agentId;
   }
 
+  // Using sessionStorage so that we can keep different current room id for each tab, and
+  // keep the same current room id if user reloads the page (or tab).
+  // Additionally, we use localStorage so that when a user opens a new tab,
+  // the previously most recently used room is selected. But after the user enters another room,
+  // that room gets persisted in sessionStorage and the behaviour from the beginning of this
+  // comment applies again.
   getCurrentRoomId() {
     let currentRoomId = window.sessionStorage.getItem('currentRoomId');
     if (!currentRoomId) {
