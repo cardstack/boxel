f549bff53fb14f31d19f075a2d4bdd3d203a904a
tintinthong <justinthong93@gmail.com>
2025-07-21 23:13:26 +0800

    use a private method so its clear what ephemeral state

--- packages/host/app/services/store.ts
+++ packages/host/app/services/store.ts
@@ -823,6 +823,23 @@ export default class StoreService extends Service implements StoreInterface {
     }
   }
 
+  // this function is used to determine if the instance will be auto-saved or not
+  // this is a temporary function that is likely to go away with the creation of completion emphemeral state solution of the store/realm
+  // the only use-case for this function is determining if a preview instance in catalog realm (which is a read-only),
+  // st a card can be mutable without persisting to the server
+  private useEphemeralState(instance: CardDef | undefined): boolean {
+    if (!instance) {
+      return false;
+    }
+    let realmURL = instance[realmURLSymbol];
+    if (!realmURL) {
+      // if a proper cannot derived, I just revert to the default behaviour of auto-save
+      return false;
+    }
+    let permissionToWrite = this.realm.permissions(realmURL.href).canWrite;
+    return !permissionToWrite;
+  }
+
   private doAutoSave(
     idOrInstance: string | CardDef,
     opts?: { isImmediate?: true },
@@ -836,11 +853,7 @@ export default class StoreService extends Service implements StoreInterface {
     } else {
       instance = idOrInstance;
     }
-    let realmToWrite = instance[realmURLSymbol]?.href;
-    let permissionToWrite = realmToWrite
-      ? this.realm.permissions(realmToWrite).canWrite
-      : false;
-    if (!permissionToWrite) {
+    if (this.useEphemeralState(instance)) {
       return;
     }
     let autoSaveState = this.initOrGetAutoSaveState(instance);
