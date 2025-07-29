cfc9ccb850b356967d506df87edb35e005fd229e
tintinthong <justinthong93@gmail.com>
2025-07-29 11:40:36 +0800

    wip copy-card api

--- packages/host/app/commands/copy-card.ts
+++ packages/host/app/commands/copy-card.ts
@@ -1,10 +1,6 @@
 import { service } from '@ember/service';
 
-import {
-  isCardInstance,
-  isResolvedCodeRef,
-  realmURL,
-} from '@cardstack/runtime-common';
+import { isCardInstance, realmURL } from '@cardstack/runtime-common';
 
 import type * as BaseCommandModule from 'https://cardstack.com/base/command';
 
@@ -33,26 +29,13 @@ export default class CopyCardCommand extends HostBaseCommand<
     return CopyCardInput;
   }
 
-  // Instances that are created via this method are eligible for garbage
-  // collection--meaning that it will be detached from the store. This means you
-  // MUST consume the instance IMMEDIATELY! it should not live in the state of
-  // the consumer.
-  protected async run(
-    input: BaseCommandModule.CopyCardInput,
-  ): Promise<BaseCommandModule.CopyCardResult> {
-    let realm = await this.determineTargetRealm(input);
+  async copyCard(sourceCard: CardDef, realm: string) {
     let doc = await this.cardService.serializeCard(input.sourceCard, {
       useAbsoluteURL: true,
     });
     delete doc.data.id;
-    if (input.codeRef) {
-      if (!isResolvedCodeRef(input.codeRef)) {
-        throw new Error('codeRef is not resolved');
-      }
-      doc.data.meta.adoptsFrom = input.codeRef;
-    }
     let newCardId = await this.store.create(doc, {
-      realm,
+      realm: realmToCopyTo,
       localDir: input.localDir,
     });
     if (typeof newCardId !== 'string') {
@@ -64,22 +47,58 @@ export default class CopyCardCommand extends HostBaseCommand<
         )}`,
       );
     }
-    let commandModule = await this.loadCommandModule();
-    const { CopyCardResult } = commandModule;
-    return new CopyCardResult({ newCardId });
+    return newCardId;
+  }
+
+  // Instances that are created via this method are eligible for garbage
+  // collection--meaning that it will be detached from the store. This means you
+  // MUST consume the instance IMMEDIATELY! it should not live in the state of
+  // the consumer.
+  protected async run(
+    input: BaseCommandModule.CopyCardInput,
+  ): Promise<BaseCommandModule.CopyCardResult> {
+    if (operatorModeStateService.state?.submode === 'interact') {
+      let realmToCopyTo = await this.determineTargetRealm(input);
+      let doc = await this.cardService.serializeCard(input.sourceCard, {
+        useAbsoluteURL: true,
+      });
+      delete doc.data.id;
+      let newCardId = await this.store.create(doc, {
+        realm: realmToCopyTo,
+        localDir: input.localDir,
+      });
+      if (typeof newCardId !== 'string') {
+        throw new Error(
+          `unable to save copied card instance: ${JSON.stringify(
+            newCardId,
+            null,
+            2,
+          )}`,
+        );
+      }
+      let commandModule = await this.loadCommandModule();
+      const { CopyCardResult } = commandModule;
+      return new CopyCardResult({ newCardId });
+    } else if (operatorModeStateService.state?.submode == 'code') {
+    } else {
+      console.error(
+        'Unknown submode:',
+        this.operatorModeStateService.state?.submode,
+      );
+    }
   }
 
   private async determineTargetRealm({
     targetStackIndex,
-    realm,
+    targetRealm,
   }: BaseCommandModule.CopyCardInput) {
-    if (realm !== undefined && targetStackIndex !== undefined) {
+    if (targetRealm !== undefined && targetStackIndex !== undefined) {
       console.warn(
-        'Both targetStackIndex and targetRealmUrl are set; only one should be set; using targetRealmUrl',
+        'Both targetStackIndex and targetRealm are set; only one should be set -- using targetRealm',
       );
     }
-    if (realm) {
-      return realm;
+    if (targetRealm) {
+      return targetRealm;
     }
     if (targetStackIndex !== undefined) {
       // use existing card in stack to determine realm url,
