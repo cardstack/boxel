0e5e296a406957bc98c7678969db65b8fc03177c
Edward Faulkner <edward@eaf4.com>
2025-07-30 14:53:47 -0400

    removing `only`

--- packages/realm-server/tests/prerendering-test.ts
+++ packages/realm-server/tests/prerendering-test.ts
@@ -11,7 +11,7 @@ import {
 } from './helpers';
 import '@cardstack/runtime-common/helpers/code-equality-assertion';
 
-module.only(basename(__filename), function () {
+module(basename(__filename), function () {
   module('prerender', function (hooks) {
     let { virtualNetwork } = createVirtualNetworkAndLoader();
     let realmURL: string;
