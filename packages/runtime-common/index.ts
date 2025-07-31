dc82c47182a3da20ea73b2f8fa75919175213c50
Burcu Noyan <burcunoyan@gmail.com>
2025-07-31 12:31:10 -0400

    refactor css var extraction

--- packages/runtime-common/index.ts
+++ packages/runtime-common/index.ts
@@ -119,6 +119,7 @@ export {
   cardTypeIcon,
   getFieldIcon,
 } from './helpers/card-type-display-name';
+export { extractCssVariables } from './helpers/extract-css-variables';
 export * from './url';
 
 export const executableExtensions = ['.js', '.gjs', '.ts', '.gts'];
