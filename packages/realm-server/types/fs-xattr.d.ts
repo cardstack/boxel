5d62675e7acfac6b91c7f9486a16131ead85917b
tintinthong <justinthong93@gmail.com>
2025-08-05 15:08:36 +0800

    add fx-xattr library to write file attributes

---
+++ packages/realm-server/types/fs-xattr.d.ts
@@ -0,0 +1,59 @@
+// we declare a type module here so the upstream can be parsed by glint as esm
+// this version is compatible with 0.4.0 release
+declare module 'fs-xattr' {
+  /**
+   * Get extended attribute `attr` from file at `path`.
+   *
+   * @returns a `Promise` that will resolve with the value of the attribute.
+   */
+  export function getAttribute(path: string, attr: string): Promise<Buffer>;
+
+  /**
+   * Synchronous version of `getAttribute`.
+   */
+  export function getAttributeSync(path: string, attr: string): Buffer;
+
+  /**
+   * Set extended attribute `attr` to `value` on file at `path`.
+   *
+   * @returns a `Promise` that will resolve when the value has been set.
+   */
+  export function setAttribute(
+    path: string,
+    attr: string,
+    value: Buffer | string,
+  ): Promise<void>;
+
+  /**
+   * Synchronous version of `setAttribute`.
+   */
+  export function setAttributeSync(
+    path: string,
+    attr: string,
+    value: Buffer | string,
+  ): void;
+
+  /**
+   * Remove extended attribute `attr` on file at `path`.
+   *
+   * @returns a `Promise` that will resolve when the value has been removed.
+   */
+  export function removeAttribute(path: string, attr: string): Promise<void>;
+
+  /**
+   * Synchronous version of `removeAttribute`.
+   */
+  export function removeAttributeSync(path: string, attr: string): void;
+
+  /**
+   * List all attributes on file at `path`.
+   *
+   * @returns a `Promise` that will resolve with an array of strings, e.g. `['user.linusu.test', 'com.apple.FinderInfo']`.
+   */
+  export function listAttributes(path: string): Promise<string[]>;
+
+  /**
+   * Synchronous version of `listAttributes`.
+   */
+  export function listAttributesSync(path: string): string[];
+}
