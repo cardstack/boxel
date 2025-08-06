3c81ed92b4fa1ff5fe81710fc630c296db0f441b
tintinthong <justinthong93@gmail.com>
2025-08-06 18:48:51 +0800

    fix lint

--- packages/realm-server/tests/card-source-endpoints-test.ts
+++ packages/realm-server/tests/card-source-endpoints-test.ts
@@ -3,7 +3,7 @@ import { Test, SuperTest } from 'supertest';
 import { join, resolve, basename } from 'path';
 import { Server } from 'http';
 import { type DirResult } from 'tmp';
-import { existsSync, readFileSync, statSync } from 'fs-extra';
+import { existsSync, readFileSync } from 'fs-extra';
 import {
   cardSrc,
   compiledCard,
@@ -432,6 +432,10 @@ module(basename(__filename), function () {
             .send(`//TEST UPDATE\n${cardSrc}`);
 
           assert.strictEqual(response.status, 204, 'HTTP 204 status');
+          assert.ok(
+            response.headers['x-created'],
+            'created date should be set for new GTS file',
+          );
           assert.strictEqual(
             response.get('X-boxel-realm-url'),
             testRealmHref,
@@ -450,14 +454,6 @@ module(basename(__filename), function () {
             src,
             `//TEST UPDATE
           ${cardSrc}`,
-          );
-
-          let fileResponse = await request
-            .get('/unused-card.gts')
-            .set('Accept', 'application/vnd.card+source');
-          assert.ok(
-            fileResponse.headers['x-created'],
-            'created date should be set for new GTS file',
           );
         });
 
