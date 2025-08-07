675cea892ba0bf42cd2cd7cfebb1e049fcdc8672
Buck Doyle <buck.doyle@cardstack.com>
2025-08-07 10:03:00 -0500

    Revert "Add more logging"
    This reverts commit c33df1d4b071bb7bbddef1dca80d8baf5d4f824d.

--- packages/realm-server/server.ts
+++ packages/realm-server/server.ts
@@ -452,7 +452,6 @@ export class RealmServer {
       }
     }
 
-    console.log('About to find published realms');
     let publishedRealms = await this.findPublishedRealms();
     return [...realms, ...publishedRealms];
   }
