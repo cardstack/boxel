74073b1a2ccf9d33ecbdddac9e72502c1735fe33
Buck Doyle <buck.doyle@cardstack.com>
2025-08-14 11:20:25 -0500

    Remove unused imports

--- packages/matrix/tests/host-mode.spec.ts
+++ packages/matrix/tests/host-mode.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import {
   synapseStart,
   synapseStop,
@@ -8,32 +8,12 @@ import {
 import {
   startServer as startRealmServer,
   type IsolatedRealmServer,
-  appURL,
 } from '../helpers/isolated-realm-server';
-import {
-  clearLocalStorage,
-  createRealm,
-  login,
-  registerRealmUsers,
-  setupUserSubscribed,
-} from '../helpers';
+import { registerRealmUsers } from '../helpers';
 
 test.describe('Host mode', () => {
   let synapse: SynapseInstance;
   let realmServer: IsolatedRealmServer;
-  const serverIndexUrl = new URL(appURL).origin;
-  const realm1Name = 'realm1';
-  const realm1URL = new URL(`user1/${realm1Name}/`, serverIndexUrl).href;
-
-  async function setupRealms(page: Page) {
-    await clearLocalStorage(page, serverIndexUrl);
-    await setupUserSubscribed('@user1:localhost', realmServer);
-    await login(page, 'user1', 'pass', {
-      url: serverIndexUrl,
-    });
-    await createRealm(page, realm1Name);
-    await page.goto(realm1URL);
-  }
 
   test.beforeEach(async () => {
     // synapse defaults to 30s for beforeEach to finish, we need a bit more time
