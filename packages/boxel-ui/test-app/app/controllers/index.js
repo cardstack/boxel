4bcd08e3bc5c0b751b19df1ac613ed6c611b199f
Burcu Noyan <burcunoyan@gmail.com>
2025-08-15 17:27:19 -0400

    can preview themes on boxel-ui freestyle

--- packages/boxel-ui/test-app/app/controllers/index.js
+++ packages/boxel-ui/test-app/app/controllers/index.js
@@ -1,5 +1,21 @@
 import FreestyleController from 'ember-freestyle/controllers/freestyle';
 import { ALL_USAGE_COMPONENTS } from '@cardstack/boxel-ui/usage';
+import {
+  extractCssVariables,
+  styleConversions,
+} from '../utils/extract-css-variables.ts';
+import THEMES from '../themes/index.ts';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { fn, get } from '@ember/helper';
+import { BoxelSelect, FieldContainer } from '@cardstack/boxel-ui/components';
+
+function getThemeStyles(cssString) {
+  if (!extractCssVariables) {
+    return;
+  }
+  return styleConversions + extractCssVariables(cssString);
+}
 
 export default class IndexController extends FreestyleController {
   constructor() {
@@ -10,5 +26,21 @@ export default class IndexController extends FreestyleController {
         component: c,
       };
     });
+    this.themes = [
+      { name: '<None Selected>' },
+      ...Object.entries(THEMES).map(([name, vars]) => ({
+        name,
+        styles: getThemeStyles(vars),
+      })),
+    ];
+
+    this.boxelSelect = BoxelSelect;
+    this.fieldContainer = FieldContainer;
+  }
+
+  @tracked currentTheme;
+
+  @action selectTheme(theme) {
+    this.currentTheme = theme?.styles ? theme : undefined;
   }
 }
---
+++ packages/boxel-ui/test-app/app/themes/index.ts
@@ -0,0 +1,7 @@
+import { bubblegum } from './bubblegum';
+import { candyland } from './candyland';
+import { doom64 } from './doom-64';
+
+const THEMES = { bubblegum, candyland, doom64 };
+
+export default THEMES;
