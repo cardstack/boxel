8bb62359810897770f66dd7ceed670e04a57122c
tintinthong <justinthong93@gmail.com>
2025-08-18 11:54:41 +0800

    use default coding llm

--- packages/host/app/commands/listing-remix.ts
+++ packages/host/app/commands/listing-remix.ts
@@ -1,6 +1,7 @@
 import { service } from '@ember/service';
 
 import { isResolvedCodeRef, RealmPaths } from '@cardstack/runtime-common';
+import { DEFAULT_CODING_LLM } from '@cardstack/runtime-common/matrix-constants';
 
 import * as CardAPI from 'https://cardstack.com/base/card-api';
 import * as BaseCommandModule from 'https://cardstack.com/base/command';
@@ -128,7 +129,7 @@ export default class RemixCommand extends HostBaseCommand<
       openRoom: true,
       attachedCards: [listing],
       skillCardIds,
-      llmModel: 'anthropic/claude-sonnet-4',
+      llmModel: DEFAULT_CODING_LLM,
     });
   }
 }
