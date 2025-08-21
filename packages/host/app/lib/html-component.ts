f60b7b55cd07e5b7ef7d18361f3acc4b5b5ec2c4
Edward Faulkner <edward@eaf4.com>
2025-08-21 12:57:59 -0400

    Support html attributes on prerendered cards
    This allow you to pass html attributes and element modifiers to the component that represents a prerendered card.

--- packages/host/app/lib/html-component.ts
+++ packages/host/app/lib/html-component.ts
@@ -1,26 +1,80 @@
 import { setComponentManager } from '@ember/component';
 import { capabilities } from '@ember/component';
 import { setComponentTemplate } from '@ember/component';
-import { htmlSafe } from '@ember/template';
 
+import templateOnly from '@ember/component/template-only';
 import { precompileTemplate } from '@ember/template-compilation';
 
 import { ComponentLike } from '@glint/template';
+import { modifier } from 'ember-modifier';
+
+import { compiler } from '@cardstack/runtime-common/etc';
 
 class _HTMLComponent {
-  constructor(readonly html: string) {}
+  constructor(
+    readonly component: TopElement,
+    readonly attrs: Record<string, string>,
+    readonly children: Node[],
+  ) {}
 }
 
-export type HTMLComponent = ComponentLike<{ Args: {} }>;
+export type HTMLComponent = ComponentLike<{ Args: {}; Element: Element }>;
+
+const cache = new Map<string, TopElement>();
+
+type TopElement = ComponentLike<{
+  Args: { attrs: Record<string, string> };
+  Element: Element;
+}>;
+
 export function htmlComponent(html: string): HTMLComponent {
-  return new _HTMLComponent(html) as unknown as HTMLComponent;
+  let testContainer = document.createElement('div');
+  testContainer.innerHTML = html;
+  if (testContainer.children.length !== 1) {
+    throw new Error(
+      `htmlComponent expected exactly one element, found ${testContainer.children.length}`,
+    );
+  }
+  let cardElement = testContainer.children[0];
+  let tagName = cardElement.tagName.toLowerCase();
+
+  let sourceParts: string[] = [];
+  let attrs: Record<string, string> = {};
+
+  sourceParts.push(`<${tagName} `);
+
+  for (let { name, value } of cardElement.attributes) {
+    attrs[name] = value;
+    sourceParts.push(`${name}={{@attrs.${name}}} `);
+  }
+
+  sourceParts.push(`...attributes />`);
+
+  let source = sourceParts.join('');
+  let component: TopElement;
+  if (cache.has(source)) {
+    component = cache.get(source)!;
+  } else {
+    component = setComponentTemplate(
+      compiler.compile(source, { strictMode: true }),
+      templateOnly(),
+    ) as TopElement;
+    cache.set(source, component);
+  }
+
+  return new _HTMLComponent(component, attrs, [
+    ...cardElement.childNodes,
+  ]) as unknown as HTMLComponent;
 }
 
 setComponentTemplate(
-  precompileTemplate('{{htmlSafe this.html}}', {
-    strictMode: true,
-    scope: () => ({ htmlSafe }),
-  }),
+  precompileTemplate(
+    '<this.component @attrs={{this.attrs}} {{withChildren this.children}} ...attributes />',
+    {
+      strictMode: true,
+      scope: () => ({ withChildren }),
+    },
+  ),
   _HTMLComponent.prototype,
 );
 
@@ -43,3 +97,9 @@ setComponentManager(
   (owner) => HTMLComponentManager.create(owner),
   _HTMLComponent.prototype,
 );
+
+const withChildren = modifier((element: Element, [children]: [Node[]]) => {
+  for (let child of children) {
+    element.appendChild(child);
+  }
+});
