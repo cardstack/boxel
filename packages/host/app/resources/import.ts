ababac76b0c7db7b2b50ec2c9e5fdafceb80ccb5
tintinthong <justinthong93@gmail.com>
2025-09-04 13:30:05 +0800

    turn card-type and module-contents business logic into service so it can be used in a command (#3191)
    * refactor card-type to be a service
    
    * update service and types
    
    * make module contents service
    
    * fix test
    
    * get module-contents to handle exported cards or fields internally
    
    * hide module syntax inside of service
    
    * throw if cannot load module
    
    * fixes based upon pairing comments
    
    * fix test
    
    * refactor loadModule into a function + disableLoaderMonitoring change for test
    
    * fix lint
    
    * fix test. Needed to remove one withoutLoaderMonitoring
    
    * try minor improvement of moduleError running

--- packages/host/app/resources/import.ts
+++ packages/host/app/resources/import.ts
@@ -1,4 +1,3 @@
-import { getOwner } from '@ember/owner';
 import { service } from '@ember/service';
 import { isTesting } from '@embroider/macros';
 import { tracked } from '@glimmer/tracking';
@@ -14,19 +13,60 @@ import NetworkService from '../services/network';
 import type LoaderService from '../services/loader-service';
 
 interface Args {
-  named: { url: string; loader: Loader };
+  named: { url: string };
+}
+
+export type LoadResult =
+  | { module: object }
+  | { error: { type: 'runtime' | 'compile'; message: string } };
+
+export async function loadModule(
+  url: string,
+  loader: Loader,
+  fetch: (url: string, options?: RequestInit) => Promise<Response>,
+): Promise<LoadResult> {
+  try {
+    let m = await loader.import<object>(url);
+    return { module: m };
+  } catch (err: any) {
+    let errResponse = await fetch(url, {
+      headers: { 'content-type': 'text/javascript' },
+    });
+    if (!errResponse.ok) {
+      return {
+        error: {
+          type: 'compile',
+          message: err.responseText ?? (await errResponse.text()),
+        },
+      };
+    } else {
+      log.error(err);
+      return {
+        error: {
+          type: 'runtime',
+          message: `Encountered error while evaluating
+${url}:
+
+${err}
+
+Check console log for more details`,
+        },
+      };
+    }
+  }
 }
 
 const log = logger('resource:import');
 
 export class ImportResource extends Resource<Args> {
   @service declare private network: NetworkService;
+  @service declare private loaderService: LoaderService;
   @tracked module: object | undefined;
   @tracked error: { type: 'runtime' | 'compile'; message: string } | undefined;
   #loaded!: Promise<void>; // modifier runs at init so we will always have a value
 
   modify(_positional: never[], named: Args['named']) {
-    let { url, loader } = named;
+    let { url } = named;
     // The loader service is shared between the realm server and the host. this
     // resource can interfere with indexing in the browser by caching modules in
     // the loader that we are trying to change in our index. you can use the
@@ -35,7 +75,7 @@ export class ImportResource extends Resource<Args> {
     if (isTesting() && (globalThis as any).__disableLoaderMonitoring) {
       return;
     }
-    this.#loaded = this.load.perform(url, loader);
+    this.#loaded = this.load.perform(url, this.loaderService.loader);
   }
 
   get loaded() {
@@ -43,49 +83,19 @@ export class ImportResource extends Resource<Args> {
   }
 
   private load = task(async (url: string, loader: Loader) => {
-    try {
-      let m = await loader.import<object>(url);
-      this.module = m;
-    } catch (err: any) {
-      let errResponse = await this.network.authedFetch(url, {
-        headers: { 'content-type': 'text/javascript' },
-      });
-      if (!errResponse.ok) {
-        this.error = {
-          type: 'compile',
-          message: err.responseText ?? (await errResponse.text()),
-        };
-      } else {
-        this.error = {
-          type: 'runtime',
-          message: `Encountered error while evaluating
-${url}:
-
-${err}
-
-Check console log for more details`,
-        };
-        log.error(err);
-      }
+    const result = await loadModule(url, loader, this.network.authedFetch);
+    if ('module' in result) {
+      this.module = result.module;
+    } else {
+      this.error = result.error;
     }
   });
 }
 
-export function importResource(
-  parent: object,
-  url: () => string,
-  loader?: () => Loader,
-) {
+export function importResource(parent: object, url: () => string) {
   return ImportResource.from(parent, () => ({
     named: {
       url: url(),
-      loader: loader
-        ? loader()
-        : (
-            (getOwner(parent) as any).lookup(
-              'service:loader-service',
-            ) as LoaderService
-          ).loader,
     },
   })) as ImportResource;
 }
---
+++ packages/host/app/services/card-type-service.ts
@@ -0,0 +1,209 @@
+import { service } from '@ember/service';
+import Service from '@ember/service';
+
+import {
+  identifyCard,
+  internalKeyFor,
+  baseRealm,
+  moduleFrom,
+  getAncestor,
+  RealmInfo,
+  SupportedMimeType,
+  isResolvedCodeRef,
+  type ResolvedCodeRef,
+} from '@cardstack/runtime-common';
+import { isCodeRef, type CodeRef } from '@cardstack/runtime-common/code-ref';
+import { Loader } from '@cardstack/runtime-common/loader';
+
+import type CardService from '@cardstack/host/services/card-service';
+
+import type {
+  BaseDef,
+  Field,
+  FieldType,
+} from 'https://cardstack.com/base/card-api';
+import type * as CardAPI from 'https://cardstack.com/base/card-api';
+
+import type LoaderService from '../services/loader-service';
+import type NetworkService from '../services/network';
+
+export type CodeRefType = CodeRef & {
+  displayName: string;
+  localName: string;
+};
+
+export interface FieldOfType {
+  name: string;
+  card: Type | CodeRefType;
+  isComputed: boolean;
+  type: FieldType;
+}
+
+export interface Type {
+  id: string;
+  module: string;
+  displayName: string;
+  super: Type | undefined;
+  fields: FieldOfType[];
+  codeRef: CodeRef;
+  moduleInfo: ModuleInfo;
+  localName: string;
+}
+
+interface ModuleInfo {
+  extension: string;
+  realmInfo: RealmInfo;
+}
+
+export default class CardTypeService extends Service {
+  @service declare private cardService: CardService;
+  @service declare private network: NetworkService;
+  @service declare private loaderService: LoaderService;
+
+  private typeCache: Map<string, Type> = new Map();
+  private moduleInfoCache: Map<string, ModuleInfo> = new Map();
+  private loader: object | undefined; //keeps track of the current used loader so cache is reset after a loader reset
+
+  invalidateAllCaches(): void {
+    this.typeCache.clear();
+    this.moduleInfoCache.clear();
+  }
+
+  async assembleType(definition: typeof BaseDef): Promise<Type> {
+    // This should go away when we move to an architecture where NO loader reset is required
+    if (this.loader !== this.loaderService.loader) {
+      this.invalidateAllCaches();
+      this.loader = this.loaderService.loader;
+    }
+    let maybeType = await this.toType(definition, this.loaderService.loader);
+    if (isCodeRefType(maybeType)) {
+      throw new Error(`bug: should never get here`);
+    }
+    return maybeType;
+  }
+
+  private async toType(
+    card: typeof BaseDef,
+    loader: Loader,
+    stack: (typeof BaseDef)[] = [],
+  ): Promise<Type | CodeRefType> {
+    let maybeRef = identifyCard(card);
+    if (!maybeRef) {
+      throw new Error(`cannot identify card ${card.name}`);
+    }
+    let ref = maybeRef;
+    if (stack.includes(card)) {
+      return {
+        ...ref,
+        displayName: card.prototype.constructor.displayName,
+        localName: card.name,
+      };
+    }
+    let id = internalKeyFor(ref, undefined);
+    let cached = this.typeCache.get(id);
+    if (cached) {
+      return cached;
+    }
+    let moduleIdentifier = moduleFrom(ref);
+    let moduleInfo =
+      this.moduleInfoCache.get(moduleIdentifier) ??
+      (await this.fetchModuleInfo(new URL(moduleIdentifier)));
+
+    let api = await loader.import<typeof CardAPI>(`${baseRealm.url}card-api`);
+    let { id: _remove, ...fields } = api.getFields(card, {
+      includeComputeds: true,
+    });
+    let superCard = getAncestor(card);
+    let superType: Type | CodeRefType | undefined;
+    if (superCard && card !== superCard) {
+      superType = await this.toType(superCard, loader, [card, ...stack]);
+    }
+
+    let fieldTypes: FieldOfType[] = await Promise.all(
+      Object.entries(fields).map(
+        async ([name, field]: [string, Field<typeof BaseDef, any>]) => ({
+          name,
+          type: field.fieldType,
+          isComputed: field.computeVia != undefined,
+          card: await this.toType(field.card, loader, [card, ...stack]),
+        }),
+      ),
+    );
+
+    let type: Type = {
+      id,
+      module: moduleIdentifier,
+      super: isCodeRefType(superType) ? undefined : superType,
+      displayName: card.prototype.constructor.displayName || 'Card',
+      fields: fieldTypes,
+      moduleInfo,
+      codeRef: ref,
+      localName: card.name,
+    };
+    this.typeCache.set(id, type);
+    return type;
+  }
+
+  private async fetchModuleInfo(url: URL): Promise<ModuleInfo> {
+    let response = await this.network.authedFetch(url, {
+      headers: { Accept: SupportedMimeType.CardSource },
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Could not get file ${url.href}, status ${response.status}: ${
+          response.statusText
+        } - ${await response.text()}`,
+      );
+    }
+    let realmURL = response.headers.get('x-boxel-realm-url');
+    if (realmURL === null) {
+      throw new Error(`Could not get realm url for ${url.href}`);
+    }
+    let realmInfo = await this.cardService.getRealmInfoByRealmURL(
+      new URL(realmURL),
+    );
+    let moduleInfo = {
+      realmInfo,
+      extension: '.' + new URL(response.url).pathname.split('.').pop() || '',
+    };
+    this.moduleInfoCache.set(url.href, moduleInfo);
+    return moduleInfo;
+  }
+}
+
+function isCodeRefType(type: any): type is CodeRefType {
+  return (
+    type && isCodeRef(type) && 'displayName' in type && 'localName' in type
+  );
+}
+
+export function isFieldOfType(obj: any): obj is FieldOfType {
+  return obj && 'card' in obj;
+}
+
+export function getCodeRefFromType(t: Type | FieldOfType): CodeRef {
+  let codeRef: CodeRef;
+  if (isFieldOfType(t)) {
+    codeRef = isCodeRefType(t.card) ? t.card : (t.card as Type).codeRef;
+  } else {
+    codeRef = t.codeRef;
+  }
+  return codeRef;
+}
+
+export function getResolvedCodeRefFromType(
+  t: Type | FieldOfType,
+): ResolvedCodeRef | undefined {
+  let codeRef = getCodeRefFromType(t);
+  if (!isResolvedCodeRef(codeRef)) {
+    return;
+  }
+  return codeRef;
+}
+
+declare module '@ember/service' {
+  interface Registry {
+    'card-type-service': CardTypeService;
+  }
+}
