4fa67b97d2f1f3d378be18b9b998a85d3778d1e0
Buck Doyle <buck.doyle@cardstack.com>
2025-09-05 07:31:16 -0500

    Fix Connect iframe URL (#3207)

--- packages/host/app/services/host-mode-service.ts
+++ packages/host/app/services/host-mode-service.ts
@@ -28,8 +28,8 @@ export default class HostModeService extends Service {
       return false;
     }
 
-    if (config.realmServerDomain) {
-      let realmServerDomain = config.realmServerDomain;
+    if (config.realmServerURL) {
+      let realmServerDomain = new URL(config.realmServerURL).hostname;
       let currentHost = window.location.hostname;
 
       return (
