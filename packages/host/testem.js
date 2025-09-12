421abbfc8b6fb15c1641f33027a71beea9cb0005
Buck Doyle <buck.doyle@cardstack.com>
2025-09-12 07:30:50 +0200

    host: Add testem override to pretty print JSON

--- packages/host/testem.js
+++ packages/host/testem.js
@@ -6,6 +6,9 @@ const XunitReporter = require('testem/lib/reporters/xunit_reporter');
 const fs = require('fs');
 
 const config = {
+  tap_log_processor: function (log) {
+    return JSON.stringify(log, null, 2);
+  },
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: ['Chrome'],
