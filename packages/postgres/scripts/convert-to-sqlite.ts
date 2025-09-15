97e2adf17acc333bbd603634599e82220aa7c83f
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2025-09-15 13:59:51 -0400

    Add UUID-like support for sqlite

--- packages/postgres/scripts/convert-to-sqlite.ts
+++ packages/postgres/scripts/convert-to-sqlite.ts
@@ -118,7 +118,15 @@ function createColumns(
           ) {
             column.push('DEFAULT', String(constraint.expr.expr.text));
             break;
+          } else if (
+            constraint.expr.type === 'func_call' &&
+            constraint.expr.name.type === 'identifier' &&
+            constraint.expr.name.name === 'gen_random_uuid'
+          ) {
+            column.push('DEFAULT', '(hex(randomblob(16)))');
+            break;
           } else {
+            debugger;
             throw new Error(
               `Don't know how to serialize default value constraint for expression type '${constraint.expr.type}'`,
             );
