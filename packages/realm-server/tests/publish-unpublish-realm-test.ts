cea248fc3d72a435af3373ffc161b507a6bf3282
Buck Doyle <buck.doyle@cardstack.com>
2025-10-02 11:26:00 +0200

    Restore test

--- packages/realm-server/tests/publish-unpublish-realm-test.ts
+++ packages/realm-server/tests/publish-unpublish-realm-test.ts
@@ -1,4 +1,4 @@
-import { module, skip, test } from 'qunit';
+import { module, test } from 'qunit';
 import supertest, { SuperTest, Test } from 'supertest';
 import { v4 as uuidv4 } from 'uuid';
 import {
@@ -100,7 +100,7 @@ module(basename(__filename), function () {
     });
 
     // TODO restore in CS-9468
-    skip('POST /_publish-realm cannot publish a realm that is not publishable', async function (assert) {
+    test('POST /_publish-realm cannot publish a realm that is not publishable', async function (assert) {
       let response = await request
         .post('/_publish-realm')
         .set('Accept', 'application/vnd.api+json')
