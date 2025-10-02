0383ec30f4667b21e4060fec033c64d909bb438c
Buck Doyle <buck.doyle@cardstack.com>
2025-10-02 05:14:11 +0200

    Remove unused variables

--- packages/realm-server/scripts/start-all.sh
+++ packages/realm-server/scripts/start-all.sh
@@ -3,14 +3,12 @@
 BASE_REALM="http-get://localhost:4201/base/"
 EXPERIMENTS_REALM="http-get://localhost:4201/experiments/"
 NODE_TEST_REALM="http-get://localhost:4202/node-test/"
-TEST_REALM="http-get://localhost:4202/test/"
 
 READY_PATH="_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson"
 
 BASE_REALM_READY="$BASE_REALM$READY_PATH"
 EXPERIMENTS_REALM_READY="$EXPERIMENTS_REALM$READY_PATH"
 NODE_TEST_REALM_READY="$NODE_TEST_REALM$READY_PATH"
-TEST_REALM_READY="$TEST_REALM$READY_PATH"
 
 SYNAPSE_URL="http://localhost:8008"
 SMTP_4_DEV_URL="http://localhost:5001"
