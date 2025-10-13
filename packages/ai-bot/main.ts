21ba9f53a3f561f391568b35d5073b2d345fed32
Ian Calvert <ianjcalvert@gmail.com>
2025-10-13 14:56:18 +0100

    Remove breaking aibot change

--- packages/ai-bot/main.ts
+++ packages/ai-bot/main.ts
@@ -413,7 +413,7 @@ Common issues are:
             senderMatrixUserId,
           );
 
-          if (true || !creditValidation.hasEnoughCredits) {
+          if (!creditValidation.hasEnoughCredits) {
             // Careful when changing this message, it's used in the UI as a detection of whether to show the "Buy credits" button.
             return responder.onError(
               `You need a minimum of ${MINIMUM_AI_CREDITS_TO_CONTINUE} credits to continue using the AI bot. Please upgrade to a larger plan, or top up your account.`,
