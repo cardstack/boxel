c4bc3305696122faf904834f84abe068151cc814
Buck Doyle <buck.doyle@cardstack.com>
2025-10-24 15:02:14 +0100

    Remove experimental things

--- packages/host/testem.js
+++ packages/host/testem.js
@@ -19,7 +19,6 @@ const config = {
         '--headless',
         '--disable-dbus',
         '--disable-dev-shm-usage',
-        `--log-net-log=/tmp/chrome-netlog-${process.env.HOST_TEST_PARTITION}.json`,
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
