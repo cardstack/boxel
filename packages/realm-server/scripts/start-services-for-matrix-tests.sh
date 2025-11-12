64f97edd747f94676e2111f475525e790c747049
Ian Calvert <ianjcalvert@gmail.com>
2025-11-12 12:19:05 +0000

    Skills realm is needed for matrix tests

--- packages/realm-server/scripts/start-services-for-matrix-tests.sh
+++ packages/realm-server/scripts/start-services-for-matrix-tests.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+pnpm --dir=../skills-realm skills:setup
 WAIT_ON_TIMEOUT=600000 NODE_NO_WARNINGS=1 start-server-and-test \
   'run-p start:pg start:prerender-dev start:worker-base start:base' \
   'http-get://localhost:4201/base/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson' \
