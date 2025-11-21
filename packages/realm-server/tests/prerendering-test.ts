354932ca5e352a6d8e460cbf5ca0bb1aa5e9880c
Buck Doyle <buck.doyle@cardstack.com>
2025-11-21 10:04:33 -0600

    Fix formatting

--- packages/realm-server/tests/prerendering-test.ts
+++ packages/realm-server/tests/prerendering-test.ts
@@ -733,9 +733,7 @@ module(basename(__filename), function () {
           `failed to find twitter:card in head html:${cleanedHead}`,
         );
         assert.ok(
-          cleanedHead.includes(
-            `property="og:url" content="${realmURL2}1"`,
-          ),
+          cleanedHead.includes(`property="og:url" content="${realmURL2}1"`),
           `failed to find og:url in head html:${cleanedHead}`,
         );
       });
