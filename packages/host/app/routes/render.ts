c1087143e7788d327471e6d2d66cda9ef4a96f58
Luke Melia <luke@lukemelia.com>
2025-11-26 19:34:14 -0500

    Guard lazilyLoadLinks flag per route

--- packages/host/app/routes/render.ts
+++ packages/host/app/routes/render.ts
@@ -1,10 +1,10 @@
-import type Controller from '@ember/controller';
 import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import type RouterService from '@ember/routing/router-service';
 import type Transition from '@ember/routing/transition';
 import { join, scheduleOnce } from '@ember/runloop';
 import { service } from '@ember/service';
+import type Controller from '@ember/controller';
 
 import { isTesting } from '@embroider/macros';
 
@@ -109,7 +109,11 @@ export default class RenderRoute extends Route<Model> {
       currentURL: this.router.currentURL,
     });
     this.#setAllModelStatuses('unusable');
-    (globalThis as any).__lazilyLoadLinks = undefined;
+    // Only unset lazilyLoadLinks if we last set it
+    if ((globalThis as any).__lazilyLoadLinksOwner === 'render') {
+      (globalThis as any).__lazilyLoadLinks = undefined;
+      (globalThis as any).__lazilyLoadLinksOwner = undefined;
+    }
     (globalThis as any).__boxelRenderContext = undefined;
   };
 
@@ -121,7 +125,11 @@ export default class RenderRoute extends Route<Model> {
   }
 
   deactivate() {
-    (globalThis as any).__lazilyLoadLinks = undefined;
+    // Only unset lazilyLoadLinks if we last set it
+    if ((globalThis as any).__lazilyLoadLinksOwner === 'render') {
+      (globalThis as any).__lazilyLoadLinks = undefined;
+      (globalThis as any).__lazilyLoadLinksOwner = undefined;
+    }
     (globalThis as any).__boxelRenderContext = undefined;
     (globalThis as any).__renderInstance = undefined;
     window.removeEventListener('error', this.errorHandler);
@@ -147,6 +155,7 @@ export default class RenderRoute extends Route<Model> {
     // activate() doesn't run early enough for this to be set before the model()
     // hook is run
     (globalThis as any).__lazilyLoadLinks = true;
+    (globalThis as any).__lazilyLoadLinksOwner = 'render';
     (globalThis as any).__boxelRenderContext = true;
     this.#authGuard.register();
     if (!isTesting()) {
