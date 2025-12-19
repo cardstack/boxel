db6f5590925e7bc956839d3455fb6d3b1892bd12
Ian Calvert <ianjcalvert@gmail.com>
2025-12-19 20:06:56 +0000

    Fix incorrectly rewritten content type

--- packages/realm-server/scripts/start-services-for-host-tests.sh
+++ packages/realm-server/scripts/start-services-for-host-tests.sh
@@ -52,7 +52,7 @@ WAIT_ON_TIMEOUT=900000 \
   NODE_NO_WARNINGS=1 \
   start-server-and-test \
     'run-p start:pg start:prerender-dev start:prerender-manager-dev start:matrix start:smtp start:worker-development start:development' \
-    'http-get://localhost:4201/base/_readiness-check?acceptHeader=application%2Fvnd.api%2Fjson|http://localhost:8008|http://localhost:5001' \
+    'http-get://localhost:4201/base/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson|http://localhost:8008|http://localhost:5001' \
     'run-p start:worker-test start:test-realms' \
-    'http-get://localhost:4202/node-test/_readiness-check?acceptHeader=application%2Fvnd.api%2Fjson' \
+    'http-get://localhost:4202/node-test/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson' \
     'wait'
