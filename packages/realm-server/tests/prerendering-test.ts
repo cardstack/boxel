822ed2815f2f3efe62f29608222ca53cf13fdc08
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2025-12-23 14:15:13 -0500

    revert test file

--- packages/realm-server/tests/prerendering-test.ts
+++ packages/realm-server/tests/prerendering-test.ts
@@ -75,7 +75,7 @@ function makeStubPagePool(maxPages: number) {
   return { pool, contextsCreated, contextsClosed };
 }
 
-module.only(basename(__filename), function () {
+module(basename(__filename), function () {
   module('prerender - dynamic tests', function (hooks) {
     let realmURL = 'http://127.0.0.1:4450/';
     let prerenderServerURL = realmURL.endsWith('/')
