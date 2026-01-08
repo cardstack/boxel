e44e6ab409b14f849e8b6b3c7c08299e3069f276
Fadhlan Ridhwanallah <fridhwanallah@gmail.com>
2026-01-08 18:53:04 +0700

    Fix realm server tests

--- packages/realm-server/tests/server-endpoints-test.ts
+++ packages/realm-server/tests/server-endpoints-test.ts
@@ -347,7 +347,7 @@ module(basename(__filename), function () {
             assert.strictEqual(response.status, 200, 'HTTP 200 status');
             let doc = response.body as SingleCardDocument;
             assert.strictEqual(
-              doc.data.attributes?.title,
+              doc.data.attributes?.cardTitle,
               'Test Card',
               'instance data is correct',
             );
@@ -555,7 +555,7 @@ module(basename(__filename), function () {
             assert.strictEqual(response.status, 200, 'HTTP 200 status');
             let doc = response.body as SingleCardDocument;
             assert.strictEqual(
-              doc.data.attributes?.title,
+              doc.data.attributes?.cardTitle,
               'Test Card',
               'instance data is correct',
             );
