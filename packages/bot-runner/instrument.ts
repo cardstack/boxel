0679124367de4f78917386f886ed7bf37bd27e6d
tintinthong <justinthong93@gmail.com>
2026-01-27 22:47:41 +0800

    fix env vars for SENTRY_DSN

--- packages/bot-runner/instrument.ts
+++ packages/bot-runner/instrument.ts
@@ -1,8 +1,8 @@
 import * as Sentry from '@sentry/node';
 
-if (process.env.BOT_RUNNER_SENTRY_DSN) {
+if (process.env.SENTRY_DSN) {
   Sentry.init({
-    dsn: process.env.BOT_RUNNER_SENTRY_DSN,
+    dsn: process.env.SENTRY_DSN,
     environment: process.env.SENTRY_ENVIRONMENT || 'development',
     maxValueLength: 8192, // prevents error messages reported in sentry from being truncated
   });
