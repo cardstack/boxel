cc60d6e057764aab356712652110f43c920fc82e
tintinthong <justinthong93@gmail.com>
2026-01-27 17:31:23 +0800

    forgot migration contents

--- packages/postgres/migrations/1769504850011_create-bot-registrations.js
+++ packages/postgres/migrations/1769504850011_create-bot-registrations.js
@@ -1,5 +1,26 @@
 exports.shorthands = undefined;
 
-exports.up = pgm => {};
+exports.up = (pgm) => {
+  pgm.createTable('bot_registrations', {
+    id: {
+      type: 'uuid',
+      notNull: true,
+      primaryKey: true,
+    },
+    username: {
+      type: 'text',
+      notNull: true,
+    },
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+    },
+  });
 
-exports.down = pgm => {};
+  pgm.createIndex('bot_registrations', 'username');
+};
+
+exports.down = (pgm) => {
+  pgm.dropIndex('bot_registrations', 'username');
+  pgm.dropTable('bot_registrations');
+};
