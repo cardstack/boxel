6c78fb57ad5067341d469b554b4e1db1668739e2
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2026-02-12 21:34:04 -0500

    add more logs for CI

--- packages/realm-server/scripts/start-services-for-matrix-tests.sh
+++ packages/realm-server/scripts/start-services-for-matrix-tests.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 pnpm --dir=../skills-realm skills:setup
 WAIT_ON_TIMEOUT=600000 NODE_NO_WARNINGS=1 start-server-and-test \
-  'run-p start:pg start:prerender-dev start:prerender-manager-dev start:worker-base start:base' \
+  'run-p -ln start:pg start:prerender-dev start:prerender-manager-dev start:worker-base start:base' \
   'http-get://localhost:4201/base/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson' \
   'wait'
