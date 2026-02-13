13e84109026246015525fb8d690815bde64fd4c7
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2026-02-12 21:08:52 -0500

    more visibility into failing start scripts

--- packages/realm-server/scripts/start-services-for-host-tests.sh
+++ packages/realm-server/scripts/start-services-for-host-tests.sh
@@ -57,8 +57,8 @@ WAIT_ON_TIMEOUT=900000 \
   LOG_LEVELS="$HOST_TEST_LOG_LEVELS" \
   NODE_NO_WARNINGS=1 \
   start-server-and-test \
-    'run-p start:pg start:prerender-dev start:prerender-manager-dev start:matrix start:smtp start:worker-development start:development' \
+    'run-p -ln --aggregate-output start:pg start:prerender-dev start:prerender-manager-dev start:matrix start:smtp start:worker-development start:development' \
     'http-get://localhost:4201/base/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson|http://localhost:8008|http://localhost:5001' \
-    'run-p start:worker-test start:test-realms' \
+    'run-p -ln --aggregate-output start:worker-test start:test-realms' \
     'http-get://localhost:4202/node-test/_readiness-check?acceptHeader=application%2Fvnd.api%2Bjson' \
     'wait'
