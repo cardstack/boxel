daa217a00c56ac3f05c4cb1ca831bf589195183d
Ian Calvert <ianjcalvert@gmail.com>
2026-02-19 15:24:34 +0000

    Delete packages/postgres/migrations/1770648743000_add-realm-user-id-to-session-rooms.js

--- packages/postgres/migrations/1770648743000_add-realm-user-id-to-session-rooms.js
+++
@@ -1,9 +0,0 @@
-exports.up = (pgm) => {
-  pgm.addColumns('session_rooms', {
-    realm_user_id: { type: 'varchar' },
-  });
-};
-
-exports.down = (pgm) => {
-  pgm.dropColumns('session_rooms', ['realm_user_id']);
-};
