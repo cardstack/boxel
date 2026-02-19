5b3d296aad96c93ef1b945151b0ea95f5acd4634
Ian Calvert <ianjcalvert@gmail.com>
2026-02-19 15:23:21 +0000

    Don't remove the sessions table

--- packages/postgres/migrations/1771264925000_move-session-room-to-users.js
+++ packages/postgres/migrations/1771264925000_move-session-room-to-users.js
@@ -15,37 +15,5 @@ exports.up = (pgm) => {
 };
 
 exports.down = (pgm) => {
-  pgm.createTable(
-    'session_rooms',
-    {
-      realm_url: { type: 'varchar', notNull: true },
-      realm_user_id: { type: 'varchar' },
-      matrix_user_id: { type: 'varchar', notNull: true },
-      room_id: { type: 'varchar', notNull: true },
-      created_at: {
-        type: 'timestamp',
-        notNull: true,
-        default: pgm.func('current_timestamp'),
-      },
-      updated_at: {
-        type: 'timestamp',
-        notNull: true,
-        default: pgm.func('current_timestamp'),
-      },
-    },
-    {
-      constraints: {
-        primaryKey: ['realm_url', 'matrix_user_id'],
-      },
-    },
-  );
-
-  pgm.sql(`
-    INSERT INTO session_rooms (realm_url, matrix_user_id, room_id)
-    SELECT '__realm-server__', matrix_user_id, session_room_id
-    FROM users
-    WHERE session_room_id IS NOT NULL
-  `);
-
   pgm.dropColumns('users', ['session_room_id']);
 };
