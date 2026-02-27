7f0d2c2262eab12a7d97c8e5a9a405bbeb068a41
Buck Doyle <buck.doyle@cardstack.com>
2026-02-27 10:27:39 -0700

    Fix migration to be environment-specific

--- packages/postgres/migrations/1772212367371_rename-base-realm-url-to-prefix.js
+++ packages/postgres/migrations/1772212367371_rename-base-realm-url-to-prefix.js
@@ -1,15 +1,49 @@
 exports.up = (pgm) => {
-  pgm.sql(
-    `UPDATE realm_user_permissions
-     SET realm_url = '@cardstack/base/'
-     WHERE realm_url = 'https://cardstack.com/base/'`,
-  );
+  switch (process.env.REALM_SENTRY_ENVIRONMENT) {
+    case 'staging':
+      pgm.sql(
+        `UPDATE realm_user_permissions
+         SET realm_url = 'https://realms-staging.stack.cards/base/'
+         WHERE realm_url = 'https://cardstack.com/base/'`,
+      );
+      break;
+    case 'production':
+      pgm.sql(
+        `UPDATE realm_user_permissions
+         SET realm_url = 'https://app.boxel.ai/base/'
+         WHERE realm_url = 'https://cardstack.com/base/'`,
+      );
+      break;
+    default:
+      pgm.sql(
+        `UPDATE realm_user_permissions
+         SET realm_url = 'http://localhost:4201/base/'
+         WHERE realm_url = 'https://cardstack.com/base/'`,
+      );
+  }
 };
 
 exports.down = (pgm) => {
-  pgm.sql(
-    `UPDATE realm_user_permissions
-     SET realm_url = 'https://cardstack.com/base/'
-     WHERE realm_url = '@cardstack/base/'`,
-  );
+  switch (process.env.REALM_SENTRY_ENVIRONMENT) {
+    case 'staging':
+      pgm.sql(
+        `UPDATE realm_user_permissions
+         SET realm_url = 'https://cardstack.com/base/'
+         WHERE realm_url = 'https://realms-staging.stack.cards/base/'`,
+      );
+      break;
+    case 'production':
+      pgm.sql(
+        `UPDATE realm_user_permissions
+         SET realm_url = 'https://cardstack.com/base/'
+         WHERE realm_url = 'https://app.boxel.ai/base/'`,
+      );
+      break;
+    default:
+      pgm.sql(
+        `UPDATE realm_user_permissions
+         SET realm_url = 'https://cardstack.com/base/'
+         WHERE realm_url = 'http://localhost:4201/base/'`,
+      );
+  }
 };
