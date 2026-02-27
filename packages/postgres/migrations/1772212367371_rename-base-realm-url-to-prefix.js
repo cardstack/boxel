7702b237b27fb699aa98bdec6a66373e43cff821
Buck Doyle <buck.doyle@cardstack.com>
2026-02-27 12:50:32 -0700

    Add table-clearing to migration

--- packages/postgres/migrations/1772212367371_rename-base-realm-url-to-prefix.js
+++ packages/postgres/migrations/1772212367371_rename-base-realm-url-to-prefix.js
@@ -1,4 +1,12 @@
 exports.up = (pgm) => {
+  // Clear cached index and module data that references the old
+  // https://cardstack.com/base/ URL format. These tables are rebuilt
+  // automatically when the realm server starts.
+  pgm.sql(`DELETE FROM boxel_index`);
+  pgm.sql(`DELETE FROM boxel_index_working`);
+  pgm.sql(`DELETE FROM realm_versions`);
+  pgm.sql(`DELETE FROM modules`);
+
   switch (process.env.REALM_SENTRY_ENVIRONMENT) {
     case 'staging':
       pgm.sql(
