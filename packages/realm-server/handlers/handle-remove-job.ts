774a9248f991729962fc11233a02060eb60b136e
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2026-03-05 18:33:23 -0500

    Add support for cancelling running indexing job from a command (#4122)
    * Add support for cancelling running indexing job from a command
    
    * fixed based on code review

--- packages/realm-server/handlers/handle-remove-job.ts
+++ packages/realm-server/handlers/handle-remove-job.ts
@@ -1,45 +1,14 @@
 import type Koa from 'koa';
 import {
-  type Expression,
-  query as _query,
-  param,
-  separatedByCommas,
+  findJobIdForReservationId,
+  forceCancelJobById,
 } from '@cardstack/runtime-common';
 import { sendResponseForBadRequest, setContextResponse } from '../middleware';
 import type { CreateRoutesArgs } from '../routes';
 
-export default function handleReindex({
+export default function handleRemoveJob({
   dbAdapter,
 }: CreateRoutesArgs): (ctxt: Koa.Context, next: Koa.Next) => Promise<void> {
-  async function query(expression: Expression) {
-    return await _query(dbAdapter, expression);
-  }
-
-  async function forceJobCompletion(jobId: string) {
-    await query([
-      `UPDATE jobs SET `,
-      ...separatedByCommas([
-        [
-          `result =`,
-          param({
-            status: 418,
-            message: `User initiated job cancellation`,
-          }),
-        ],
-        [`status = 'rejected'`],
-        [`finished_at = NOW()`],
-      ]),
-      'WHERE id =',
-      param(jobId),
-    ] as Expression);
-    await query([
-      `UPDATE job_reservations SET completed_at = NOW() WHERE job_id =`,
-      param(jobId),
-      `AND completed_at IS NULL`,
-    ]);
-    await query([`NOTIFY jobs_finished`]);
-  }
-
   return async function (ctxt: Koa.Context, _next: Koa.Next) {
     let jobId = ctxt.URL.searchParams.get('job_id');
     let jobReservationId = ctxt.URL.searchParams.get('reservation_id');
@@ -52,10 +21,7 @@ export default function handleReindex({
     }
 
     if (jobReservationId) {
-      let [{ job_id: jobId }] = (await query([
-        `SELECT job_id FROM job_reservations WHERE id =`,
-        param(jobReservationId),
-      ])) as [{ job_id: string }];
+      jobId = await findJobIdForReservationId(dbAdapter, jobReservationId);
       if (!jobId) {
         await sendResponseForBadRequest(
           ctxt,
@@ -63,9 +29,9 @@ export default function handleReindex({
         );
         return;
       }
-      await forceJobCompletion(jobId);
+      await forceCancelJobById(dbAdapter, jobId);
     } else if (jobId) {
-      await forceJobCompletion(jobId);
+      await forceCancelJobById(dbAdapter, jobId);
     }
     return setContextResponse(ctxt, new Response(null, { status: 204 }));
   };
