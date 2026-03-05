4f67b1669e93916ec1c8877a8d5def1a45252653
tintinthong <justinthong93@gmail.com>
2026-03-05 09:21:49 +0800

    Fix catalog env SKIP_CATALOG again (#4109)
    * only check for skip_catalog=true
    
    * fix more catalog env
    
    * pls do not fallback bcos its super confusing
    
    * ensure ci-host tests skips catalog
    
    * fix with booleans
    
    * handle check for web-assets

--- packages/realm-server/scripts/start-services-for-host-tests.sh
+++ packages/realm-server/scripts/start-services-for-host-tests.sh
@@ -48,14 +48,7 @@ export CATALOG_REALM_PATH="$CATALOG_TEMP_PATH"
 
 # Make host-test startup logs focus on indexing progress rather than per-request noise.
 HOST_TEST_LOG_LEVELS="${HOST_TEST_LOG_LEVELS:-*=info,realm:requests=warn,realm-index-updater=debug,index-runner=debug,index-perf=debug,index-writer=debug,worker=debug,worker-manager=debug}"
-# Default to skipping catalog for host tests, but allow `SKIP_CATALOG=false`
-# to opt back into starting catalog in start-development.sh.
-if [ "${SKIP_CATALOG:-true}" = "false" ]; then
-  SKIP_CATALOG=""
-else
-  SKIP_CATALOG="${SKIP_CATALOG:-true}"
-fi
-
+SKIP_CATALOG="${SKIP_CATALOG:-}"
 # There is a race condition starting up the servers that setting up the
 # submission realm triggers which triggers the start-development.sh script to
 # SIGTERM. currently we don't need the submission realm for host tests to
