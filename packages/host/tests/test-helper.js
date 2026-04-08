de80bf717fdb3070793b34dc091b4e4d84f91964
Edward Faulkner <edward@eaf4.com>
2026-04-08 17:47:18 -0400

    the docs are wrong, this is still at test-support

--- packages/host/tests/test-helper.js
+++ packages/host/tests/test-helper.js
@@ -3,7 +3,7 @@ import config from '@cardstack/host/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import setupOperatorModeParametersMatchAssertion from '@cardstack/host/tests/helpers/operator-mode-parameters-match';
-import { start as examStart } from 'ember-exam/addon-test-support';
+import { start as examStart } from 'ember-exam/test-support';
 // eslint-disable-next-line ember/no-test-import-export
 import { loadRealmTests } from './live-test';
 import { setupQUnit } from './helpers/setup-qunit';
