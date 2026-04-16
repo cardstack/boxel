32a086e46974a5ad071c0d279c08005ecc9c2352
Edward Faulkner <edward@eaf4.com>
2026-04-16 14:58:35 -0400

    host: upgrade @embroider/vite (major) + related deps
    Bumps embroider/vite and related vite-side deps to the next major and
    adjusts vite.config.mjs for the API changes.

--- packages/host/vite.config.mjs
+++ packages/host/vite.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
-import { extensions, classicEmberSupport, ember } from '@embroider/vite';
+import {
+  extensions as _extensions,
+  classicEmberSupport,
+  ember,
+} from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 import { createRequire } from 'node:module';
 import { scopedCSS } from 'glimmer-scoped-css/rollup';
 
 const require = createRequire(import.meta.url);
+
+// TODO: working around possible upstream problem. The app blueprint uses this
+// for guiding babel, but it includes '.json' which doesn't work in babel.
+const extensions = _extensions.filter((e) => e !== '.json');
 
 export default defineConfig({
   resolve: {
