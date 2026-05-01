3e96dbb0176d59224750083f2bc5ab98f4c3f9f8
Buck Doyle <buck.doyle@cardstack.com>
2026-05-01 07:58:20 -0500

    Fix return type of idFromCardOrURL

--- packages/host/app/utils/id-from-card-or-url.ts
+++ packages/host/app/utils/id-from-card-or-url.ts
@@ -1,7 +1,11 @@
+import { rri, type RealmResourceIdentifier } from '@cardstack/runtime-common';
+
 import type { CardDef } from 'https://cardstack.com/base/card-api';
 
-export function idFromCardOrURL(cardOrURL: CardDef | URL | string): string {
-  if (typeof cardOrURL === 'string') return cardOrURL;
-  if (cardOrURL instanceof URL) return cardOrURL.href;
-  return cardOrURL.id;
+export function idFromCardOrURL(
+  cardOrURL: CardDef | URL | string,
+): RealmResourceIdentifier {
+  if (typeof cardOrURL === 'string') return rri(cardOrURL);
+  if (cardOrURL instanceof URL) return rri(cardOrURL.href);
+  return rri(cardOrURL.id);
 }
