e32387deff5f568c484fba1cbf1b7e517d8145a2
Buck Doyle <buck.doyle@cardstack.com>
2026-05-12 19:05:46 +0200

    Remove transitional command/parameter rename code (#4771)

--- packages/host/app/commands/get-available-realm-urls.ts
+++
@@ -1,5 +0,0 @@
-// Deprecated alias kept for backwards compatibility with cards still
-// referencing the old URL-flavored module path. Remove once all
-// downstream consumers have been on the renamed path for at least one
-// release. Tracked by CS-11046.
-export { default } from './get-available-realm-identifiers';
