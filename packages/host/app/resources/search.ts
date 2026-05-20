a5353d5c5baa1e0a8e50b7c57a9f7fa3759941de
Buck Doyle <buck.doyle@cardstack.com>
2026-05-20 13:16:39 -0500

    Fix lint/type errors

--- packages/host/app/resources/search.ts
+++ packages/host/app/resources/search.ts
@@ -302,9 +302,7 @@ export class SearchResource<
     return this.realmsToSearch
       .map((realm) => {
         let realmPath = new RealmPaths(ri(realm));
-        let cards = this.instances.filter((card) =>
-          realmPath.inRealm(card.id),
-        );
+        let cards = this.instances.filter((card) => realmPath.inRealm(card.id));
         return { realm, cards };
       })
       .filter((r) => r.cards.length > 0);
