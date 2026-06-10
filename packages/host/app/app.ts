b2186be971cb75a28d099bd308b3a261dd496157
Buck Doyle <buck.doyle@cardstack.com>
2026-06-10 10:36:31 -0500

    host: Restore Ember Inspector with Vite

--- packages/host/app/app.ts
+++ packages/host/app/app.ts
@@ -3,6 +3,7 @@ import './lib/public-path'; // this should be first
 import './lib/setup-globals'; // This should be second
 import './deprecation-workflow';
 import Application from '@ember/application';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
@@ -17,6 +18,16 @@ export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver.withModules(compatModules);
+
+  // Let ember-inspector reach ember-source modules in development. The macro
+  // strips this assignment (and the import) from production builds entirely.
+  inspector = macroCondition(isDevelopingApp())
+    ? (
+        importSync('@embroider/legacy-inspector-support/ember-source-4.12') as {
+          default: (app: Application) => void;
+        }
+      ).default(this)
+    : undefined;
 }
 
 loadInitializers(App, config.modulePrefix, compatModules);
