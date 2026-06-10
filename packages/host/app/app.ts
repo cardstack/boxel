e47e52a1d287107f8e1ae038bddb76300f60f6f7
Buck Doyle <buck.doyle@cardstack.com>
2026-06-10 14:58:09 -0500

    host: Restore Ember Inspector with Vite (#5184)

--- packages/host/app/app.ts
+++ packages/host/app/app.ts
@@ -3,6 +3,7 @@ import './lib/public-path'; // this should be first
 import './lib/setup-globals'; // This should be second
 import './deprecation-workflow';
 import Application from '@ember/application';
+import setupInspector from '@embroider/legacy-inspector-support/ember-source-4.12';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
@@ -17,6 +18,8 @@ export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver.withModules(compatModules);
+
+  inspector = setupInspector(this);
 }
 
 loadInitializers(App, config.modulePrefix, compatModules);
