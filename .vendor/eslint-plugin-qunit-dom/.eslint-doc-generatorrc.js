60ca82ba4eab29310cbcaa4ac694bdadd6771147
Buck Doyle <buck.doyle@cardstack.com>
2026-07-06 08:15:36 -0500

    Replace vendored package with updated version

--- .vendor/eslint-plugin-qunit-dom/.eslint-doc-generatorrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint-doc-generator').GenerateOptions} */
-const config = {
-  pathRuleDoc: 'rules/{name}.md',
-  ruleDocSectionInclude: ['Examples'],
-  ruleDocTitleFormat: 'name',
-};
-
-module.exports = config;
