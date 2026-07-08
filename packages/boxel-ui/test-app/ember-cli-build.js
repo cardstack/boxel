6ba40935164bc224a8140714b6695dfb205831b3
Edward Faulkner <edward@eaf4.com>
2026-07-08 15:57:35 -0400

    Convert boxel-ui/test-app to vite
    This eliminates the last direct usages of webpack, @embroider/webpack, and several broccoli libraries.

--- packages/boxel-ui/test-app/ember-cli-build.js
+++ packages/boxel-ui/test-app/ember-cli-build.js
@@ -1,56 +1,11 @@
 'use strict';
-
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const { Webpack } = require('@embroider/webpack');
 const { compatBuild } = require('@embroider/compat');
-const withSideWatch = require('./lib/with-side-watch');
 
-module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // Add options here
-    autoImport: {
-      watchDependencies: ['@cardstack/boxel-ui'],
-    },
-    'ember-cli-babel': {
-      enableTypeScriptTransform: true,
-    },
-    trees: {
-      app: withSideWatch('app', {
-        watching: ['../addon'],
-      }),
-    },
-  });
+module.exports = async function (defaults) {
+  const { buildOnce } = await import('@embroider/vite');
 
-  return compatBuild(app, Webpack, {
-    skipBabel: [
-      {
-        package: 'qunit',
-      },
-    ],
-    staticAddonTestSupportTrees: true,
-    staticAddonTrees: true,
-    // staticHelpers: true,
-    // staticComponents: true,
-    // staticAppPaths: [],
-    packagerOptions: {
-      webpackConfig: {
-        module: {
-          rules: [
-            {
-              test: /\.woff2$/,
-              type: 'asset',
-            },
-            {
-              test: /\.otf$/,
-              type: 'asset',
-            },
-            {
-              test: /\.webp$/,
-              type: 'asset',
-            },
-          ],
-        },
-      },
-    },
-  });
+  const app = new EmberApp(defaults, {});
+
+  return compatBuild(app, buildOnce, {});
 };
