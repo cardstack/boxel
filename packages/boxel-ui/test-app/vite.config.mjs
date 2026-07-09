7b18127dfc6deab80a9995ab6659e7fc9623f8dd
Burcu Noyan <burcunoyan@gmail.com>
2026-07-09 16:55:36 -0400

    restore boxel-ui test app to run on port 4220

--- packages/boxel-ui/test-app/vite.config.mjs
+++ packages/boxel-ui/test-app/vite.config.mjs
@@ -3,6 +3,9 @@ import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
+  server: {
+    port: 4220,
+  },
   plugins: [
     classicEmberSupport(),
     ember(),
